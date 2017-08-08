50a9cd4518745fac7485fdcbd80be03e4823cde1
Iñigo López Canalejo <inigo.lopez@tum.de>
2017-08-08 16:18:49 +0200

    printing

--- applications/CompressiblePotentialFlowApplication/tests/test_potential_adjoint_element_2d.py
+++ applications/CompressiblePotentialFlowApplication/tests/test_potential_adjoint_element_2d.py
@@ -124,6 +124,7 @@ class TestCase(KratosUnittest.TestCase):
             
         # analytical implementation       
         ShapeDerivativeMatrix = self.adjoint_element.Calculate(SHAPE_DERIVATIVE_MATRIX_1,self.model_part.ProcessInfo)
+        print(ShapeDerivativeMatrix)
         self.assertMatrixAlmostEqual(FDShapeDerivativeMatrix, ShapeDerivativeMatrix)
 
 

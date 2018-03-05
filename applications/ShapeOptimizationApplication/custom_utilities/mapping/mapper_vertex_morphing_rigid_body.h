6837d48606d0fc041fe7b19328c3bb6e92dff51d
matthias <matthias.willer@tum.de>
2018-03-05 15:14:51 +0100

    assembling modified matrix row-wise and cleaning code

--- applications/ShapeOptimizationApplication/custom_utilities/mapping/mapper_vertex_morphing_rigid_body.h
+++ applications/ShapeOptimizationApplication/custom_utilities/mapping/mapper_vertex_morphing_rigid_body.h
@@ -17,6 +17,7 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <fstream>
 
 // ------------------------------------------------------------------------------
 // External includes
@@ -260,7 +261,7 @@ public:
             int collumn_id = neighbor_node.GetValue(MAPPING_ID);
 
             double weight = list_of_weights[neighbor_itr] / sum_of_weights;
-            mMappingMatrix.push_back(row_id,collumn_id,weight);
+            mMappingMatrix.insert_element(row_id,collumn_id,weight);
         }
     }
 
@@ -489,138 +490,94 @@ public:
     // --------------------------------------------------------------------------
     void CorrectDesignUpdateWithRigidBodyConstraints( CompressedLinearSolverType::Pointer linear_solver )
     {
-        // centroid A
-        Vector centroid_undeformed = ZeroVector(3);
-        // centroid B
-        Vector centroid_deformed = ZeroVector(3);
 
-        for(int node_index = 0 ; node_index<mListOfRigidNodes.size() ; node_index++)
+        if (mNumberOfRigidNodes != 0)
         {
-            // Get node information
-            ModelPart::NodeType& node_i = *mListOfRigidNodes[node_index];
+            boost::timer svd_time;
+            std::cout << "\n> Starting to compute SVD ..." << std::endl;
+            
+            Vector centroid_undeformed = ZeroVector(3); // centroid A
+            Vector centroid_deformed = ZeroVector(3); // centroid B
 
-            array_3d& coord = node_i.Coordinates();
-            array_3d& def = node_i.FastGetSolutionStepValue( SHAPE_UPDATE );
-            // array_3d& def = node_i.FastGetSolutionStepValue( OBJECTIVE_SENSITIVITY );
-
-
-            centroid_undeformed(0) += coord[0];
-            centroid_undeformed(1) += coord[1];
-            centroid_undeformed(2) += coord[2];
-
-            centroid_deformed(0) += coord[0] + def[0];
-            centroid_deformed(1) += coord[1] + def[1];
-            centroid_deformed(2) += coord[2] + def[2];
-        }
-        centroid_undeformed(0) = centroid_undeformed(0)/mNumberOfRigidNodes;
-        centroid_undeformed(1) = centroid_undeformed(1)/mNumberOfRigidNodes;
-        centroid_undeformed(2) = centroid_undeformed(2)/mNumberOfRigidNodes;
-        centroid_deformed(0) = centroid_deformed(0)/mNumberOfRigidNodes;
-        centroid_deformed(1) = centroid_deformed(1)/mNumberOfRigidNodes;
-        centroid_deformed(2) = centroid_deformed(2)/mNumberOfRigidNodes;
-
-        // H matrix (3x3)
-        Matrix H = ZeroMatrix(3,3);
-        for (int i=0;i<3;i++)
-        {
-            for(int j=0;j<3;j++)
+            // compute centroid deformed/undeformed
+            for( auto& node_i : mListOfRigidNodes )
             {
-                for(int node_index = 0 ; node_index<mListOfRigidNodes.size() ; node_index++)
-                {
-                    // Get node information
-                    ModelPart::NodeType& node_i = *mListOfRigidNodes[node_index];
+                Vector coord = node_i->Coordinates();
+                Vector def = node_i->FastGetSolutionStepValue( SHAPE_UPDATE );
 
-                    array_3d& coord = node_i.Coordinates();
-                    array_3d& def = node_i.FastGetSolutionStepValue( SHAPE_UPDATE );
-
-                    H(i,j) += (coord[i] - centroid_undeformed(i))*(coord[j] + def[j] - centroid_deformed(j));
-                }
+                centroid_undeformed += coord;
+                centroid_deformed += coord + def;
             }
+            centroid_undeformed = centroid_undeformed/mNumberOfRigidNodes;
+            centroid_deformed = centroid_deformed/mNumberOfRigidNodes;
+
+            // H matrix (3x3)
+            Matrix H = ZeroMatrix(3,3);
+
+            // assemble H-matrix
+            for( auto& node_i : mListOfRigidNodes )
+            {
+                Vector coord = node_i->Coordinates();
+                Vector def = node_i->FastGetSolutionStepValue( SHAPE_UPDATE );
+                
+                H += outer_prod( (coord - centroid_undeformed) , (coord + def - centroid_deformed) );
+            }
+
+            // U,V,S = svd(H)
+            Matrix U = ZeroMatrix(3,3);
+            Matrix V = ZeroMatrix(3,3);
+            Matrix S = ZeroMatrix(3,3);
+
+            SVDUtils<double>::JacobiSingularValueDecomposition(H, U, S, V);
+
+            // R
+            Matrix R = prod(trans(V),trans(U));
+            double detR = MathUtils<double>::Det(R);
+
+            if ( detR < 0 ) // special reflection case
+            {
+                R(0,0) *= -1;
+                R(0,1) *= -1;
+                R(0,2) *= -1;
+            }
+
+            // t
+            Vector t = ZeroVector(3);
+            t = - prod(R, centroid_undeformed) + centroid_deformed;
+
+            // compute rigid body movement of rigid nodes
+            int counter = 0;
+            for( auto& node_i : mListOfRigidNodes )
+            {
+                Vector coord = node_i->Coordinates();
+                Vector modifiedDeformation = prod(coord, R) - coord;
+
+                x_variables_in_geometry_space_rigid[counter] = modifiedDeformation(0);
+                y_variables_in_geometry_space_rigid[counter] = modifiedDeformation(1);
+                z_variables_in_geometry_space_rigid[counter] = modifiedDeformation(2);
+                
+                counter ++;
+            }
+            std::cout << "> Time needed for SVD: " << svd_time.elapsed() << " s" << std::endl;
         }
-        KRATOS_WATCH(H);
 
-        // U,V,S = svd(H)
-        Matrix U = ZeroMatrix(3,3);
-        Matrix V = ZeroMatrix(3,3);
-        Matrix S = ZeroMatrix(3,3);
-
-        SVDUtils<double>::JacobiSingularValueDecomposition(H, U, S, V);
-
-        KRATOS_WATCH(U);
-        KRATOS_WATCH(S);
-        KRATOS_WATCH(V);
-
-        // R
-        Matrix R = prod(trans(V),trans(U));
-        KRATOS_WATCH(R);
-
-        double detR = MathUtils<double>::Det(R);
-        KRATOS_WATCH(detR);
-
-        if ( detR < 0 ) // special reflection case
-        {
-            R(0,0) *= -1;
-            R(0,1) *= -1;
-            R(0,2) *= -1;
-        }
-
-        // t
-        Vector t = ZeroVector(3);
-        t = - prod(R, centroid_undeformed) + centroid_undeformed;
-        KRATOS_WATCH(t);
-
-
-        // From here we have R & t
-        KRATOS_WATCH(mListOfRigidNodes.size());
-        KRATOS_WATCH(mNumberOfRigidNodes);
-        KRATOS_WATCH(mNumberOfDesignVariables);
-
-        // compute rigid body movement of rigid nodes
-        for(int node_index = 0 ; node_index<mListOfRigidNodes.size() ; node_index++)
-        {
-            // Get node information
-            ModelPart::NodeType& node_i = *mListOfRigidNodes[node_index];
-            array_3d& coord = node_i.Coordinates();
-
-            x_variables_in_geometry_space_rigid[node_index] = (coord[0]*R(0,0) + coord[1]*R(0,1) + coord[2]*R(0,2) + t(0)) - coord[0];
-            y_variables_in_geometry_space_rigid[node_index] = (coord[0]*R(1,0) + coord[1]*R(1,1) + coord[2]*R(1,2) + t(1)) - coord[1];
-            z_variables_in_geometry_space_rigid[node_index] = (coord[0]*R(2,0) + coord[1]*R(2,1) + coord[2]*R(2,2) + t(2)) - coord[2];
-        }
+        boost::timer assembling_time;
+        std::cout << "\n> Starting to assemble modifiedMappingMatrix..." << std::endl;
 
         // compute modified mapping matrix
-        CompressedMatrix modifiedMatrix(mNumberOfDesignVariables+mNumberOfRigidNodes , mNumberOfDesignVariables );
+        CompressedMatrixType modifiedMappingMatrix( mNumberOfDesignVariables + mNumberOfRigidNodes , mNumberOfDesignVariables );
+
         Vector x_variables_modified, y_variables_modified, z_variables_modified;
         x_variables_modified.resize(mNumberOfDesignVariables + mNumberOfRigidNodes,0.0);
         y_variables_modified.resize(mNumberOfDesignVariables + mNumberOfRigidNodes,0.0);
         z_variables_modified.resize(mNumberOfDesignVariables + mNumberOfRigidNodes,0.0);
 
-        double penalty_factor = 10000;
+        double penalty_factor = 1000;
 
-
-
-        // // Copy from orignal mapping matrix & shape update
-        // for( int node_index_i = 0 ; node_index_i<mNumberOfDesignVariables ; node_index_i++)
-        // {
-        //     // modified matrix
-        //     for( int node_index_j = 0 ; node_index_j<mNumberOfDesignVariables ; node_index_j++)
-        //     {
-        //         double matrix_entry = mMappingMatrix(node_index_i,node_index_j);
-        //         if(matrix_entry!=0.0)
-        //             modifiedMatrix.insert_element(node_index_i, node_index_j, matrix_entry);
-
-        //     }
-        //     // modified vectors
-        //     x_variables_modified[node_index_i] = x_variables_in_geometry_space[node_index_i];
-        //     y_variables_modified[node_index_i] = y_variables_in_geometry_space[node_index_i];
-        //     z_variables_modified[node_index_i] = z_variables_in_geometry_space[node_index_i];
-        // }
-
-
-
-        // USing identity matrix for intial ds
-        for( int node_index_i = 0 ; node_index_i<mNumberOfDesignVariables ; node_index_i++)
+        // Using identity matrix for initial ds
+        for( int node_index_i = 0 ; node_index_i<mNumberOfDesignVariables ; node_index_i++ )
         {
-            modifiedMatrix.insert_element(node_index_i, node_index_i, 1.0);
+            modifiedMappingMatrix.insert_element(node_index_i, node_index_i, 1.0);
 
             // modified vectors
             x_variables_modified[node_index_i] = x_variables_in_design_space[node_index_i];
@@ -629,68 +586,68 @@ public:
         }
 
 
-
-
-        for( int node_index_i = 0 ; node_index_i<mNumberOfRigidNodes ; node_index_i++)
+        int counter = 0;
+        for( auto& node_i : mListOfRigidNodes )
         {
             // Get node information
-            ModelPart::NodeType& node_i = *mListOfRigidNodes[node_index_i];
-            int i = node_i.GetValue(MAPPING_ID);
+            int i = node_i->GetValue( MAPPING_ID );
 
-            // modified matrix
-            for( int node_index_j = 0 ; node_index_j<mNumberOfDesignVariables ; node_index_j++)
-            {
-                double matrix_entry = penalty_factor*mMappingMatrix(i,node_index_j);
-                if(matrix_entry!=0.0)
-                    modifiedMatrix.insert_element(mNumberOfDesignVariables + node_index_i, node_index_j, matrix_entry);
-            }
+            // Copy respective row from mMappingMatrix to modifiedMappingMatrix
+            row(modifiedMappingMatrix, mNumberOfDesignVariables + counter) = penalty_factor*row(mMappingMatrix,i);
 
             // modified vectors
-            x_variables_modified[node_index_i+mNumberOfDesignVariables] = penalty_factor*x_variables_in_geometry_space_rigid[node_index_i];
-            y_variables_modified[node_index_i+mNumberOfDesignVariables] = penalty_factor*y_variables_in_geometry_space_rigid[node_index_i];
-            z_variables_modified[node_index_i+mNumberOfDesignVariables] = penalty_factor*z_variables_in_geometry_space_rigid[node_index_i];
+            x_variables_modified[mNumberOfDesignVariables+counter] = penalty_factor*x_variables_in_geometry_space_rigid[counter];
+            y_variables_modified[mNumberOfDesignVariables+counter] = penalty_factor*y_variables_in_geometry_space_rigid[counter];
+            z_variables_modified[mNumberOfDesignVariables+counter] = penalty_factor*z_variables_in_geometry_space_rigid[counter];
+            
+            counter ++;
         }
+        std::cout << "> Time needed for assembling modifiedMappingMatrix: " << assembling_time.elapsed() << " s" << std::endl;
 
-        // // compute matrix
-        // CompressedMatrixType transA = trans(modifiedMatrix);
-        // CompressedMatrixType optimalMappingMatrix = prod(transA, modifiedMatrix);
+        // write mapping matrix
 
-        // // CompressedMatrixType optimalMappingMatrix;
-        // // CompressedSpaceType::TransposeMult(modifiedMatrix,modifiedMatrix,optimalMappingMatrix);
+        // writeMatrixToFile( modifiedMappingMatrix,
+        //                    mNumberOfDesignVariables+mNumberOfRigidNodes,
+        //                    mNumberOfDesignVariables,
+        //                    "modifiedMappingMatrix.txt");
 
-        // KRATOS_WATCH("test4")
+        // writeMatrixToFile( mMappingMatrix,
+        //                    mNumberOfDesignVariables,
+        //                    mNumberOfDesignVariables,
+        //                    "mappingMatrix.txt");
 
-
-        // // compute vectors
-        // Vector x_variables_RHS, y_variables_RHS, z_variables_RHS;
-        // x_variables_RHS.resize(mNumberOfDesignVariables);
-        // y_variables_RHS.resize(mNumberOfDesignVariables);
-        // z_variables_RHS.resize(mNumberOfDesignVariables);
-
-        // // CompressedSpaceType::TransposeMult(modifiedMatrix,x_variables_modified,x_variables_RHS);
-        // // CompressedSpaceType::TransposeMult(modifiedMatrix,y_variables_modified,y_variables_RHS);
-        // // CompressedSpaceType::TransposeMult(modifiedMatrix,z_variables_modified,z_variables_RHS);
-
-        // noalias(x_variables_RHS) = prod(transA , x_variables_modified);
-        // noalias(y_variables_RHS) = prod(transA , y_variables_modified);
-        // noalias(z_variables_RHS) = prod(transA , z_variables_modified);
-
-        // KRATOS_WATCH("test5")
-
-        // Vector x_variables_in_design_space;
-        // Vector y_variables_in_design_space;
-        // Vector z_variables_in_design_space;
+        boost::timer solving_time;
+        std::cout << "\n> Starting to solve modified System..." << std::endl;
 
         x_variables_in_design_space.resize(mNumberOfDesignVariables,0.0);
         y_variables_in_design_space.resize(mNumberOfDesignVariables,0.0);
         z_variables_in_design_space.resize(mNumberOfDesignVariables,0.0);
 
-        linear_solver->Solve(modifiedMatrix, x_variables_in_design_space, x_variables_modified);
-        linear_solver->Solve(modifiedMatrix, y_variables_in_design_space, y_variables_modified);
-        linear_solver->Solve(modifiedMatrix, z_variables_in_design_space, z_variables_modified);
+        linear_solver->Solve(modifiedMappingMatrix, x_variables_in_design_space, x_variables_modified);
+        linear_solver->Solve(modifiedMappingMatrix, y_variables_in_design_space, y_variables_modified);
+        linear_solver->Solve(modifiedMappingMatrix, z_variables_in_design_space, z_variables_modified);
 
-        KRATOS_WATCH("Solution done!")
+        std::cout << "> Time needed for solving modified System: " << solving_time.elapsed() << " s" << std::endl;
     }
+
+    void writeMatrixToFile( Matrix matrix, int nrow, int ncol, const char* filename )
+    {
+        std::ofstream file (filename);
+        if (file.is_open())
+        {
+            for (int i=0;i<nrow;i++)
+            {
+                for (int j=0;j<ncol;j++)
+                {
+                    double val = matrix(i,j);
+                    file << val << ", ";
+                }
+                file << "\n";
+            }
+            file.close();
+        }
+    }
+
 
     // ==============================================================================
 

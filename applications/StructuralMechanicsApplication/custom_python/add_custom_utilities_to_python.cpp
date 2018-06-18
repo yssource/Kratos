e3a350a7761aede66e4c9601157e5a6174645a2f
philbucher <philipp.bucher@tum.de>
2018-06-18 16:58:58 +0200

    exposing to Python

--- applications/StructuralMechanicsApplication/custom_python/add_custom_utilities_to_python.cpp
+++ applications/StructuralMechanicsApplication/custom_python/add_custom_utilities_to_python.cpp
@@ -22,6 +22,7 @@
 
 //Utilities
 #include "custom_utilities/formfinding_io_utility.h"
+#include "custom_utilities/assign_material_orientation_utility.h"
 
 
 namespace Kratos
@@ -40,9 +41,15 @@ void  AddCustomUtilitiesToPython(pybind11::module& m)
     .def("PrintPrestressData",&FormfindingIOUtility::PrintPrestressData )
     ;
 
+    class_<AssignMaterialOrientationUtility>(m,"AssignMaterialOrientationUtility")
+    .def(init<ModelPart&>())
+    .def("Execute",&AssignMaterialOrientationUtility::Execute)
+    .def("WriteFiberAngles",&AssignMaterialOrientationUtility::WriteFiberAngles )
+    ;
+
 }
 
-}  // namespace Python.  
+}  // namespace Python.
 
 } // Namespace Kratos
 

70c12c442dd2e6c930c14c6dd15b4fe63262e056
philbucher <philipp.bucher@tum.de>
2018-06-18 16:49:53 +0200

    Dummy souce file

---
+++ applications/StructuralMechanicsApplication/custom_utilities/assign_material_orientation_utility.cpp
@@ -0,0 +1,29 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    Peter Wilson
+//                   Philipp Bucher
+//
+
+
+// System includes
+
+
+// External includes
+
+
+// Project includes
+#include "assign_material_orientation_utility.h"
+
+namespace Kratos
+{
+
+}  // namespace Kratos.
+
+

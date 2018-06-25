1ad13a083e15ef933ac7ebb6adfd49e496eb91f1
philbucher <phibbi@gmx.net>
2018-06-25 17:41:20 +0200

    started with a new file to reimplement the functionalities

--- applications/StructuralMechanicsApplication/custom_utilities/assign_material_orientation_utility.cpp
+++ applications/StructuralMechanicsApplication/custom_utilities/assign_material_orientation_utility.cpp
@@ -20,9 +20,38 @@
 
 // Project includes
 #include "assign_material_orientation_utility.h"
+#include "structural_mechanics_application_variables.h"
 
 namespace Kratos
 {
+
+void AssignMaterialOrientationUtility::Execute(Parameters MethodParameters)
+{
+
+}
+
+void AssignMaterialOrientationUtility::WriteFiberAngles(const std::string& rFileName)
+{
+    std::ofstream element_data_file (rFileName);
+
+    if (element_data_file.is_open())
+    {
+        element_data_file << "Begin ElementalData MATERIAL_ORIENTATION_ANGLE" << std::endl;
+        for (auto& element : mrModelPart.Elements())
+        {
+            element_data_file << "\t" << element.Id()
+                                << " " << element.GetValue(MATERIAL_ORIENTATION_ANGLE)
+                                << std::endl;
+        }
+        element_data_file << "End ElementalData" << std::endl;
+
+        element_data_file.close();
+    }
+    else std::cout << "Unable to open file " << rFileName << std::endl;
+
+    std::cout << "Use \"cat " << rFileName << " >> your_mdpa_file.mdpa\" to append "
+                << "the MATERIAL_ORIENTATION_ANGLE values to the mdpa-file" << std::endl;
+}
 
 }  // namespace Kratos.
 

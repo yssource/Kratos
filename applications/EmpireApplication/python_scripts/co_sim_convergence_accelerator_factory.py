00883831cc72e8058ab125d2bb7bc43eb4176f1d
philbucher <philipp.bucher@tum.de>
2018-07-02 11:59:04 +0200

    ConvergenceAcceleratorFactory added

---
+++ applications/EmpireApplication/python_scripts/co_sim_convergence_accelerator_factory.py
@@ -0,0 +1,27 @@
+from __future__ import print_function, absolute_import, division  # makes KratosMultiphysics backward compatible with python 2.6 and 2.7
+
+available_convergence_accelerators = {
+    "constant" : "constant_convergence_accelerator",
+    "aitken"   : "aitken",
+    "iqnils"   : "iqnils",
+    "mvqn"     : "mvpn",
+}
+
+def CreateConvergenceAccelerator(convergence_accelerator_settings):
+    """This function creates and returns the convergence accelerator used for CoSimulation
+    New convergence accelerators have to be registered by adding them to "available_convergence_accelerators"
+    """
+    if (type(convergence_accelerator_settings) != dict):
+        raise Exception("Input is expected to be provided as a python dictionary")
+
+    accelerator_type = convergence_accelerator_settings["type"]
+
+    if accelerator_type in available_convergence_accelerators:
+        accelerator_module = __import__(available_convergence_accelerators[accelerator_type])
+        return accelerator_module.Create(convergence_accelerator_settings)
+    else:
+        err_msg  = 'The requested convergence accelerator "' + accelerator_type + '" is not available!\n'
+        err_msg += 'The following convergence accelerators are available:\n'
+        for avail_accelerator in available_convergence_accelerators:
+            err_msg += "\t" + avail_accelerator + "\n"
+        raise NameError(err_msg)

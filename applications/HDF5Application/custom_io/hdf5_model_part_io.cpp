a6d9469db2bd6469af2c145d898ab611ba9f9412
jcotela <jcotela@cimne.upc.edu>
2018-07-03 15:21:36 +0200

    hotfix to output submodelpart to hdf5.

--- applications/HDF5Application/custom_io/hdf5_model_part_io.cpp
+++ applications/HDF5Application/custom_io/hdf5_model_part_io.cpp
@@ -197,7 +197,14 @@ void ModelPartIO::WriteModelPart(ModelPart& rModelPart)
     BuiltinTimer timer;
     Internals::WriteVariablesList(*mpFile, mPrefix, rModelPart);
     Internals::WriteBufferSize(*mpFile, mPrefix, rModelPart.GetBufferSize());
-    WriteProperties(rModelPart.rProperties());
+    if (rModelPart.IsSubModelPart())
+    {
+        WriteProperties( rModelPart.GetParentModelPart()->rProperties());
+    }
+    else
+    {
+        WriteProperties(rModelPart.rProperties());
+    }
     Internals::WriteDataValueContainer(*mpFile, mPrefix + "/ProcessInfo", rModelPart.GetProcessInfo());
     rModelPart.Nodes().Sort(); // Avoid inadvertently reordering partway through
                                // the writing process.

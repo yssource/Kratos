7160d618126afe4d53a1c655649b15fcd397c5ff
philbucher <philipp.bucher@tum.de>
2018-07-23 09:34:03 +0200

    Adding test-case for Mok-FSI

--- applications/EmpireApplication/tests/co_simulation_test_factory.py
+++ applications/EmpireApplication/tests/co_simulation_test_factory.py
@@ -9,16 +9,16 @@ import os
 
 import co_simulation_test_case
 
-class TestSmallCoSimulationCases(KratosUnittest.TestCase):
+class TestSmallCoSimulationCases(co_simulation_test_case.CoSimulationTestCase):
     '''This class contains "small" CoSimulation-Cases, small enough to run in the nightly suite
     '''
     def test_MokFSI(self):
         with co_simulation_test_case.ControlledExecutionScope(os.path.dirname(os.path.realpath(__file__))):
-            # self.createTest('test_structural_mesh_motion_2d/rectangle_2D3N_test')
-            # self.runTest()
-            kratos_utils.DeleteFileIfExisting("./test_mdpa_files/rectangle_2D3N_test.time")
+            self.createTest('fsi_mok/cosim_mok_fsi')
+            self.runTest()
+            self._DeleteTimeFiles("fsi_mok")
 
-class TestCoSimulationCases(KratosUnittest.TestCase):
+class TestCoSimulationCases(co_simulation_test_case.CoSimulationTestCase):
     '''This class contains "full" CoSimulation-Cases, too large for the nightly suite and therefore
     have to be in the validation-suite
     '''

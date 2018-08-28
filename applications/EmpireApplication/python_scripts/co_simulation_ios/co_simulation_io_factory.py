5e2b36880a22b23fbb5d38952913e1bac722d0aa
philbucher <philipp.bucher@tum.de>
2018-08-28 11:25:35 +0200

    /solver-stage-fsi: Auto stash before merge of "empire/solver-stage-fsi" and "origin/empire/solver-stage-fsi"

--- applications/EmpireApplication/python_scripts/co_simulation_ios/co_simulation_io_factory.py
+++ applications/EmpireApplication/python_scripts/co_simulation_ios/co_simulation_io_factory.py
@@ -10,8 +10,14 @@ def CreateIO(io_name, solvers, solver_name, cosim_solver_details, level):
     """This function creates and returns the IO used for CoSimulation
     New IOs have to be registered by adding them to "available_ios"
     """
+
+    import os
+    from os import listdir
+    from os.path import isfile, join
+    onlyfiles = [print(f) for f in listdir(os.path.dirname(os.path.abspath(__file__))) if f.endswith("_io.py")]
+
     if io_name in available_ios:
-        io_module = __import__(available_ios[io_name])
+        io_module = __import__("available_ios[io_name]")
         return io_module.Create(solvers, solver_name, cosim_solver_details, level)
     else:
         err_msg  = 'The requested IO "' + io_name + '" is not available!\n'
--- applications/EmpireApplication/tests/co_simulation_test_factory.py
+++ applications/EmpireApplication/tests/co_simulation_test_factory.py
@@ -17,14 +17,14 @@ class TestSmallCoSimulationCases(co_simulation_test_case.CoSimulationTestCase):
             self.createTest("fsi_mok", "cosim_mok_fsi")
             self.runTest()
 
-class TestCoSimulationCases(co_simulation_test_case.CoSimulationTestCase):
-    '''This class contains "full" CoSimulation-Cases, too large for the nightly suite and therefore
-    have to be in the validation-suite
-    '''
-    def test_WallFSI(self):
-        with co_simulation_test_case.ControlledExecutionScope(os.path.dirname(os.path.realpath(__file__))):
-            self.createTest("fsi_wall", "cosim_wall_weak_coupling_fsi")
-            self.runTest()
+# class TestCoSimulationCases(co_simulation_test_case.CoSimulationTestCase):
+#     '''This class contains "full" CoSimulation-Cases, too large for the nightly suite and therefore
+#     have to be in the validation-suite
+#     '''
+#     def test_WallFSI(self):
+#         with co_simulation_test_case.ControlledExecutionScope(os.path.dirname(os.path.realpath(__file__))):
+#             self.createTest("fsi_wall", "cosim_wall_weak_coupling_fsi")
+#             self.runTest()
 
     def test_SDoFDragRectangleFSI(self):
         with co_simulation_test_case.ControlledExecutionScope(os.path.dirname(os.path.realpath(__file__))):

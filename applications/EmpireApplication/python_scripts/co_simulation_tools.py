56b4c6701cd5ab485507b7b31bf17485f993c97c
philbucher <philipp.bucher@tum.de>
2018-10-01 19:02:05 +0200

    added ValidateAndAssignDefaults to tools

--- applications/EmpireApplication/python_scripts/co_simulation_tools.py
+++ applications/EmpireApplication/python_scripts/co_simulation_tools.py
@@ -1,3 +1,5 @@
+import json
+
 def GetSolverCoSimulationDetails(co_simulation_solver_settings):
     num_solvers = len(co_simulation_solver_settings)
     solver_cosim_details = {}
@@ -32,26 +34,39 @@ def ExportArrayToSolver(solver, data_name, data_array, buffer_index=0):
     solver.ImportData(data_settings, solver)
 
 
+def ValidateAndAssignDefaults(defaults, settings, recursive=False):
+    for key, val in settings.items():
+        # check if the current entry also exists in the defaults
+        if not key in defaults.keys():
+            err_msg  = 'The item with name "' + key + '" is present in this '
+            err_msg += 'settings\nbut NOT in the defaults!\n'
+            err_msg += 'settings are:\n'
+            err_msg += json.dumps(settings, indent=4)
+            err_msg += '\ndefaults are:\n'
+            err_msg += json.dumps(defaults, indent=4)
+            raise Exception(err_msg)
 
-class CoSimulationParameters(object):
-    """Object to mimic Kratos::Parameters
-    It works with a python-dictionary, thus it does not
-    need many of the auxilliary methods
-    """
-    def __init__(self):
-        pass
+        # check if the type is the same in the defaults
+        if type(settings[key]) != type(defaults[key]):
+            err_msg  = 'The type of the item with name "' + key + '" (type: "'
+            err_msg += str(type(settings[key]).__name__)+'") in this '
+            err_msg += 'settings\nis NOT the same as in the defaults (type: "'
+            err_msg += str(type(defaults[key]).__name__)+'")!\n'
+            err_msg += 'settings are:\n'
+            err_msg += json.dumps(settings, indent=4)
+            err_msg += '\ndefaults are:\n'
+            err_msg += json.dumps(defaults, indent=4)
+            raise Exception(err_msg)
 
-    def ValidateAndAssignDefaults(self, defaults):
-        pass
+    # loop the defaults and add the missing entries
+    for key_d, val_d in defaults.items():
+        if key_d not in settings: # add the default in case the setting is not present
+            settings[key_d] = val_d
+        elif recursive and type(val_d) is dict:
+            RecursivelyValidateAndAssignDefaults(val_d, val)
 
-    def RecursivelyValidateAndAssignDefaults(self, defaults):
-        pass
-
-    def Merge(self):
-        """TODO: not really sure how to implement this, have to take a look at
-        the issue again
-        """
-        pass
+def RecursivelyValidateAndAssignDefaults(defaults, settings):
+    ValidateAndAssignDefaults(defaults, settings, recursive=True)
 
 
 class CoSimulationSpace(object):

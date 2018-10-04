247eaa1f597afd6a8c1e785cd329b821f119918f
philbucher <philipp.bucher@tum.de>
2018-10-04 21:11:18 +0200

    fixing commented tests

--- applications/EmpireApplication/tests/co_simulation_test_factory.py
+++ applications/EmpireApplication/tests/co_simulation_test_factory.py
@@ -9,27 +9,27 @@ import os
 
 import co_simulation_test_case
 
-# class TestSmallCoSimulationCases(co_simulation_test_case.CoSimulationTestCase):
-#     '''This class contains "small" CoSimulation-Cases, small enough to run in the nightly suite
-#     '''
-#     def test_MokFSI(self):
-#         with co_simulation_test_case.ControlledExecutionScope(os.path.dirname(os.path.realpath(__file__))):
-#             self.createTest("fsi_mok", "cosim_mok_fsi")
-#             self.runTest()
+class TestSmallCoSimulationCases(co_simulation_test_case.CoSimulationTestCase):
+    '''This class contains "small" CoSimulation-Cases, small enough to run in the nightly suite
+    '''
+    def test_MokFSI(self):
+        with co_simulation_test_case.ControlledExecutionScope(os.path.dirname(os.path.realpath(__file__))):
+            self.createTest("fsi_mok", "cosim_mok_fsi")
+            self.runTest()
 
 class TestCoSimulationCases(co_simulation_test_case.CoSimulationTestCase):
     '''This class contains "full" CoSimulation-Cases, too large for the nightly suite and therefore
     have to be in the validation-suite
     '''
-    # def test_WallFSI(self):
-    #     with co_simulation_test_case.ControlledExecutionScope(os.path.dirname(os.path.realpath(__file__))):
-    #         self.createTest("fsi_wall", "cosim_wall_weak_coupling_fsi")
-    #         self.runTest()
+    def test_WallFSI(self):
+        with co_simulation_test_case.ControlledExecutionScope(os.path.dirname(os.path.realpath(__file__))):
+            self.createTest("fsi_wall", "cosim_wall_weak_coupling_fsi")
+            self.runTest()
 
-    # def test_SDoFDragRectangleFSI(self):
-    #     with co_simulation_test_case.ControlledExecutionScope(os.path.dirname(os.path.realpath(__file__))):
-    #         self.createTest("fsi_sdof_drag_rectangle", "cosim_sdof_drag_rectangle_fsi")
-    #         self.runTest()
+    def test_SDoFDragRectangleFSI(self):
+        with co_simulation_test_case.ControlledExecutionScope(os.path.dirname(os.path.realpath(__file__))):
+            self.createTest("fsi_sdof_drag_rectangle", "cosim_sdof_drag_rectangle_fsi")
+            self.runTest()
 
     def test_MDoFDragPitchRectangleFSI(self):
         with co_simulation_test_case.ControlledExecutionScope(os.path.dirname(os.path.realpath(__file__))):

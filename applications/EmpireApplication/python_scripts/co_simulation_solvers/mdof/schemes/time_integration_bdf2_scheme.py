c47af8b3c365ff07c26e7d742a74e489aea42d3d
mpentek <mpentek@gmail.com>
2018-10-09 11:56:49 +0200

    Bugfix BDF2 (dummy)

--- applications/EmpireApplication/python_scripts/co_simulation_solvers/mdof/schemes/time_integration_bdf2_scheme.py
+++ applications/EmpireApplication/python_scripts/co_simulation_solvers/mdof/schemes/time_integration_bdf2_scheme.py
@@ -36,42 +36,17 @@ class TimeIntegrationBDF2Scheme(TimeIntegrationBaseScheme):
         # base scheme settings
         super(TimeIntegrationBDF2Scheme, self).__init__(scheme_settings)
 
-    def Initialize(self, model):
-        """
-        """
-        # initial displacement, velocity and acceleration
-        self.u0 = model.u0
-        self.v0 = model.v0
-        self.a0 = model.a0
-        # initial force
-        # PMT is this needed/correct like this?
-        self.force = model.f0
-
-        # initial displacement, velocity and acceleration
-        self.u1 = self.u0
-        self.v1 = self.v0
-        self.a1 = self.a0
-
     def _AssembleLHS(self, model):
         """
         """
-        return model.m
+        pass
 
     def _AssembleRHS(self, model):
         """
         """
-        RHS = self.force - np.dot(model.b, self.v0) - np.dot(model.k, self.u0)
-        # PMT might be a problem, as it seems to be solving for accelerations
-        #self.an0 =  np.linalg.solve(LHS, RHS)
-        # should be displacement based
-        # also check indexing u0 vs u1 which is new-old
-
-        # and here an update of self.f1
-        self.f1 = self.force
-        return RHS
+        pass
 
     def UpdateDerivedValues(self):
         """
         """
-        self.u0 = self.u1 + self.dt * self.v1
-        self.v0 = self.v1 + self.dt * self.a1
+        pass

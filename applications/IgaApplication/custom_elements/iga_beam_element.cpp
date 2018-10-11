d7411a23606e18df80f4d36a65b4ae254a12b18c
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2018-10-11 17:00:15 +0200

    Rename SetElementEquationId

--- applications/IgaApplication/custom_elements/iga_beam_element.cpp
+++ applications/IgaApplication/custom_elements/iga_beam_element.cpp
@@ -58,10 +58,10 @@ void IgaBeamElement::EquationIdVector(
     rResult.resize(NumberOfDofs());
 
     for (std::size_t i = 0; i < NumberOfNodes(); i++) {
-        SetEquationId(rResult, i, 0, DISPLACEMENT_X);
-        SetEquationId(rResult, i, 1, DISPLACEMENT_Y);
-        SetEquationId(rResult, i, 2, DISPLACEMENT_Z);
-        SetEquationId(rResult, i, 3, DISPLACEMENT_ROTATION);
+        SetElementEquationId(rResult, i, 0, DISPLACEMENT_X);
+        SetElementEquationId(rResult, i, 1, DISPLACEMENT_Y);
+        SetElementEquationId(rResult, i, 2, DISPLACEMENT_Z);
+        SetElementEquationId(rResult, i, 3, DISPLACEMENT_ROTATION);
     }
 
     KRATOS_CATCH("")

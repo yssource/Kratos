0ae95aae59af88165ef9b819cd83c450e10968f2
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2018-10-15 21:55:43 +0200

    Add CheckLowerMatrix

--- applications/IgaApplication/custom_elements/iga_debug.h
+++ applications/IgaApplication/custom_elements/iga_debug.h
@@ -119,6 +119,44 @@ struct IgaDebug
             }
         }
     }
+
+    template <typename TMatrix>
+    static void CheckLowerMatrix(
+        Parameters& ExpectedData,
+        const std::string& key,
+        const TMatrix& ActualValue
+    )
+    {
+        const auto expected_value = ExpectedData[key].GetMatrix();
+
+        if (expected_value.size1() != ActualValue.size1()) {
+            std::cout << "IgaDebug: " << key << ".size1()\n"
+                      << "  expected = " << expected_value.size1() << "\n"
+                      << "  actual   = " << ActualValue.size1() << std::endl;
+
+            throw std::runtime_error("IgaDebug: Size mismatch");
+        }
+
+        if (expected_value.size2() != ActualValue.size2()) {
+            std::cout << "IgaDebug: " << key << ".size2()\n"
+                      << "  expected = " << expected_value.size2() << "\n"
+                      << "  actual   = " << ActualValue.size2() << std::endl;
+
+            throw std::runtime_error("IgaDebug: Size mismatch");
+        }
+
+        for (std::size_t i = 0; i < expected_value.size1(); i++) {
+            for (std::size_t j = 0; j <= i; j++) {
+                if (!CheckWithAbsoluteTolerance(expected_value(i, j), ActualValue(i, j), 1e-7)) {
+                    std::cout << "IgaDebug: " << key << "(" << i << ", " << j << ")\n"
+                              << "  expected = " << expected_value(i, j) << "\n"
+                              << "  actual   = " << ActualValue(i, j) << std::endl;
+
+                    throw std::runtime_error("IgaDebug: Value mismatch");
+                }
+            }
+        }
+    }
 };
 
 

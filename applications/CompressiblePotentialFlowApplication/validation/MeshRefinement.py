0e27b38dcfa5fa119595447d81c6fa72676f664f
Inigo Lopez <inigolcanalejo@gmail.com>
2018-11-05 08:48:45 +0100

    setting all aoas

--- applications/CompressiblePotentialFlowApplication/validation/MeshRefinement.py
+++ applications/CompressiblePotentialFlowApplication/validation/MeshRefinement.py
@@ -29,7 +29,7 @@ Initial_FarField_MeshSize = TBD
 FarField_Refinement_Factor = TBD
 
 work_dir = '/home/inigo/simulations/naca0012/07_salome/05_MeshRefinement/'
-input_mdpa_path = work_dir + 'mdpas_aoa5/'
+input_mdpa_path = work_dir + 'mdpas_all_aoa/'
 output_gid_path = '/media/inigo/10740FB2740F9A1C/Outputs/03_MeshRefinement/'
 latex_output = open(work_dir + '/plots/latex_output.txt', 'w')
 latex_output.flush()
--- applications/CompressiblePotentialFlowApplication/validation/simulation.sh
+++ applications/CompressiblePotentialFlowApplication/validation/simulation.sh
@@ -12,16 +12,16 @@ GITBRANCH=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 Input_Dir=/home/inigo/simulations/naca0012/07_salome/05_MeshRefinement
 
 #Parameters
-Number_Of_Refinements=2
-Number_Of_AOAS=1
+Number_Of_Refinements=9
+Number_Of_AOAS=16
 
-Initial_AOA=5.0
+Initial_AOA=0.0
 AOA_Increment=1.0
 
 Initial_Airfoil_MeshSize=1.024e-2
 Airfoil_Refinement_Factor=2.0
 
-Initial_FarField_MeshSize=2.0
+Initial_FarField_MeshSize=0.5
 FarField_Refinement_Factor=1.0
 
 DATE=`date '+%Y%m%d_%H%M%S'`

4f445b1ae8fba0ee3dfd21ee0abab90d4b23cab2
Inigo Lopez <inigolcanalejo@gmail.com>
2018-11-12 10:20:48 +0100

    resetting paths

--- applications/CompressiblePotentialFlowApplication/validation/scripts/preamble.sh
+++ applications/CompressiblePotentialFlowApplication/validation/scripts/preamble.sh
@@ -1,7 +1,13 @@
+#Resetting paths
+export PYTHONPATH=""
+export LD_LIBRARY_PATH=""
+
+#Setting paths
 source /home/inigo/Documents/paths/salomeConverter.sh
 source /home/inigo/Documents/paths/kratosWake01.sh
 source /home/inigo/intel/mkl/bin/mklvars.sh intel64 lp64
 
+#Going to the current directory
 BASEDIR=$(dirname "$0")
 echo "BASEDIR"
 cd $BASEDIR
@@ -9,7 +15,7 @@ echo "The current working directory: $PWD"
 echo "The previous current working directory: $OPLDPWD"
 
 GITBRANCH=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
-Input_Dir=/home/inigo/simulations/naca0012/07_salome/05_MeshRefinement
+Input_Dir=/home/inigo/simulations/naca0012/07_salome/07_MeshRefinement
 
 DATE=`date '+%Y%m%d_%H%M%S'`
 FILE=${Input_Dir}/plots/output_terminal.txt

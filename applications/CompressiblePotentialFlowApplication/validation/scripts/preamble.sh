47240efbbc766e1240d6f6b39cbc0a0a7c0b07cc
Inigo Lopez <inigolcanalejo@gmail.com>
2018-11-12 13:56:30 +0100

    preparing for simulation

--- applications/CompressiblePotentialFlowApplication/validation/scripts/preamble.sh
+++ applications/CompressiblePotentialFlowApplication/validation/scripts/preamble.sh
@@ -7,6 +7,9 @@ source /home/inigo/Documents/paths/salomeConverter.sh
 source /home/inigo/Documents/paths/kratosWake01.sh
 source /home/inigo/intel/mkl/bin/mklvars.sh intel64 lp64
 
+echo "PYTHONPATH = $PYTHONPATH"
+echo "LD_LIBRARY_PATH = $LD_LIBRARY_PATH"
+
 GITBRANCH=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 Input_Dir=/home/inigo/simulations/naca0012/07_salome/07_MeshRefinement
 

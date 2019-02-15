d997e80d01998f976f37be89061656792e552a5b
sunethwarna <suneth.warna@gmail.com>
2019-02-15 13:29:26 +0100

    add rans process to python

---
+++ applications/RANSConstitutiveLawsApplication/custom_python/add_custom_processes_to_python.cpp
@@ -0,0 +1,45 @@
+// System includes
+
+#ifdef KRATOS_USE_AMATRIX
+#include "boost/numeric/ublas/matrix.hpp" // for the sparse space dense vector
+#endif // KRATOS_USE_AMATRIX
+
+// External includes
+#include "pybind11/pybind11.h"
+
+// Project includes
+#include "includes/model_part.h"
+#include "spaces/ublas_space.h"
+#include "linear_solvers/linear_solver.h"
+
+// Application includes
+#include "custom_processes/turbulence_eddy_viscosity_model_process.h"
+#include "custom_processes/turbulence_evm_k_epsilon_process.h"
+
+namespace Kratos {
+namespace Python {
+
+
+void AddCustomProcessesToPython(pybind11::module& m)
+{
+    namespace py = pybind11;
+
+    typedef UblasSpace<double, CompressedMatrix, Vector> SparseSpaceType;
+    typedef UblasSpace<double, Matrix, Vector> LocalSpaceType;
+    typedef LinearSolver<SparseSpaceType, LocalSpaceType > LinearSolverType;
+
+    typedef TurbulenceEvmKEpsilonProcess<2, SparseSpaceType,  LocalSpaceType, LinearSolverType> TurbulenceEvmKEpsilon2DProcess;
+    py::class_<TurbulenceEvmKEpsilon2DProcess, TurbulenceEvmKEpsilon2DProcess::Pointer, Process>(
+        m,"TurbulenceEvmKEpsilon2DProcess")
+        .def(py::init<ModelPart&, Parameters&, LinearSolverType::Pointer >())
+        ;
+
+    typedef TurbulenceEvmKEpsilonProcess<3, SparseSpaceType,  LocalSpaceType, LinearSolverType> TurbulenceEvmKEpsilon3DProcess;
+    py::class_<TurbulenceEvmKEpsilon3DProcess, TurbulenceEvmKEpsilon3DProcess::Pointer, Process>(
+        m,"TurbulenceEvmKEpsilon3DProcess")
+        .def(py::init<ModelPart&, Parameters&, LinearSolverType::Pointer >())
+        ;
+}
+
+} // namespace Python.
+} // Namespace Kratos

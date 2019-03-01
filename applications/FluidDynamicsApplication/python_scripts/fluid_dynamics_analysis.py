1a5cc1219e83d655e9bac5da6640b18cd09b8a5a
sunethwarna <suneth.warna@gmail.com>
2019-03-01 11:43:33 +0100

    adding a check for turbulent process

--- applications/FluidDynamicsApplication/python_scripts/fluid_dynamics_analysis.py
+++ applications/FluidDynamicsApplication/python_scripts/fluid_dynamics_analysis.py
@@ -39,9 +39,10 @@ class FluidDynamicsAnalysis(AnalysisStage):
         return python_solvers_wrapper_fluid.CreateSolver(self.model, self.project_parameters)
 
     def ModifyAfterSolverInitialize(self):
-        if self._GetSolver().turbulence_process is not None:
-            self._list_of_processes.append(self._GetSolver().turbulence_process)
-            self._GetSolver().turbulence_process.ExecuteInitialize()
+        if ("turbulence_process" in dir(self._GetSolver())):
+            if self._GetSolver().turbulence_process is not None:
+                self._list_of_processes.append(self._GetSolver().turbulence_process)
+                self._GetSolver().turbulence_process.ExecuteInitialize()
 
     def _CreateProcesses(self, parameter_name, initialization_order):
         """Create a list of Processes

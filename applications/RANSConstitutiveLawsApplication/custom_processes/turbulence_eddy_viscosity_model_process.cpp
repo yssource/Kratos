5e529689c53ea5d78932a7d855afdef4b81d96a0
sunethwarna <suneth.warna@gmail.com>
2019-03-07 10:52:05 +0100

    refactoring evm base process

--- applications/RANSConstitutiveLawsApplication/custom_processes/turbulence_eddy_viscosity_model_process.cpp
+++ applications/RANSConstitutiveLawsApplication/custom_processes/turbulence_eddy_viscosity_model_process.cpp
@@ -126,7 +126,8 @@ void TurbulenceEddyViscosityModelProcess<TDim, TSparseSpace, TDenseSpace, TLinea
     mpDistanceCalculator->Execute();
 
     if (mEchoLevel > 0)
-        KRATOS_INFO("TurbulenceModel") << "Finished calculating wall distances." << std::endl;
+        KRATOS_INFO("TurbulenceModel")
+            << "Finished calculating wall distances." << std::endl;
 
     KRATOS_CATCH("");
 }
@@ -230,6 +231,289 @@ void TurbulenceEddyViscosityModelProcess<TDim, TSparseSpace, TDenseSpace, TLinea
     //     KRATOS_WATCH(cond);
 
     // KRATOS_CATCH("");
+}
+
+template <unsigned int TDim, class TSparseSpace, class TDenseSpace, class TLinearSolver>
+void TurbulenceEddyViscosityModelProcess<TDim, TSparseSpace, TDenseSpace, TLinearSolver>::CalculateYplus(unsigned int Step)
+{
+    int number_of_nodes = mrModelPart.NumberOfNodes();
+    const ProcessInfo& r_current_process_info = mrModelPart.GetProcessInfo();
+
+    const double von_karman = r_current_process_info[WALL_VON_KARMAN];
+    const double beta = r_current_process_info[WALL_SMOOTHNESS_BETA];
+
+#pragma omp parallel for
+    for (int i = 0; i < number_of_nodes; ++i)
+    {
+        Node<3>& r_node = *(mrModelPart.NodesBegin() + i);
+
+        const array_1d<double, 3>& r_velocity =
+            r_node.FastGetSolutionStepValue(VELOCITY, Step);
+        const double velocity_norm = norm_2(r_velocity);
+
+        const double nu = r_node.FastGetSolutionStepValue(KINEMATIC_VISCOSITY);
+        const double wall_distance = r_node.FastGetSolutionStepValue(DISTANCE);
+
+        double& y_plus = r_node.FastGetSolutionStepValue(RANS_Y_PLUS);
+
+        y_plus = CalculationUtilities::CalculateYplus(
+            velocity_norm, wall_distance, nu, von_karman, beta, 10);
+    }
+}
+
+template <unsigned int TDim, class TSparseSpace, class TDenseSpace, class TLinearSolver>
+void TurbulenceEddyViscosityModelProcess<TDim, TSparseSpace, TDenseSpace, TLinearSolver>::FindConditionGaussPointIndices(
+    ModelPart* pModelPart)
+{
+    const int number_of_conditions = pModelPart->NumberOfConditions();
+
+#pragma omp parallel for
+    for (int i_cond = 0; i_cond < number_of_conditions; ++i_cond)
+    {
+        Condition& r_condition = *(pModelPart->ConditionsBegin() + i_cond);
+        Geometry<Node<3>>& r_condition_geometry = r_condition.GetGeometry();
+        const Element& r_element = *(r_condition.GetValue(PARENT_ELEMENT).lock());
+        const Geometry<Node<3>>& r_element_geometry = r_element.GetGeometry();
+
+        Vector gauss_weights;
+        Matrix shape_functions;
+        Geometry<Node<3>>::ShapeFunctionsGradientsType shape_derivatives;
+
+        CalculationUtilities::CalculateGeometryData(
+            r_element_geometry, r_element.GetIntegrationMethod(), gauss_weights,
+            shape_functions, shape_derivatives);
+
+        const unsigned int num_gauss_points = gauss_weights.size();
+        const unsigned int number_of_element_nodes = r_element_geometry.PointsNumber();
+        const unsigned int number_of_condition_nodes =
+            r_condition_geometry.PointsNumber();
+
+        std::vector<int>& r_gauss_point_indices = r_condition.GetValue(GAUSS_POINT_INDICES);
+        r_gauss_point_indices.clear();
+        r_gauss_point_indices.resize(number_of_condition_nodes);
+
+        for (unsigned int i_cond_node = 0; i_cond_node < number_of_condition_nodes; ++i_cond_node)
+        {
+            int g_index = -1;
+            double min_distance = 0.0;
+            for (unsigned int g = 0; g < num_gauss_points; g++)
+            {
+                const Vector& gauss_shape_functions = row(shape_functions, g);
+
+                double gp_x(0.0), gp_y(0.0), gp_z(0.0);
+
+                for (unsigned int i_node = 0; i_node < number_of_element_nodes; ++i_node)
+                {
+                    gp_x += gauss_shape_functions[i_node] *
+                            r_element_geometry[i_node].X();
+                    gp_y += gauss_shape_functions[i_node] *
+                            r_element_geometry[i_node].Y();
+                    gp_z += gauss_shape_functions[i_node] *
+                            r_element_geometry[i_node].Z();
+                }
+
+                double distance = 0.0;
+                distance += std::pow(gp_x - r_condition_geometry[i_cond_node].X(), 2);
+                distance += std::pow(gp_y - r_condition_geometry[i_cond_node].Y(), 2);
+                distance += std::pow(gp_z - r_condition_geometry[i_cond_node].Z(), 2);
+                distance = std::sqrt(distance);
+
+                if (g_index == -1)
+                {
+                    g_index = 0;
+                    min_distance = distance;
+                }
+
+                if (min_distance > distance)
+                {
+                    min_distance = distance;
+                    g_index = g;
+                }
+            }
+            r_gauss_point_indices[i_cond_node] = g_index;
+        }
+    }
+}
+
+template <unsigned int TDim, class TSparseSpace, class TDenseSpace, class TLinearSolver>
+void TurbulenceEddyViscosityModelProcess<TDim, TSparseSpace, TDenseSpace, TLinearSolver>::FindConditionsParentElements(
+    ModelPart* pModelPart)
+{
+    FindNodalNeighboursProcess find_nodal_neighbours_process(*pModelPart);
+    find_nodal_neighbours_process.Execute();
+
+    const int number_of_conditions = pModelPart->NumberOfConditions();
+
+#pragma omp parallel for
+    for (int i_cond = 0; i_cond < number_of_conditions; ++i_cond)
+    {
+        Condition& r_condition = *(pModelPart->ConditionsBegin() + i_cond);
+
+        Geometry<Node<3>>& r_geometry = r_condition.GetGeometry();
+        WeakPointerVector<Element> element_candidates;
+
+        const IndexType number_of_nodes = r_geometry.PointsNumber();
+
+        for (IndexType i_node = 0; i_node < number_of_nodes; ++i_node)
+        {
+            WeakPointerVector<Element>& r_node_element_candidates =
+                r_geometry[i_node].GetValue(NEIGHBOUR_ELEMENTS);
+            for (IndexType i_elem = 0; i_elem < r_node_element_candidates.size(); ++i_elem)
+                element_candidates.push_back(r_node_element_candidates(i_elem));
+        }
+
+        std::vector<IndexType> node_ids(number_of_nodes);
+        for (IndexType i_node = 0; i_node < number_of_nodes; ++i_node)
+            node_ids[i_node] = r_geometry[i_node].Id();
+
+        std::sort(node_ids.begin(), node_ids.end());
+
+        std::vector<IndexType> element_node_ids;
+        for (IndexType i_elem = 0; i_elem < element_candidates.size(); ++i_elem)
+        {
+            Geometry<Node<3>>& r_element_geometry =
+                element_candidates[i_elem].GetGeometry();
+            const IndexType number_of_element_nodes = r_element_geometry.PointsNumber();
+            element_node_ids.resize(number_of_element_nodes);
+
+            for (IndexType i_node = 0; i_node < number_of_element_nodes; ++i_node)
+                element_node_ids[i_node] = r_element_geometry[i_node].Id();
+
+            std::sort(element_node_ids.begin(), element_node_ids.end());
+
+            // If all the node in the condition is included in the element, then it is the parent element for that condition
+            if (std::includes(element_node_ids.begin(), element_node_ids.end(),
+                              node_ids.begin(), node_ids.end()))
+            {
+                r_condition.SetValue(PARENT_ELEMENT, element_candidates(i_elem));
+                break;
+            }
+        }
+    }
+}
+
+template <unsigned int TDim, class TSparseSpace, class TDenseSpace, class TLinearSolver>
+void TurbulenceEddyViscosityModelProcess<TDim, TSparseSpace, TDenseSpace, TLinearSolver>::InitializeConditionFlagsForModelPart(
+    ModelPart* pModelPart, const Flags& rFlag)
+{
+    KRATOS_TRY
+
+    auto& conditions_array = pModelPart->Conditions();
+
+    for (auto& it_cond : conditions_array)
+    {
+        bool is_flag_true = true;
+
+        auto& r_geometry = it_cond.GetGeometry();
+
+        for (auto& it_node : r_geometry.Points())
+            if (!(it_node.Is(rFlag)))
+                is_flag_true = false;
+        it_cond.Set(rFlag, is_flag_true);
+    }
+
+    KRATOS_CATCH("");
+}
+
+template <unsigned int TDim, class TSparseSpace, class TDenseSpace, class TLinearSolver>
+void TurbulenceEddyViscosityModelProcess<TDim, TSparseSpace, TDenseSpace, TLinearSolver>::InitializeConditionsForModelPart(
+    ModelPart* pModelPart)
+{
+    this->InitializeConditionFlagsForModelPart(pModelPart, INLET);
+    this->InitializeConditionFlagsForModelPart(pModelPart, OUTLET);
+    this->InitializeConditionFlagsForModelPart(pModelPart, STRUCTURE);
+
+    this->FindConditionsParentElements(pModelPart);
+    this->FindConditionGaussPointIndices(pModelPart);
+}
+
+template <unsigned int TDim, class TSparseSpace, class TDenseSpace, class TLinearSolver>
+void TurbulenceEddyViscosityModelProcess<TDim, TSparseSpace, TDenseSpace, TLinearSolver>::AddSolutionStepVariables()
+{
+    KRATOS_TRY
+
+    mrModelPart.GetNodalSolutionStepVariablesList().push_back(DISTANCE);
+    mrModelPart.GetNodalSolutionStepVariablesList().push_back(FLAG_VARIABLE);
+    mrModelPart.GetNodalSolutionStepVariablesList().push_back(KINEMATIC_VISCOSITY);
+    mrModelPart.GetNodalSolutionStepVariablesList().push_back(TURBULENT_VISCOSITY);
+
+    KRATOS_INFO("TurbulenceModel") << "Added eddy viscosity turbulence "
+                                      "model solution step variables.\n";
+
+    KRATOS_CATCH("");
+}
+
+template <unsigned int TDim, class TSparseSpace, class TDenseSpace, class TLinearSolver>
+void TurbulenceEddyViscosityModelProcess<TDim, TSparseSpace, TDenseSpace, TLinearSolver>::AddDofs()
+{
+    KRATOS_INFO("TurbulenceModel")
+        << "Added eddy viscosity turbulence model dofs.\n";
+}
+
+template <unsigned int TDim, class TSparseSpace, class TDenseSpace, class TLinearSolver>
+void TurbulenceEddyViscosityModelProcess<TDim, TSparseSpace, TDenseSpace, TLinearSolver>::InitializeTurbulenceModelPart()
+{
+    KRATOS_THROW_ERROR(std::runtime_error,
+                       "Calling base class InitializeTurbulenceModelPart.", "");
+}
+
+template <unsigned int TDim, class TSparseSpace, class TDenseSpace, class TLinearSolver>
+void TurbulenceEddyViscosityModelProcess<TDim, TSparseSpace, TDenseSpace, TLinearSolver>::UpdateFluidViscosity()
+{
+    KRATOS_TRY
+
+    NodesArrayType& nodes = mrModelPart.Nodes();
+
+// Modifying viscosity of the nodes with the calculated turbulent viscosity
+#pragma omp parallel for
+    for (int i = 0; i < static_cast<int>(nodes.size()); ++i)
+    {
+        auto it_node = nodes.begin() + i;
+        const double kinematic_viscosity =
+            it_node->FastGetSolutionStepValue(KINEMATIC_VISCOSITY);
+        const double turbulent_viscosity =
+            it_node->FastGetSolutionStepValue(TURBULENT_VISCOSITY);
+
+        double& effective_viscosity = it_node->FastGetSolutionStepValue(VISCOSITY);
+        effective_viscosity = kinematic_viscosity + turbulent_viscosity;
+    }
+
+    KRATOS_CATCH("");
+}
+
+template <unsigned int TDim, class TSparseSpace, class TDenseSpace, class TLinearSolver>
+void TurbulenceEddyViscosityModelProcess<TDim, TSparseSpace, TDenseSpace, TLinearSolver>::InitializeConditions()
+{
+    KRATOS_THROW_ERROR(std::runtime_error,
+                       "Calling base class InitializeConditions.", "");
+}
+
+/* Protected functions ****************************************************/
+template <unsigned int TDim, class TSparseSpace, class TDenseSpace, class TLinearSolver>
+void TurbulenceEddyViscosityModelProcess<TDim, TSparseSpace, TDenseSpace, TLinearSolver>::InitializeNodeFlags(
+    const Parameters& rParameters, const Flags& rFlag)
+{
+    KRATOS_TRY
+
+    for (std::size_t i = 0; i < rParameters.size(); ++i)
+    {
+        std::string model_part_name = rParameters.GetArrayItem(i).GetString();
+        KRATOS_ERROR_IF(!mrModelPart.HasSubModelPart(model_part_name))
+            << "TurbulenceEddyViscosityModelProcess: Wall condition "
+            << model_part_name << " not found." << std::endl;
+        ModelPart& current_model_part = mrModelPart.GetSubModelPart(model_part_name);
+
+        NodesArrayType& nodes_array = current_model_part.Nodes();
+
+#pragma omp parallel for
+        for (int i = 0; i < static_cast<int>(nodes_array.size()); ++i)
+        {
+            auto it_node = nodes_array.begin() + i;
+            it_node->Set(rFlag, true);
+        }
+    }
+
+    KRATOS_CATCH("");
 }
 
 /* External functions *****************************************************/
--- applications/RANSConstitutiveLawsApplication/custom_processes/turbulence_eddy_viscosity_model_process.h
+++ applications/RANSConstitutiveLawsApplication/custom_processes/turbulence_eddy_viscosity_model_process.h
@@ -19,16 +19,16 @@
 // External includes
 
 // Project includes
-#include "custom_elements/evm_k_epsilon/evm_k_epsilon_utilities.h"
-#include "custom_utilities/geometry_utilities.h"
 #include "includes/cfd_variables.h"
 #include "includes/define.h"
 #include "includes/model_part.h"
 #include "processes/find_nodal_neighbours_process.h"
 #include "processes/process.h"
 #include "processes/variational_distance_calculation_process.h"
-#include "utilities/normal_calculation_utils.h"
 #include "rans_constitutive_laws_application_variables.h"
+#include "utilities/normal_calculation_utils.h"
+
+#include "custom_utilities/calculation_utilities.h"
 
 namespace Kratos
 {
@@ -160,248 +160,30 @@ protected:
     ///@name Protected Operations
     ///@{
 
-    virtual void AddSolutionStepVariables()
-    {
-        KRATOS_TRY
+    virtual void AddSolutionStepVariables();
 
-        mrModelPart.GetNodalSolutionStepVariablesList().push_back(DISTANCE);
-        mrModelPart.GetNodalSolutionStepVariablesList().push_back(FLAG_VARIABLE);
-        mrModelPart.GetNodalSolutionStepVariablesList().push_back(KINEMATIC_VISCOSITY);
-        mrModelPart.GetNodalSolutionStepVariablesList().push_back(TURBULENT_VISCOSITY);
+    virtual void AddDofs();
 
-        KRATOS_INFO("TurbulenceModel") << "Added eddy viscosity turbulence "
-                                          "model solution step variables.\n";
+    virtual void InitializeTurbulenceModelPart();
 
-        KRATOS_CATCH("");
-    }
+    virtual void UpdateFluidViscosity();
 
-    virtual void AddDofs()
-    {
-        KRATOS_INFO("TurbulenceModel")
-            << "Added eddy viscosity turbulence model dofs.\n";
-    }
+    virtual void InitializeConditions();
 
-    virtual void InitializeTurbulenceModelPart()
-    {
-        KRATOS_THROW_ERROR(std::runtime_error,
-                           "Calling base class InitializeTurbulenceModelPart.",
-                           "");
-    }
+    void InitializeConditionsForModelPart(ModelPart* pModelPart);
 
-    void InitializeConditionFlagsForModelPart(ModelPart* pModelPart, const Flags& rFlag)
-    {
-        KRATOS_TRY
+    void InitializeConditionFlagsForModelPart(ModelPart* pModelPart, const Flags& rFlag);
 
-        auto& conditions_array = pModelPart->Conditions();
+    void FindConditionsParentElements(ModelPart* pModelPart);
 
-        for (auto& it_cond : conditions_array)
-        {
-            bool is_flag_true = true;
-
-            auto& r_geometry = it_cond.GetGeometry();
-
-            for (auto& it_node : r_geometry.Points())
-                if (!(it_node.Is(rFlag)))
-                    is_flag_true = false;
-            it_cond.Set(rFlag, is_flag_true);
-        }
-
-        KRATOS_CATCH("");
-    }
-
-    void FindConditionsParentElements(ModelPart* pModelPart)
-    {
-        FindNodalNeighboursProcess find_nodal_neighbours_process(*pModelPart);
-        find_nodal_neighbours_process.Execute();
-
-        const int number_of_conditions = pModelPart->NumberOfConditions();
-
-#pragma omp parallel for
-        for (int i_cond = 0; i_cond < number_of_conditions; ++i_cond)
-        {
-            Condition& r_condition = *(pModelPart->ConditionsBegin() + i_cond);
-
-            Geometry<Node<3>>& r_geometry = r_condition.GetGeometry();
-            WeakPointerVector<Element> element_candidates;
-
-            const IndexType number_of_nodes = r_geometry.PointsNumber();
-
-            for (IndexType i_node = 0; i_node < number_of_nodes; ++i_node)
-            {
-                WeakPointerVector<Element>& r_node_element_candidates =
-                    r_geometry[i_node].GetValue(NEIGHBOUR_ELEMENTS);
-                for (IndexType i_elem = 0; i_elem < r_node_element_candidates.size(); ++i_elem)
-                    element_candidates.push_back(r_node_element_candidates(i_elem));
-            }
-
-            std::vector<IndexType> node_ids(number_of_nodes);
-            for (IndexType i_node = 0; i_node < number_of_nodes; ++i_node)
-                node_ids[i_node] = r_geometry[i_node].Id();
-
-            std::sort(node_ids.begin(), node_ids.end());
-
-            std::vector<IndexType> element_node_ids;
-            for (IndexType i_elem = 0; i_elem < element_candidates.size(); ++i_elem)
-            {
-                Geometry<Node<3>>& r_element_geometry =
-                    element_candidates[i_elem].GetGeometry();
-                const IndexType number_of_element_nodes =
-                    r_element_geometry.PointsNumber();
-                element_node_ids.resize(number_of_element_nodes);
-
-                for (IndexType i_node = 0; i_node < number_of_element_nodes; ++i_node)
-                    element_node_ids[i_node] = r_element_geometry[i_node].Id();
-
-                std::sort(element_node_ids.begin(), element_node_ids.end());
-
-                // If all the node in the condition is included in the element, then it is the parent element for that condition
-                if (std::includes(element_node_ids.begin(), element_node_ids.end(),
-                                  node_ids.begin(), node_ids.end()))
-                {
-                    r_condition.SetValue(PARENT_ELEMENT, element_candidates(i_elem));
-                    break;
-                }
-            }
-        }
-    }
-
-    void FindConditionGaussPointIndices(ModelPart* pModelPart)
-    {
-        const int number_of_conditions = pModelPart->NumberOfConditions();
-
-#pragma omp parallel for
-        for (int i_cond = 0; i_cond < number_of_conditions; ++i_cond)
-        {
-            Condition& r_condition = *(pModelPart->ConditionsBegin() + i_cond);
-            Geometry<Node<3>>& r_condition_geometry = r_condition.GetGeometry();
-            const Element& r_element = *(r_condition.GetValue(PARENT_ELEMENT).lock());
-            const Geometry<Node<3>>& r_element_geometry = r_element.GetGeometry();
-
-            Vector gauss_weights;
-            Matrix shape_functions;
-            Geometry<Node<3>>::ShapeFunctionsGradientsType shape_derivatives;
-
-            CalculateGeometryData(r_element_geometry, r_element.GetIntegrationMethod(),
-                                  gauss_weights, shape_functions, shape_derivatives);
-
-            const unsigned int num_gauss_points = gauss_weights.size();
-            const unsigned int number_of_element_nodes =
-                r_element_geometry.PointsNumber();
-            const unsigned int number_of_condition_nodes =
-                r_condition_geometry.PointsNumber();
-
-            std::vector<int>& r_gauss_point_indices =
-                r_condition.GetValue(GAUSS_POINT_INDICES);
-            r_gauss_point_indices.clear();
-            r_gauss_point_indices.resize(number_of_condition_nodes);
-
-            for (unsigned int i_cond_node = 0;
-                 i_cond_node < number_of_condition_nodes; ++i_cond_node)
-            {
-                int g_index = -1;
-                double min_distance = 0.0;
-                for (unsigned int g = 0; g < num_gauss_points; g++)
-                {
-                    const Vector& gauss_shape_functions = row(shape_functions, g);
-
-                    double gp_x(0.0), gp_y(0.0), gp_z(0.0);
-
-                    for (unsigned int i_node = 0; i_node < number_of_element_nodes; ++i_node)
-                    {
-                        gp_x += gauss_shape_functions[i_node] *
-                                r_element_geometry[i_node].X();
-                        gp_y += gauss_shape_functions[i_node] *
-                                r_element_geometry[i_node].Y();
-                        gp_z += gauss_shape_functions[i_node] *
-                                r_element_geometry[i_node].Z();
-                    }
-
-                    double distance = 0.0;
-                    distance +=
-                        std::pow(gp_x - r_condition_geometry[i_cond_node].X(), 2);
-                    distance +=
-                        std::pow(gp_y - r_condition_geometry[i_cond_node].Y(), 2);
-                    distance +=
-                        std::pow(gp_z - r_condition_geometry[i_cond_node].Z(), 2);
-                    distance = std::sqrt(distance);
-
-                    if (g_index == -1)
-                    {
-                        g_index = 0;
-                        min_distance = distance;
-                    }
-
-                    if (min_distance > distance)
-                    {
-                        min_distance = distance;
-                        g_index = g;
-                    }
-                }
-                r_gauss_point_indices[i_cond_node] = g_index;
-            }
-        }
-    }
-
-    virtual void UpdateFluidViscosity()
-    {
-        KRATOS_TRY
-
-        NodesArrayType& nodes = mrModelPart.Nodes();
-
-// Modifying viscosity of the nodes with the calculated turbulent viscosity
-#pragma omp parallel for
-        for (int i = 0; i < static_cast<int>(nodes.size()); ++i)
-        {
-            auto it_node = nodes.begin() + i;
-            const double kinematic_viscosity =
-                it_node->FastGetSolutionStepValue(KINEMATIC_VISCOSITY);
-            const double turbulent_viscosity =
-                it_node->FastGetSolutionStepValue(TURBULENT_VISCOSITY);
-
-            double& effective_viscosity = it_node->FastGetSolutionStepValue(VISCOSITY);
-            effective_viscosity = kinematic_viscosity + turbulent_viscosity;
-        }
-
-        KRATOS_CATCH("");
-    }
-
-    virtual void InitializeConditions()
-    {
-        KRATOS_THROW_ERROR(std::runtime_error,
-                           "Calling base class InitializeConditions.", "");
-    }
+    void FindConditionGaussPointIndices(ModelPart* pModelPart);
 
     void GenerateModelPart(ModelPart& rOriginModelPart,
                            ModelPart& rDestinationModelPart,
                            const Element& rReferenceElement,
                            const Condition& rReferenceCondition);
 
-    void CalculateYplus(unsigned int Step = 0)
-    {
-        int number_of_nodes = mrModelPart.NumberOfNodes();
-        const ProcessInfo& r_current_process_info = mrModelPart.GetProcessInfo();
-
-        const double von_karman = r_current_process_info[WALL_VON_KARMAN];
-        const double beta = r_current_process_info[WALL_SMOOTHNESS_BETA];
-
-#pragma omp parallel for
-        for (int i = 0; i < number_of_nodes; ++i)
-        {
-            Node<3>& r_node = *(mrModelPart.NodesBegin() + i);
-
-            const array_1d<double, 3>& r_velocity =
-                r_node.FastGetSolutionStepValue(VELOCITY, Step);
-            const double velocity_norm = norm_2(r_velocity);
-
-            const double nu = r_node.FastGetSolutionStepValue(KINEMATIC_VISCOSITY);
-            const double wall_distance = r_node.FastGetSolutionStepValue(DISTANCE);
-
-            double& y_plus = r_node.FastGetSolutionStepValue(RANS_Y_PLUS);
-
-            y_plus = EvmKepsilonModelUtilities::CalculateYplus(
-                velocity_norm, wall_distance, nu, von_karman, beta, 10);
-        }
-    }
+    void CalculateYplus(unsigned int Step = 0);
 
     ///@}
     ///@name Protected  Access
@@ -437,30 +219,7 @@ private:
 
     void CalculateWallDistances();
 
-    void InitializeNodeFlags(const Parameters& rParameters, const Flags& rFlag)
-    {
-        KRATOS_TRY
-
-        for (std::size_t i = 0; i < rParameters.size(); ++i)
-        {
-            std::string model_part_name = rParameters.GetArrayItem(i).GetString();
-            KRATOS_ERROR_IF(!mrModelPart.HasSubModelPart(model_part_name))
-                << "TurbulenceEddyViscosityModelProcess: Wall condition "
-                << model_part_name << " not found." << std::endl;
-            ModelPart& current_model_part = mrModelPart.GetSubModelPart(model_part_name);
-
-            NodesArrayType& nodes_array = current_model_part.Nodes();
-
-#pragma omp parallel for
-            for (int i = 0; i < static_cast<int>(nodes_array.size()); ++i)
-            {
-                auto it_node = nodes_array.begin() + i;
-                it_node->Set(rFlag, true);
-            }
-        }
-
-        KRATOS_CATCH("");
-    }
+    void InitializeNodeFlags(const Parameters& rParameters, const Flags& rFlag);
 
     // void AssignBoundaryConditions();
 

f9fba632d0a513e9faa52c22a167c09830c4edae
marcnunezc <marcnc94@gmail.com>
2019-03-07 17:07:00 +0100

    Uncommenting figure plotting

--- applications/CompressiblePotentialFlowApplication/python_scripts/compute_lift_level_set_process.py
+++ applications/CompressiblePotentialFlowApplication/python_scripts/compute_lift_level_set_process.py
@@ -33,40 +33,47 @@ class ComputeLiftProcess(KratosMultiphysics.Process):
     def ExecuteFinalizeSolutionStep(self):
         print("wip_compute_lift_level_set_process")
         self.process.Execute()
-        # x_upper=[]
-        # cp_upper=[]
-        # x_lower=[]
-        # cp_lower=[]
-        # for element in self.fluid_model_part.Elements:
-        #     if element.Is(KratosMultiphysics.BOUNDARY) and element.IsNot(KratosMultiphysics.INTERFACE):
-        #         gp=element.GetValue(KratosMultiphysics.BODY_FORCE)#provisional name
-        #         pressure=element.GetValue(KratosMultiphysics.PRESSURE)
-        #         normal=element.GetValue(KratosMultiphysics.NORMAL)
-        #         if normal[1]<=0:
-        #             x_upper.append(gp[0])
-        #             cp_upper.append(pressure)
-        #         else:
-        #             x_lower.append(gp[0])
-        #             cp_lower.append(pressure)
-        # max_x=max(max(x_upper),max(x_lower))
-        # min_x=min(min(x_upper),min(x_lower))
-        # for i in range(0,len(x_upper)):
-        #     x_upper[i]=(x_upper[i]-min_x)/abs(max_x-min_x)
-        # for i in range(0,len(x_lower)):
-        #     x_lower[i]=(x_lower[i]-min_x)/abs(max_x-min_x)
+        x_upper=[]
+        cp_upper=[]
+        x_lower=[]
+        cp_lower=[]
+        for element in self.fluid_model_part.Elements:
+            if element.Is(KratosMultiphysics.BOUNDARY) and element.IsNot(KratosMultiphysics.STRUCTURE):
+                gp=element.GetValue(KratosMultiphysics.BODY_FORCE)#provisional name
+                pressure=element.GetValue(KratosMultiphysics.PRESSURE)
+                normal=element.GetValue(KratosMultiphysics.NORMAL)
+                if pressure==0.0:
+                    print(element.Id)
+                if normal[1]<=0:
+                    x_upper.append(gp[0])
+                    cp_upper.append(pressure)
+                else:
+                    x_lower.append(gp[0])
+                    cp_lower.append(pressure)
+        max_x=max(max(x_upper),max(x_lower))
+        min_x=min(min(x_upper),min(x_lower))
+        for i in range(0,len(x_upper)):
+            x_upper[i]=(x_upper[i]-min_x)/abs(max_x-min_x)
+        for i in range(0,len(x_lower)):
+            x_lower[i]=(x_lower[i]-min_x)/abs(max_x-min_x)
+        # for i in range(len(x_upper)):
+            # if x_upper[i]>0.4 and x_upper[i]<0.6:
+                # print(x_upper[i],cp_upper[i])
         print("Cl:",self.result_force[1]) 
         print("Cd:",self.result_force[0])   
         # self.fluid_model_part.SetValue(KratosMultiphysics.FRICTION_COEFFICIENT,self.result_force[1])
         # for node in self.fluid_model_part.GetSubModelPart("KuttaLS").Nodes:
         #     Cl_jump=node.GetValue(KratosMultiphysics.TEMPERATURE)
         # print("Cl_jump:",Cl_jump)
-        # plt.plot(x_upper,cp_upper,'o',label='Upper surface')
-        # plt.plot(x_lower,cp_lower,'ro',label='Lower surface')
-        # title="Cl: %.5f, Cd: %.5f" % (self.result_force[1],self.result_force[0])
-        # plt.title(title)
-        # plt.legend()
-        # plt.gca().invert_yaxis()
-        # # plt.show()
-        # plt.savefig(self.problem_name+'.png', bbox_inches='tight')
-        # plt.close('all')
+        plt.plot(x_upper,cp_upper,'o',label='Upper surface')
+        plt.plot(x_lower,cp_lower,'ro',label='Lower surface')
+        title="Cl: %.5f, Cd: %.5f" % (self.result_force[1],self.result_force[0])
+        plt.title(title)
+        plt.legend()
+        # plt.ylim(-1.8, 1.2)
+        # plt.xlim(-0.2, 1.2)
+        plt.gca().invert_yaxis()
+        # plt.show()
+        plt.savefig(self.problem_name+'.png', bbox_inches='tight')
+        plt.close('all')
     

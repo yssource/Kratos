1d92e8539ced594b76006615a41e13cb35d7f476
DB <daniel.baumgaertner@tum.de>
2019-03-11 15:41:34 +0100

    added traction variable

--- applications/ShapeOptimizationApplication/shape_optimization_application.cpp
+++ applications/ShapeOptimizationApplication/shape_optimization_application.cpp
@@ -82,6 +82,7 @@ namespace Kratos
 
     // For Mapping
     KRATOS_CREATE_VARIABLE(int,MAPPING_ID);
+    KRATOS_CREATE_3D_VARIABLE_WITH_COMPONENTS(TRACTION);
 
     // For bead optimization
     KRATOS_CREATE_VARIABLE(double,ALPHA);
@@ -161,6 +162,7 @@ namespace Kratos
 
         // For mapping
         KRATOS_REGISTER_VARIABLE(MAPPING_ID);
+        KRATOS_REGISTER_3D_VARIABLE_WITH_COMPONENTS(TRACTION);
 
         // For bead optimization
         KRATOS_REGISTER_VARIABLE(ALPHA);

db309feb9b18361269f69c4608a1b34abfe0d72e
sunethwarna <suneth.warna@gmail.com>
2019-03-13 20:08:39 +0100

    remove debug checks

--- applications/RANSConstitutiveLawsApplication/custom_elements/evm_k_epsilon/evm_k_epsilon_utilities.cpp
+++ applications/RANSConstitutiveLawsApplication/custom_elements/evm_k_epsilon/evm_k_epsilon_utilities.cpp
@@ -29,8 +29,8 @@ double CalculateTurbulentViscosity(const double C_mu,
                                    const double minimum_viscosity,
                                    const double maximum_viscosity)
 {
-    CheckIfVariableIsPositive(turbulent_kinetic_energy);
-    CheckIfVariableIsPositive(turbulent_energy_dissipation_rate);
+    // CheckIfVariableIsPositive(turbulent_kinetic_energy);
+    // CheckIfVariableIsPositive(turbulent_energy_dissipation_rate);
 
     const double nu_t = std::min<double>(
         std::max<double>(minimum_viscosity, C_mu * f_mu * std::pow(turbulent_kinetic_energy, 2) /
@@ -42,7 +42,7 @@ double CalculateTurbulentViscosity(const double C_mu,
 
 double CalculateFmu(const double y_plus)
 {
-    CheckIfVariableIsPositive(y_plus);
+    // CheckIfVariableIsPositive(y_plus);
     return 1.0 - std::exp(-0.0115 * y_plus);
 }
 
@@ -50,9 +50,9 @@ double CalculateF2(const double turbulent_kinetic_energy,
                    const double kinematic_viscosity,
                    const double turbulent_energy_dissipation_rate)
 {
-    CheckIfVariableIsPositive(turbulent_kinetic_energy);
-    CheckIfVariableIsPositive(turbulent_energy_dissipation_rate);
-    CheckIfVariableIsPositive(kinematic_viscosity);
+    // CheckIfVariableIsPositive(turbulent_kinetic_energy);
+    // CheckIfVariableIsPositive(turbulent_energy_dissipation_rate);
+    // CheckIfVariableIsPositive(kinematic_viscosity);
 
     if (turbulent_energy_dissipation_rate == 0.0)
         return 1.0;
@@ -72,10 +72,10 @@ double CalculateStabilizationTau(const double velocity_magnitude,
                                  const double reaction,
                                  const double delta_time)
 {
-    CheckIfVariableIsPositive(velocity_magnitude);
-    CheckIfVariableIsPositive(length);
-    CheckIfVariableIsPositive(effective_kinematic_viscosity);
-    CheckIfVariableIsPositive(delta_time);
+    // CheckIfVariableIsPositive(velocity_magnitude);
+    // CheckIfVariableIsPositive(length);
+    // CheckIfVariableIsPositive(effective_kinematic_viscosity);
+    // CheckIfVariableIsPositive(delta_time);
 
     PrintIfVariableIsNegative(velocity_magnitude);
     PrintIfVariableIsNegative(length);
@@ -83,22 +83,22 @@ double CalculateStabilizationTau(const double velocity_magnitude,
     PrintIfVariableIsNegative(reaction);
     PrintIfVariableIsNegative(delta_time);
 
-    // const double Pe = std::max<double>(
-    //     0.5 * velocity_magnitude * length / effective_kinematic_viscosity, 1e-15);
-    // const double alpha = std::max<double>(
-    //     std::min<double>((std::exp(2.0 * Pe) + 1) / (std::exp(2.0 * Pe) - 1) - 1.0 / Pe, 1.0),
-    //     0.0);
+    const double Pe = std::max<double>(
+        0.5 * velocity_magnitude * length / effective_kinematic_viscosity, 1e-15);
+    const double alpha = std::max<double>(
+        std::min<double>((std::exp(2.0 * Pe) + 1) / (std::exp(2.0 * Pe) - 1) - 1.0 / Pe, 1.0),
+        0.0);
 
-    // const double stab_tau = alpha * length / (2 * velocity_magnitude);
+    const double stab_tau = alpha * length / (2 * velocity_magnitude);
 
-    // PrintIfVariableIsNegative(stab_tau);
-    // return stab_tau;
+    PrintIfVariableIsNegative(stab_tau);
+    return stab_tau;
 
-    const double stab_1 = 4.0 * effective_kinematic_viscosity / std::pow(length, 2);
-    const double stab_2 = 2.0 * velocity_magnitude / length;
-    const double stab_3 = reaction;
+    // const double stab_1 = 4.0 * effective_kinematic_viscosity / std::pow(length, 2);
+    // const double stab_2 = 2.0 * velocity_magnitude / length;
+    // const double stab_3 = reaction;
 
-    return 1.0 / (stab_1 + stab_2 + stab_3);
+    // return 1.0 / (stab_1 + stab_2 + stab_3);
 }
 
 template <unsigned int TDim>
@@ -121,8 +121,8 @@ double CalculateSourceTerm(const BoundedMatrix<double, TDim, TDim>& rVelocityGra
                            const double turbulent_kinematic_viscosity,
                            const double turbulent_kinetic_energy)
 {
-    CheckIfVariableIsPositive(turbulent_kinematic_viscosity);
-    CheckIfVariableIsPositive(turbulent_kinetic_energy);
+    // CheckIfVariableIsPositive(turbulent_kinematic_viscosity);
+    // CheckIfVariableIsPositive(turbulent_kinetic_energy);
 
     const double velocity_divergence =
         CalculationUtilities::CalculateMatrixTrace<TDim>(rVelocityGradient);

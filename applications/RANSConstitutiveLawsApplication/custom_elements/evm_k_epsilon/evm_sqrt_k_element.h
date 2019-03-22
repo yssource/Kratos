4a11e32de3f6a4ff3741952b29ba06132a7a0749
sunethwarna <suneth.warna@gmail.com>
2019-03-22 17:43:59 +0100

    adding sqrt k - epsilon

---
+++ applications/RANSConstitutiveLawsApplication/custom_elements/evm_k_epsilon/evm_sqrt_k_element.h
@@ -0,0 +1,486 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//					 Kratos default license: kratos/license.txt
+//
+//  Main authors:    , KratosAppGenerator
+//
+
+#if !defined(KRATOS_EVM_SQRT_K_ELEMENT_H_INCLUDED)
+#define KRATOS_EVM_SQRT_K_ELEMENT_H_INCLUDED
+
+// System includes
+
+// External includes
+
+// Project includes
+#include "includes/element.h"
+#include "includes/properties.h"
+#include "utilities/brent_iteration.h"
+
+#include "custom_elements/evm_k_epsilon/evm_k_epsilon_utilities.h"
+#include "custom_elements/rans_constitutive_element.h"
+#include "includes/cfd_variables.h"
+#include "rans_constitutive_laws_application_variables.h"
+
+namespace Kratos
+{
+///@name Kratos Globals
+///@{
+
+///@}
+///@name Type Definitions
+///@{
+
+///@}
+///@name  Enum's
+///@{
+
+///@}
+///@name  Functions
+///@{
+
+///@}
+///@name Kratos Classes
+///@{
+
+template <unsigned int TDim, unsigned int TNumNodes>
+class EvmSqrtKElement : public RANSConstitutiveElement<TDim, TNumNodes, 1>
+{
+public:
+    ///@name Type Definitions
+    ///@{
+
+    typedef RANSConstitutiveElement<TDim, TNumNodes, 1> BaseType;
+
+    /// Node type (default is: Node<3>)
+    typedef Node<3> NodeType;
+
+    /**
+     * Properties are used to store any parameters
+     * related to the constitutive law
+     */
+    typedef Properties PropertiesType;
+
+    /// Geometry type (using with given NodeType)
+    typedef Geometry<NodeType> GeometryType;
+
+    /// Definition of nodes container type, redefined from GeometryType
+    typedef Geometry<NodeType>::PointsArrayType NodesArrayType;
+
+    /// Vector type for local contributions to the linear system
+    typedef Vector VectorType;
+
+    /// Matrix type for local contributions to the linear system
+    typedef Matrix MatrixType;
+
+    typedef std::size_t IndexType;
+
+    typedef std::size_t SizeType;
+
+    typedef std::vector<std::size_t> EquationIdVectorType;
+
+    typedef std::vector<Dof<double>::Pointer> DofsVectorType;
+
+    typedef PointerVectorSet<Dof<double>, IndexedObject> DofsArrayType;
+
+    /// Type for shape function values container
+    typedef MatrixRow<Matrix> ShapeFunctionsType;
+
+    /// Type for a matrix containing the shape function gradients
+    typedef Kratos::Matrix ShapeFunctionDerivativesType;
+
+    /// Type for an array of shape function gradient matrices
+    typedef GeometryType::ShapeFunctionsGradientsType ShapeFunctionDerivativesArrayType;
+
+    static constexpr unsigned int TBlockSize = 1;
+
+    static constexpr unsigned int TLocalSize = TNumNodes * TBlockSize;
+
+    ///@}
+    ///@name Pointer Definitions
+    /// Pointer definition of EvmSqrtKElement
+    KRATOS_CLASS_POINTER_DEFINITION(EvmSqrtKElement);
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
+    /**
+     * Constructor.
+     */
+    EvmSqrtKElement(IndexType NewId = 0);
+
+    /**
+     * Constructor using an array of nodes
+     */
+    EvmSqrtKElement(IndexType NewId, const NodesArrayType& ThisNodes);
+
+    /**
+     * Constructor using Geometry
+     */
+    EvmSqrtKElement(IndexType NewId, GeometryType::Pointer pGeometry);
+
+    /**
+     * Constructor using Properties
+     */
+    EvmSqrtKElement(IndexType NewId,
+                    GeometryType::Pointer pGeometry,
+                    PropertiesType::Pointer pProperties);
+
+    /**
+     * Copy Constructor
+     */
+    EvmSqrtKElement(EvmSqrtKElement const& rOther);
+
+    /**
+     * Destructor
+     */
+    ~EvmSqrtKElement() override;
+
+    ///@}
+    ///@name Operators
+    ///@{
+
+    /// Assignment operator.
+    EvmSqrtKElement& operator=(EvmSqrtKElement const& rOther);
+
+    ///@}
+    ///@name Operations
+    ///@{
+
+    /**
+     * ELEMENTS inherited from this class have to implement next
+     * Create and Clone methods: MANDATORY
+     */
+
+    /**
+     * creates a new element pointer
+     * @param NewId: the ID of the new element
+     * @param ThisNodes: the nodes of the new element
+     * @param pProperties: the properties assigned to the new element
+     * @return a Pointer to the new element
+     */
+    Element::Pointer Create(IndexType NewId,
+                            NodesArrayType const& ThisNodes,
+                            PropertiesType::Pointer pProperties) const override;
+
+    /**
+     * creates a new element pointer
+     * @param NewId: the ID of the new element
+     * @param pGeom: the geometry to be employed
+     * @param pProperties: the properties assigned to the new element
+     * @return a Pointer to the new element
+     */
+    Element::Pointer Create(IndexType NewId,
+                            GeometryType::Pointer pGeom,
+                            PropertiesType::Pointer pProperties) const override;
+
+    /**
+     * creates a new element pointer and clones the previous element data
+     * @param NewId: the ID of the new element
+     * @param ThisNodes: the nodes of the new element
+     * @param pProperties: the properties assigned to the new element
+     * @return a Pointer to the new element
+     */
+    Element::Pointer Clone(IndexType NewId, NodesArrayType const& ThisNodes) const override;
+
+    /**
+     * this determines the elemental equation ID vector for all elemental
+     * DOFs
+     * @param rResult: the elemental equation ID vector
+     * @param rCurrentProcessInfo: the current process info instance
+     */
+    void EquationIdVector(EquationIdVectorType& rResult, ProcessInfo& CurrentProcessInfo) override;
+
+    /**
+     * determines the elemental list of DOFs
+     * @param ElementalDofList: the list of DOFs
+     * @param rCurrentProcessInfo: the current process info instance
+     */
+    void GetDofList(DofsVectorType& rElementalDofList, ProcessInfo& CurrentProcessInfo) override;
+
+    void GetValuesVector(VectorType& rValues, int Step = 0) override;
+
+    void GetFirstDerivativesVector(VectorType& values, int Step = 0) override;
+
+    void GetSecondDerivativesVector(VectorType& values, int Step = 0) override;
+
+    /**
+     * ELEMENTS inherited from this class have to implement next
+     * CalculateLocalSystem, CalculateLeftHandSide and CalculateRightHandSide
+     * methods they can be managed internally with a private method to do the
+     * same calculations only once: MANDATORY
+     */
+
+    /**
+     * this is called during the assembling process in order
+     * to calculate all elemental contributions to the global system
+     * matrix and the right hand side
+     * @param rLeftHandSideMatrix: the elemental left hand side matrix
+     * @param rRightHandSideVector: the elemental right hand side
+     * @param rCurrentProcessInfo: the current process info instance
+     */
+    void CalculateLocalSystem(MatrixType& rLeftHandSideMatrix,
+                              VectorType& rRightHandSideVector,
+                              ProcessInfo& rCurrentProcessInfo) override;
+
+    /**
+     * this is called during the assembling process in order
+     * to calculate the elemental left hand side matrix only
+     * @param rLeftHandSideMatrix: the elemental left hand side matrix
+     * @param rCurrentProcessInfo: the current process info instance
+     */
+    void CalculateLeftHandSide(MatrixType& rLeftHandSideMatrix,
+                               ProcessInfo& rCurrentProcessInfo) override;
+
+    /**
+     * this is called during the assembling process in order
+     * to calculate the elemental right hand side vector only
+     * @param rRightHandSideVector: the elemental right hand side vector
+     * @param rCurrentProcessInfo: the current process info instance
+     */
+    void CalculateRightHandSide(VectorType& rRightHandSideVector,
+                                ProcessInfo& rCurrentProcessInfo) override;
+
+    /**
+     * this is called during the assembling process in order
+     * to calculate the first derivatives contributions for the LHS and RHS
+     * @param rLeftHandSideMatrix: the elemental left hand side matrix
+     * @param rRightHandSideVector: the elemental right hand side
+     * @param rCurrentProcessInfo: the current process info instance
+     */
+    void CalculateFirstDerivativesContributions(MatrixType& rLeftHandSideMatrix,
+                                                VectorType& rRightHandSideVector,
+                                                ProcessInfo& rCurrentProcessInfo) override;
+
+    /**
+     * this is called during the assembling process in order
+     * to calculate the elemental left hand side matrix for the first derivatives constributions
+     * @param rLeftHandSideMatrix: the elemental left hand side matrix
+     * @param rCurrentProcessInfo: the current process info instance
+     */
+    void CalculateFirstDerivativesLHS(MatrixType& rLeftHandSideMatrix,
+                                      ProcessInfo& rCurrentProcessInfo) override;
+
+    /**
+     * this is called during the assembling process in order
+     * to calculate the elemental right hand side vector for the first derivatives constributions
+     * @param rRightHandSideVector: the elemental right hand side vector
+     * @param rCurrentProcessInfo: the current process info instance
+     */
+    void CalculateFirstDerivativesRHS(VectorType& rRightHandSideVector,
+                                      ProcessInfo& rCurrentProcessInfo) override;
+
+    /**
+     * ELEMENTS inherited from this class must implement this methods
+     * if they need to add dynamic element contributions
+     * note: second derivatives means the accelerations if the displacements are the dof of the analysis
+     * note: time integration parameters must be set in the rCurrentProcessInfo before calling these methods
+     * CalculateSecondDerivativesContributions,
+     * CalculateSecondDerivativesLHS, CalculateSecondDerivativesRHS methods are : OPTIONAL
+     */
+
+    /**
+     * this is called during the assembling process in order
+     * to calculate the second derivative contributions for the LHS and RHS
+     * @param rLeftHandSideMatrix: the elemental left hand side matrix
+     * @param rRightHandSideVector: the elemental right hand side
+     * @param rCurrentProcessInfo: the current process info instance
+     */
+    void CalculateSecondDerivativesContributions(MatrixType& rLeftHandSideMatrix,
+                                                 VectorType& rRightHandSideVector,
+                                                 ProcessInfo& rCurrentProcessInfo) override;
+
+    /**
+     * this is called during the assembling process in order
+     * to calculate the elemental left hand side matrix for the second derivatives constributions
+     * @param rLeftHandSideMatrix: the elemental left hand side matrix
+     * @param rCurrentProcessInfo: the current process info instance
+     */
+    void CalculateSecondDerivativesLHS(MatrixType& rLeftHandSideMatrix,
+                                       ProcessInfo& rCurrentProcessInfo) override;
+
+    /**
+     * this is called during the assembling process in order
+     * to calculate the elemental right hand side vector for the second derivatives constributions
+     * @param rRightHandSideVector: the elemental right hand side vector
+     * @param rCurrentProcessInfo: the current process info instance
+     */
+    void CalculateSecondDerivativesRHS(VectorType& rRightHandSideVector,
+                                       ProcessInfo& rCurrentProcessInfo) override;
+
+    /**
+     * this is called during the assembling process in order
+     * to calculate the elemental mass matrix
+     * @param rMassMatrix: the elemental mass matrix
+     * @param rCurrentProcessInfo: the current process info instance
+     */
+    void CalculateMassMatrix(MatrixType& rMassMatrix, ProcessInfo& rCurrentProcessInfo) override;
+
+    /**
+     * this is called during the assembling process in order
+     * to calculate the elemental damping matrix
+     * @param rDampingMatrix: the elemental damping matrix
+     * @param rCurrentProcessInfo: the current process info instance
+     */
+    void CalculateDampingMatrix(MatrixType& rDampingMatrix,
+                                ProcessInfo& rCurrentProcessInfo) override;
+
+    /// Computes the local contribution associated to 'new' velocity and pressure values
+    /**
+     * Provides local contributions to the system associated to the velocity and
+     * pressure terms (convection, diffusion, pressure gradient/velocity divergence
+     * and stabilization).
+     * @param rDampingMatrix Will be filled with the velocity-proportional "damping" matrix
+     * @param rRightHandSideVector the elemental right hand side vector
+     * @param rCurrentProcessInfo the current process info instance
+     */
+    void CalculateLocalVelocityContribution(MatrixType& rDampingMatrix,
+                                            VectorType& rRightHandSideVector,
+                                            ProcessInfo& rCurrentProcessInfo) override;
+
+    /**
+     * This method provides the place to perform checks on the completeness of the input
+     * and the compatibility with the problem options as well as the contitutive laws selected
+     * It is designed to be called only once (or anyway, not often) typically at the beginning
+     * of the calculations, so to verify that nothing is missing from the input
+     * or that no common error is found.
+     * @param rCurrentProcessInfo
+     * this method is: MANDATORY
+     */
+    int Check(const ProcessInfo& rCurrentProcessInfo) override;
+
+    ///@}
+    ///@name Access
+    ///@{
+
+    ///@}
+    ///@name Inquiry
+    ///@{
+
+    ///@}
+    ///@name Input and output
+    ///@{
+
+    /// Turn back information as a string.
+    std::string Info() const override;
+
+    /// Print information about this object.
+    void PrintInfo(std::ostream& rOStream) const override;
+
+    /// Print object's data.
+    void PrintData(std::ostream& rOStream) const override;
+
+    ///@}
+    ///@name Friends
+    ///@{
+
+    ///@}
+
+protected:
+    ///@name Protected static Member Variables
+    ///@{
+
+    ///@}
+    ///@name Protected member Variables
+    ///@{
+
+    ///@}
+    ///@name Protected Operators
+    ///@{
+
+    ///@}
+    ///@name Protected Operations
+    ///@{
+
+    ///@}
+    ///@name Protected  Access
+    ///@{
+
+    ///@}
+    ///@name Protected Inquiry
+    ///@{
+
+    ///@}
+    ///@name Protected LifeCycle
+    ///@{
+
+    ///@}
+
+private:
+    ///@name Static Member Variables
+    ///@{
+
+    ///@}
+    ///@name Member Variables
+    ///@{
+
+    ///@}
+    ///@name Private Operators
+    ///@{
+
+    ///@}
+    ///@name Private Operations
+    ///@{
+
+    array_1d<double, 3> GetConvectiveVelocity(const double turbulent_kinetic_energy_sigma,
+                                              const double variable_value,
+                                              const Vector& rGaussShapeFunctions,
+                                              const Matrix& rGaussShapeDerivatives) const;
+
+    double CalculateReactionTerm(const double kinematic_viscosity,
+                                 const double wall_distance,
+                                 const double gamma,
+                                 const double c_mu,
+                                 const double f_mu,
+                                 const Matrix& rShapeDerivatives) const;
+
+    double CalculateSourceTerm(const double turbulent_kinematic_viscosity,
+                               const double turbulent_kinetic_energy,
+                               const double variable_value,
+                               const Matrix& rShapeDerivatives) const;
+
+    ///@}
+    ///@name Serialization
+    ///@{
+
+    friend class Serializer;
+
+    void save(Serializer& rSerializer) const override;
+    void load(Serializer& rSerializer) override;
+
+    ///@}
+    ///@name Private  Access
+    ///@{
+
+    ///@}
+    ///@name Private Inquiry
+    ///@{
+
+    ///@}
+    ///@name Un accessible methods
+    ///@{
+
+    ///@}
+
+}; // Class EvmTurbulentKineticEnergyElement
+
+///@}
+
+///@name Type Definitions
+///@{
+
+///@}
+///@name Input and output
+///@{
+
+///@}
+
+} // namespace Kratos.
+
+#endif // KRATOS_EVM_SQRT_K_ELEMENT_H_INCLUDED  defined

f870ac3d06c7d5584be60bcdace4e8f28135b8ff
Miguel Masó Sotomayor <mmaso@cimne.upc.edu>
2019-04-03 17:39:27 +0200

    pase python process

---
+++ applications/ManufacturedFluidSolutionsApplication/python_scripts/manufactured_solution_base_process.py
@@ -0,0 +1,22 @@
+# Importing the Kratos Library
+import KratosMultiphysics as KM
+import KratosMultiphysics.ManufacturedFluidSolutionApplication as MS
+
+
+def Factory(settings, Model):
+    raise Exception("Trying the build the ManufacturedSolutionBaseProcess. Please, call the derived classes")
+
+## All the processes python should be derived from "Process"
+class ManufacturedSolutionBaseProcess(KM.Process):
+    """ Base class for all the sub-processes for the manufactured solutions
+    It provides an mandatory method to set the manufactured solution
+    and sets the manufactured_process attribute, which is in charge to apply
+    the custom velocity, pressure and body force.
+    """
+    def SetManufacturedSolution(self, manufactured_solution):
+        self.manufactured_solution = manufactured_solution
+
+        if hasattr(self, 'model_part'):
+            self.manufactured_process = MS.ManufacturedSolutionUtility(self.model_part, self.manufactured_solution)
+        else:
+            KM.Logger.PrintWarning("ManufacturedSolutionBaseProcess", "No model_part attribute found. The manufactured process is not constructed.")
---
+++ applications/ManufacturedFluidSolutionsApplication/python_scripts/manufactured_solution_process.py
@@ -0,0 +1,80 @@
+# Importing the Kratos Library
+import KratosMultiphysics as KM
+import KratosMultiphysics.ManufacturedFluidSolutionApplication as MS
+
+
+def Factory(settings, Model):
+    if not isinstance(settings, KM.Parameters):
+        raise Exception("expected input shall be a Parameters object, encapsulating a json string")
+    return ManufacturedSolutionProcess(Model, settings["Parameters"])
+
+## All the processes python should be derived from "Process"
+class ManufacturedSolutionProcess(KM.Process):
+    '''
+    A wrapper for all the processes related to the manufactured solution,
+    such as the body force, the boundary conditions o the error computation processes
+    '''
+    def __init__(self, model, settings ):
+
+        KM.Process.__init__(self)
+
+        default_settings = KM.Parameters("""
+            {
+                "model_part_name"          : "model_part",
+                "manufactured_name"        : "manufactured_solution_name",
+                "manufactured_parameters"  : {},
+                "processes_list"           : []
+            }
+            """
+            )
+
+        settings.ValidateAndAssignDefaults(default_settings)
+
+        # Build the manufactured solution
+        fluid_property = model[settings["model_part_name"].GetString()].ElementsArray()[0].Properties
+        manufactured_class = getattr(MS, settings["manufactured_name"].GetString())
+        self.manufactured = manufactured_class(fluid_properties, settings["manufactured_parameters"])
+
+        # Build the sub processes
+        from process_factory import KratosProcessFactory
+        factory = KratosProcessFactory(model)
+        self.processes = factory.ConstructListOfProcesses(settings["processes_list"])
+
+        for process in self.processes:
+            process.SetManufacturedSolution(self.manufactured))
+
+    def Check(self):
+        for process in self.processes:
+            process.Check()
+
+    def ExecuteInitialize(self):
+        for process in self.processes:
+            process.ExecuteInitialize()
+
+    def ExecuteBeforeSolutionLoop(self):
+        for process in self.processes:
+            process.ExecuteBeforeSolutionLoop()
+
+    def ExecuteInitializeSolutionStep(self):
+        for process in self.processes:
+            process.ExecuteInitializeSolutionStep()
+
+    def ExecuteFinalizeSolutionStep(self):
+        for process in self.processes:
+            process.ExecuteFinalizeSolutionStep()
+
+    def ExecuteBeforeOutputStep(self):
+        for process in self.processes:
+            process.ExecuteBeforeOutputStep()
+
+    def ExecuteAfterOutputStep(self):
+        for process in self.processes:
+            process.ExecuteAfterOutputStep()
+
+    def ExecuteFinalize(self):
+        for process in self.processes:
+            process.ExecuteFinalize()
+
+    def Clear(self):
+        for process in self.processes:
+            process.Clear()

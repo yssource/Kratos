6a6b9d804ebc429ec0ddba1c4843d6c55dad6f22
Inigo Lopez <inigolcanalejo@gmail.com>
2019-04-05 14:19:46 +0200

    adding TODO

--- applications/CompressiblePotentialFlowApplication/python_scripts/apply_far_field_process.py
+++ applications/CompressiblePotentialFlowApplication/python_scripts/apply_far_field_process.py
@@ -1,5 +1,5 @@
 import KratosMultiphysics
-import KratosMultiphysics.CompressiblePotentialFlowApplication as CompressiblePotentialFlowApplication
+import KratosMultiphysics.CompressiblePotentialFlowApplication as CPFApp
 import math
 
 def Factory(settings, Model):
@@ -29,85 +29,96 @@ class ApplyFarFieldProcess(KratosMultiphysics.Process):
         self.fluid_model_part = self.model_part.GetRootModelPart()
 
         self.inlet_phi = settings["inlet_phi"].GetDouble()
-        self.velocity_infinity = KratosMultiphysics.Vector(3)
-        self.velocity_infinity[0] = settings["velocity_infinity"][0].GetDouble(
-        )
-        self.velocity_infinity[1] = settings["velocity_infinity"][1].GetDouble(
-        )
-        self.velocity_infinity[2] = settings["velocity_infinity"][2].GetDouble(
-        )
+        self.velocity_inf = KratosMultiphysics.Vector(3)
+        self.velocity_inf[0] = settings["velocity_infinity"][0].GetDouble()
+        self.velocity_inf[1] = settings["velocity_infinity"][1].GetDouble()
+        self.velocity_inf[2] = settings["velocity_infinity"][2].GetDouble()
         self.density_infinity = settings["density_infinity"].GetDouble()
         self.mach_infinity = settings["mach_infinity"].GetDouble()
         self.gamma = settings["gamma"].GetDouble()
         self.pressure_infinity = settings["pressure_infinity"].GetDouble()
 
         self.u_infinity = math.sqrt(
-            self.velocity_infinity[0]**2 + self.velocity_infinity[1]**2 + self.velocity_infinity[2]**2)
+            self.velocity_inf[0]**2 + self.velocity_inf[1]**2 + self.velocity_inf[2]**2)
         self.a_infinity = self.u_infinity / self.mach_infinity
 
         # For the model part
-        self.model_part.ProcessInfo.SetValue(
-            CompressiblePotentialFlowApplication.VELOCITY_INFINITY, self.velocity_infinity)
+        self.model_part.ProcessInfo.SetValue(CPFApp.VELOCITY_INFINITY, self.velocity_inf)
 
         # For the conditions
-        self.fluid_model_part.GetProperties()[0].SetValue(
-            CompressiblePotentialFlowApplication.DENSITY_INFINITY, self.density_infinity)
+        self.fluid_model_part.GetProperties()[0].SetValue(CPFApp.DENSITY_INFINITY, self.density_infinity)
 
         # For the elements
-        self.fluid_model_part.GetProperties()[1].SetValue(
-            CompressiblePotentialFlowApplication.VELOCITY_INFINITY, self.velocity_infinity)
-        self.fluid_model_part.GetProperties()[1].SetValue(
-            CompressiblePotentialFlowApplication.DENSITY_INFINITY, self.density_infinity)
-        self.fluid_model_part.GetProperties()[1].SetValue(
-            CompressiblePotentialFlowApplication.MACH_INFINITY, self.mach_infinity)
-        self.fluid_model_part.GetProperties()[1].SetValue(
-            CompressiblePotentialFlowApplication.GAMMA, self.gamma)
-        self.fluid_model_part.GetProperties()[1].SetValue(
-            KratosMultiphysics.SOUND_VELOCITY, self.a_infinity)
-        self.fluid_model_part.GetProperties()[1].SetValue(
-            CompressiblePotentialFlowApplication.PRESSURE_INFINITY, self.pressure_infinity)
+        self.fluid_model_part.GetProperties()[1].SetValue(CPFApp.VELOCITY_INFINITY, self.velocity_inf)
+        self.fluid_model_part.GetProperties()[1].SetValue(CPFApp.DENSITY_INFINITY, self.density_infinity)
+        self.fluid_model_part.GetProperties()[1].SetValue(CPFApp.MACH_INFINITY, self.mach_infinity)
+        self.fluid_model_part.GetProperties()[1].SetValue(CPFApp.GAMMA, self.gamma)
+        self.fluid_model_part.GetProperties()[1].SetValue(KratosMultiphysics.SOUND_VELOCITY, self.a_infinity)
+        self.fluid_model_part.GetProperties()[1].SetValue(CPFApp.PRESSURE_INFINITY, self.pressure_infinity)
 
     def Execute(self):
-        #KratosMultiphysics.VariableUtils().SetVectorVar(CompressiblePotentialFlowApplication.VELOCITY_INFINITY, self.velocity_infinity, self.model_part.Conditions)
+        #KratosMultiphysics.VariableUtils().SetVectorVar(CPFApp.VELOCITY_INFINITY, self.velocity_inf, self.model_part.Conditions)
         for cond in self.model_part.Conditions:
-            cond.SetValue(CompressiblePotentialFlowApplication.VELOCITY_INFINITY, self.velocity_infinity)
+            cond.SetValue(CPFApp.VELOCITY_INFINITY, self.velocity_inf)
 
-        #select the first node
+        # Select the first node in the mesh as reference
         for node in self.model_part.Nodes:
-            node1 = node
+            x0 = node.X
+            y0 = node.Y
+            z0 = node.Z
             break
-        
-        #find the node with the minimal x
-        x0 = node1.X
-        y0 = node1.X
-        z0 = node1.X
 
+        # Find smallest distance_to_reference
         pos = 1e30
         for node in self.model_part.Nodes:
+            # Computing distance to reference
             dx = node.X - x0
             dy = node.Y - y0
             dz = node.Z - z0
-            
-            tmp = dx*self.velocity_infinity[0] + dy*self.velocity_infinity[1] + dz*self.velocity_infinity[2]
-            
-            if(tmp < pos):
-                pos = tmp
 
+            distance_to_reference = dx*self.velocity_inf[0] + dy*self.velocity_inf[1] + dz*self.velocity_inf[2]
+
+            if(distance_to_reference < pos):
+                pos = distance_to_reference
+                self.inlet_node = node
+
+        # Fix nodes in the inlet
         for node in self.model_part.Nodes:
+            # Computing distance to reference
             dx = node.X - x0
             dy = node.Y - y0
             dz = node.Z - z0
-            
-            tmp = dx*self.velocity_infinity[0] + dy*self.velocity_infinity[1] + dz*self.velocity_infinity[2]
-            
-            if(tmp < pos+1e-9):
-                node.Fix(CompressiblePotentialFlowApplication.VELOCITY_POTENTIAL)
-                node.SetSolutionStepValue(CompressiblePotentialFlowApplication.VELOCITY_POTENTIAL,0,self.inlet_phi)
-                if self.model_part.HasNodalSolutionStepVariable(CompressiblePotentialFlowApplication.ADJOINT_VELOCITY_POTENTIAL):
-                    node.Fix(CompressiblePotentialFlowApplication.ADJOINT_VELOCITY_POTENTIAL)
-                    node.SetSolutionStepValue(CompressiblePotentialFlowApplication.ADJOINT_VELOCITY_POTENTIAL,0,0.0)
+
+            distance_to_reference = dx*self.velocity_inf[0] + dy*self.velocity_inf[1] + dz*self.velocity_inf[2]
+
+            if(distance_to_reference < pos+1e-9):
+                node.Fix(CPFApp.VELOCITY_POTENTIAL)
+                node.SetSolutionStepValue(CPFApp.VELOCITY_POTENTIAL,0,self.inlet_phi)
+                if self.model_part.HasNodalSolutionStepVariable(CPFApp.ADJOINT_VELOCITY_POTENTIAL):
+                    node.Fix(CPFApp.ADJOINT_VELOCITY_POTENTIAL)
+                    node.SetSolutionStepValue(CPFApp.ADJOINT_VELOCITY_POTENTIAL,0,0.0)
+
+        #TODO: Check best way of initializing the flow field
+        '''
+        # Compute free stream direction
+        self.free_stream_direction = self.velocity_inf
+        self.free_stream_direction[0] /= self.u_infinity
+        self.free_stream_direction[1] /= self.u_infinity
+        self.free_stream_direction[2] /= self.u_infinity
         
+        for node in self.fluid_model_part.Nodes:
+            # Computing distance to inlet
+            dx = node.X - self.inlet_node.X
+            dy = node.Y - self.inlet_node.Y
+            dz = node.Z - self.inlet_node.Z
+
+            distance_to_inlet = dx*self.free_stream_direction[0] + dy*self.free_stream_direction[1] + dz*self.free_stream_direction[2]
+
+            initial_potential = self.inlet_phi + self.u_infinity * distance_to_inlet
+            #node.SetValue(KratosMultiphysics.WATER_PRESSURE,initial_potential)
+            #node.SetSolutionStepValue(CPFApp.VELOCITY_POTENTIAL,0,initial_potential)
+            #node.SetSolutionStepValue(CPFApp.AUXILIARY_VELOCITY_POTENTIAL, 0, initial_potential)
+        '''
+
     def ExecuteInitializeSolutionStep(self):
         self.Execute()
-        
-        

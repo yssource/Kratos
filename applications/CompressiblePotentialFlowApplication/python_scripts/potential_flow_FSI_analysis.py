f3d043a96ac9152e981a3948ff4cb05540008fe8
Lukas Hoeffner <lukas.hoeffner@tum.de>
2019-04-10 15:01:43 +0200

    tried handling both solvers in analysis (dead)

--- applications/CompressiblePotentialFlowApplication/python_scripts/potential_flow_FSI_analysis.py
+++ applications/CompressiblePotentialFlowApplication/python_scripts/potential_flow_FSI_analysis.py
@@ -2,6 +2,7 @@ from __future__ import print_function, absolute_import, division  # makes Kratos
 
 # Importing Kratos
 import KratosMultiphysics
+import KratosMultiphysics.MeshMovingApplication.python_solvers_wrapper_mesh_motion as mesh_mothion_solvers
 
 # Importing the base class
 from KratosMultiphysics.CompressiblePotentialFlowApplication.potential_flow_analysis import PotentialFlowAnalysis
@@ -10,16 +11,18 @@ class PotentialFlowFSIAnalysis(PotentialFlowAnalysis):
     '''Main script for potential flow simulations with basic fluid-structure-interaction.'''
 
     def __init__(self,model,project_parameters):
+        super(PotentialFlowFSIAnalysis, self).__init__(model,project_parameters)
         self.iteration_count = 0
         self.disp = 0
         self.prev_disp = 0
-        super(PotentialFlowFSIAnalysis, self).__init__(model,project_parameters)
+
 
 #    def Run(self):
  #
   #      super(PotentialFlowFSIAnalysis, self).Run()
+
     def _CreateSolver(self):
-       # self.mesh_solver =
+        self.mesh_solver = mesh_mothion_solvers.CreateSolver(self.model, self.project_parameters)
         return super(PotentialFlowFSIAnalysis, self)._CreateSolver()
 
     def StructuralModelResponse(self, lift):
@@ -28,11 +31,19 @@ class PotentialFlowFSIAnalysis(PotentialFlowAnalysis):
 
 
     def KeepAdvancingSolutionLoop(self):
-        f = 0.01 # relative error for convergence check
+        f = 0.01 # relative change for convergence check
         max_iteration_count = 4
-        rel_change = abs(1-self.prev_disp/self.disp)
         self.iteration_count = self.iteration_count+1
-        return self.iteration_count<max_iteration_count and rel_change>f
+        if self.disp==0.0:
+            return self.iteration_count<max_iteration_count
+        else:
+            rel_change = abs(1-self.prev_disp/self.disp)
+            return self.iteration_count<max_iteration_count and rel_change>f
+
+    def MoveMesh(self):
+        self.body_model_part = self.model["MainModelPart.Body2D_BodySurface"]
+        for node in self.body_model_part.Nodes:
+            node.SetSolutionStepValue(KratosMultiphysics.MESH_DISPLACEMENT_Y,0,self.disp)
 
 
     def RunSolutionLoop(self):
@@ -40,3 +51,4 @@ class PotentialFlowFSIAnalysis(PotentialFlowAnalysis):
             super(PotentialFlowFSIAnalysis, self).RunSolutionLoop()
             self.prev_disp = self.disp
             self.disp = self.StructuralModelResponse(KratosMultiphysics.TOTAL_LIFT)
+            self.MoveMesh()

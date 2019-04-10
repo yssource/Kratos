55bb9435c6d0ae806b932e572849ee9f0f857ebb
Miguel <mmaso@cimne.upc.edu>
2019-04-10 09:42:39 +0200

    printing elapsed time

--- applications/ManufacturedFluidSolutionsApplication/python_scripts/print_error_statistics_process.py
+++ applications/ManufacturedFluidSolutionsApplication/python_scripts/print_error_statistics_process.py
@@ -4,6 +4,7 @@ import KratosMultiphysics.ManufacturedFluidSolutionsApplication as MS
 
 # Other imports
 import h5py
+import time
 import numpy as np
 
 from KratosMultiphysics.ManufacturedFluidSolutionsApplication.manufactured_solution_base_process import ManufacturedSolutionBaseProcess as ManufacturedProcess
@@ -36,12 +37,6 @@ class PrintErrorStatisticsProcess(ManufacturedProcess):
 
         self.model_part = model[self.settings["model_part_name"].GetString()]
 
-        case_dtype = np.dtype([("label", h5py.special_dtype(vlen=str)),
-                               ("num_nodes", np.uint32),
-                               ("num_elems", np.uint32),
-                               ("time_step", np.float),
-                               ("rel_error", np.float)])
-
         self.f = h5py.File(self.settings["file_name"].GetString() + ".hdf5", 'a') # 'a' means append mode
 
         dset_name = "manufactured_{:03d}".format(self.settings["manufactured_id"].GetInt())
@@ -49,8 +44,16 @@ class PrintErrorStatisticsProcess(ManufacturedProcess):
             self.dset = self.f[dset_name]
             self.dataset_is_initialized = True
         else:
+            case_dtype = np.dtype([("label", h5py.special_dtype(vlen=str)),
+                                ("num_nodes", np.uint32),
+                                ("num_elems", np.uint32),
+                                ("time_step", np.float),
+                                ("computational_time", np.float),
+                                ("rel_error", np.float)])
             self.dset = self.f.create_dataset(dset_name, (0,), maxshape=(None,), chunks=True, dtype=case_dtype)
             self.dataset_is_initialized = False
+
+        self.start_time = time.time()
 
 
     def ExecuteFinalize(self):
@@ -74,11 +77,13 @@ class PrintErrorStatisticsProcess(ManufacturedProcess):
 
 
     def _WriteAverageRelativeError(self):
+        elapsed_time = time.time() - self.start_time
         rel_err = self.manufactured_process.ComputeMean(MS.VELOCITY_RELATIVE_ERROR)
         case_data = (self.settings["label"].GetString(),
                      self.model_part.NumberOfNodes(),
                      self.model_part.NumberOfElements(),
                      self.model_part.ProcessInfo[KM.DELTA_TIME],
+                     elapsed_time,
                      rel_err)
 
         case_idx = self.dset.len()

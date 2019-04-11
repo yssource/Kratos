1344c38f3d7a678879375dce86b455021acb26f5
aggelosez <aggelos.eznepidis@gmail.com>
2019-04-11 15:34:09 +0200

    minor changes

--- applications/CompressiblePotentialFlowApplication/custom_elements/incompressible_potential_flow_element.cpp
+++ applications/CompressiblePotentialFlowApplication/custom_elements/incompressible_potential_flow_element.cpp
@@ -492,8 +492,9 @@ void IncompressiblePotentialFlowElement<Dim, NumNodes>::ComputeLHSGaussPointCont
     const double weight, Matrix& lhs, const ElementalData<NumNodes, Dim>& data) const
 {
     array_1d<double, Dim> velocity;
-    Matrix lhs1;
-    Matrix lhs2;
+    //Matrix lhs1;
+    //Matrix lhs2;
+    BoundedMatrix<double, NumNodes, NumNodes> DNVVDN;
     const double density_infinity = GetProperties().GetValue(DENSITY_INFINITY);
     const double speed_of_sound = GetProperties().GetValue(SPEED_OF_SOUND);
     const double gamma = GetProperties().GetValue(GAMMA);
@@ -502,15 +503,28 @@ void IncompressiblePotentialFlowElement<Dim, NumNodes>::ComputeLHSGaussPointCont
     //const array_1d<double, 3> vinfinity = rCurrentProcessInfo[VELOCITY_INFINITY];
     //const double vinfinity_squared = inner_prod(vinfinity, vinfinity));
     const double vinfinity_squared = 100.0;
-    const double speed_of_sound_squared = speed_of_sound * speed_of_sound;                                                                      // import speed of sound / add const double in front?
-    const double exponent = 1 / (gamma - 1);                                                                                                    // import gamma / add const double in front?
-    double density = density_infinity*pow((1+0.5*(gamma-1)*vinfinity_squared*(1-vsquared/vinfinity_squared)/speed_of_sound_squared),exponent); // import density infinity / add double in front?
-    double drho_dvsquared = pow(density, (2 - gamma)) / (2 * speed_of_sound_squared);                                                     // import density infinity / add double in front?
-    noalias(lhs1) += density * weight * prod(data.DN_DX, trans(data.DN_DX));// + 2 * drho_dvsquared * weight * prod(prod(trans(data.DN_DX) * velocity), prod(trans(velocity) * data.DN_DX)));                                                                    // do we need noalias?
-    noalias(lhs2) += 2.0 * drho_dvsquared * weight * prod(prod((data.DN_DX),velocity), prod(trans(velocity), data.DN_DX));                // doesnt work
+    const double speed_of_sound_squared = speed_of_sound * speed_of_sound;
+    const double exponent = 1 / (gamma - 1);
+    double density = density_infinity*pow((1+0.5*(gamma-1)*vinfinity_squared*(1-vsquared/vinfinity_squared)/speed_of_sound_squared),exponent);
+    int temp;
+    double drho_dvsquared = pow(density, (2 - gamma)) / (2 * speed_of_sound_squared);
+    //std::cout  << "weight " << weight << std::endl;//", drho_dvsquared " << drho_dvsquared << std::endl;
+    //std::cin >> temp;
+    BoundedVector<double, NumNodes> DNV = prod(data.DN_DX, velocity);
+    BoundedVector<double, NumNodes> VDN = prod(trans(velocity), trans(data.DN_DX));
+    for (unsigned int i = 0; i < NumNodes; ++i)
+    {
+        for (unsigned int k = 0; k < NumNodes; ++k)
+        {
+            DNVVDN(i, k) = 2.0 * drho_dvsquared * weight *DNV(i)*VDN(k);
+        }
+    }
+    //DNVVDN = prod(DNV, trans(VDN));
+    noalias(lhs) += (density_infinity * weight * prod(data.DN_DX, trans(data.DN_DX))+DNVVDN);// + 0.0*2.0 * drho_dvsquared * weight * DNVVDN);
+    //noalias(lhs2) += 2.0 * drho_dvsquared * weight * DNVVDN;
     //noalias(lhs) = lhs1 + lhs2;
 
-    noalias(lhs) += density_infinity * weight * prod(data.DN_DX, trans(data.DN_DX));
+    //noalias(lhs) += density_infinity*weight * prod(data.DN_DX, trans(data.DN_DX)); // original line
 }
 
 // template <int Dim, int NumNodes>

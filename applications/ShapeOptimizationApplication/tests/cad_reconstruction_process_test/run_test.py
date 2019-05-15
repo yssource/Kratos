77f470b2f392c2086e04dac43c8fcec6d42a3ba4
DB <daniel.baumgaertner@tum.de>
2019-05-15 16:17:55 +0200

    adjusted and improved unit tests

--- applications/ShapeOptimizationApplication/tests/cad_reconstruction_process_test/run_test.py
+++ applications/ShapeOptimizationApplication/tests/cad_reconstruction_process_test/run_test.py
@@ -12,13 +12,22 @@ from cad_reconstruction_mapper import CADMapper
 import numpy as np
 
 # =======================================================================================================
+# Parameters
+# =======================================================================================================
+specific_tests_to_run = [] # leave empty to run all tests
+write_test_results_to_csv = False
+suppress_output = True
+delete_result_folders = True
+
+# =======================================================================================================
 # Helper functions
 # =======================================================================================================
 @contextmanager
 def suppress_stdout():
     with open(os.devnull, "w") as devnull:
         old_stdout = sys.stdout
-        sys.stdout = devnull
+        if suppress_output:
+            sys.stdout = devnull
         try:
             yield
         finally:
@@ -42,10 +51,11 @@ def ExtractResults(cad_model):
 def TestResults(pole_coordinates, test_case_string):
     error_tolerance = 10
 
-    # with open("pole_coords_test_"+ test_case_string +".csv", 'w', newline='') as csvfile:
-    #     csv_writer = csv.writer(csvfile, delimiter=',',quotechar='|',quoting=csv.QUOTE_NONE)
-    #     for coords in pole_coordinates:
-    #         csv_writer.writerow(coords)
+    if write_test_results_to_csv:
+        with open("pole_coords_test_"+ test_case_string +".csv", 'w', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile, delimiter=',',quotechar='|',quoting=csv.QUOTE_NONE)
+            for coords in pole_coordinates:
+                csv_writer.writerow(coords)
 
     with open("pole_coords_test_"+ test_case_string +".csv", 'r') as f:
         reader = csv.reader(f)
@@ -61,222 +71,218 @@ fe_model = KratosMultiphysics.Model()
 start_time = time.time()
 
 # =======================================================================================================
-# Test 1: Distance minimization without enforcement and alpha regularization (including several iterations and rhs testing)
+# Test: Distance minimization without enforcement and alpha regularization (including rhs testing)
 # =======================================================================================================
-print("\n> Starting test 1...")
-start_time_2 = time.time()
+test_number = 1
+if test_number in specific_tests_to_run or len(specific_tests_to_run) == 0 :
+    print("\n> Starting test "+str(test_number)+"...")
 
-with suppress_stdout():
+    with suppress_stdout():
+            with open("parameters.json",'r') as parameter_file:
+                parameters = KratosMultiphysics.Parameters(parameter_file.read())
 
-    with open("parameters.json",'r') as parameter_file:
-        parameters = KratosMultiphysics.Parameters(parameter_file.read())
+            parameters["solution"]["test_solution"].SetBool(True)
+            parameters["output"]["results_directory"].SetString("Results_Test_"+str(test_number))
 
-    parameters["solution"]["iterations"].SetInt(3)
-    parameters["solution"]["test_solution"].SetBool(True)
-    parameters["output"]["results_directory"].SetString("Results_Test_1")
+            # Mapping
+            cad_model = an.Model()
+            PerformMapping(cad_model, fe_model, parameters)
 
-    # Mapping
-    cad_model = an.Model()
-    PerformMapping(cad_model, fe_model, parameters)
-
-    # Actual test
-    pole_coordinates = ExtractResults(cad_model)
-    TestResults(pole_coordinates, "01")
-
-time_for_first_test = time.time()-start_time_2
+            # Actual test
+            pole_coordinates = ExtractResults(cad_model)
+            TestResults(pole_coordinates, str(test_number))
 
 # =======================================================================================================
-# Test 2: Distance minimization with alpha regularization and constraint enforcement
+# Test: Distance minimization with alpha regularization and constraint enforcement
 # =======================================================================================================
-print("\n> Starting test 2...")
-with suppress_stdout():
+test_number = 2
+if test_number in specific_tests_to_run or len(specific_tests_to_run) == 0 :
+    print("\n> Starting test "+str(test_number)+"...")
 
-    with open("parameters.json",'r') as parameter_file:
-        parameters = KratosMultiphysics.Parameters(parameter_file.read())
+    with suppress_stdout():
+        with open("parameters.json",'r') as parameter_file:
+            parameters = KratosMultiphysics.Parameters(parameter_file.read())
 
-    parameters["regularization"]["alpha"].SetDouble(0.1)
-    parameters["conditions"]["edges"]["fe_based"]["apply_enforcement_conditions"].SetBool(True)
-    parameters["conditions"]["edges"]["fe_based"]["apply_corner_enforcement_conditions"].SetBool(True)
-    parameters["output"]["results_directory"].SetString("Results_Test_2")
+        parameters["regularization"]["alpha"].SetDouble(0.1)
+        parameters["conditions"]["edges"]["fe_based"]["apply_enforcement_conditions"].SetBool(True)
+        parameters["conditions"]["edges"]["fe_based"]["apply_corner_enforcement_conditions"].SetBool(True)
+        parameters["output"]["results_directory"].SetString("Results_Test_"+str(test_number))
 
-    # Mapping
-    cad_model = an.Model()
-    PerformMapping(cad_model, fe_model, parameters)
+        # Mapping
+        cad_model = an.Model()
+        PerformMapping(cad_model, fe_model, parameters)
 
-    # Actual test
-    pole_coordinates = ExtractResults(cad_model)
-    TestResults(pole_coordinates, "02")
-
-# =======================================================================================================
-# Test 3: Distance minimization with integral approach
-# =======================================================================================================
-print("\n> Starting test 3...")
-with suppress_stdout():
-
-    with open("parameters.json",'r') as parameter_file:
-        parameters = KratosMultiphysics.Parameters(parameter_file.read())
-
-    parameters["conditions"]["general"]["apply_integral_method"].SetBool(True)
-    parameters["output"]["results_directory"].SetString("Results_Test_3")
-
-    # Mapping
-    cad_model = an.Model()
-    PerformMapping(cad_model, fe_model, parameters)
-
-    # Actual test
-    pole_coordinates = ExtractResults(cad_model)
-    TestResults(pole_coordinates, "03")
+        # Actual test
+        pole_coordinates = ExtractResults(cad_model)
+        TestResults(pole_coordinates, str(test_number))
 
 # =======================================================================================================
-# Test 4: Curvature condition
+# Test: Distance minimization with integral approach
 # =======================================================================================================
-print("\n> Starting test 4...")
-with suppress_stdout():
+test_number = 3
+if test_number in specific_tests_to_run or len(specific_tests_to_run) == 0 :
+    print("\n> Starting test "+str(test_number)+"...")
 
-    with open("parameters.json",'r') as parameter_file:
-        parameters = KratosMultiphysics.Parameters(parameter_file.read())
+    with suppress_stdout():
+        with open("parameters.json",'r') as parameter_file:
+            parameters = KratosMultiphysics.Parameters(parameter_file.read())
 
-    parameters["conditions"]["faces"]["curvature"]["apply_curvature_minimization"].SetBool(True)
-    parameters["output"]["results_directory"].SetString("Results_Test_4")
+        parameters["conditions"]["general"]["apply_integral_method"].SetBool(True)
+        parameters["output"]["results_directory"].SetString("Results_Test_"+str(test_number))
 
-    # Mapping
-    cad_model = an.Model()
-    PerformMapping(cad_model, fe_model, parameters)
+        # Mapping
+        cad_model = an.Model()
+        PerformMapping(cad_model, fe_model, parameters)
 
-    # Actual test
-    pole_coordinates = ExtractResults(cad_model)
-    TestResults(pole_coordinates, "04")
-
-# =======================================================================================================
-# Test 5: KL shell behavior
-# =======================================================================================================
-print("\n> Starting test 5...")
-with suppress_stdout():
-
-    with open("parameters.json",'r') as parameter_file:
-        parameters = KratosMultiphysics.Parameters(parameter_file.read())
-
-    parameters["conditions"]["faces"]["mechanical"]["apply_KL_shell"].SetBool(True)
-    parameters["output"]["results_directory"].SetString("Results_Test_5")
-
-    # Mapping
-    cad_model = an.Model()
-    PerformMapping(cad_model, fe_model, parameters)
-
-    # Actual test
-    pole_coordinates = ExtractResults(cad_model)
-    TestResults(pole_coordinates, "05")
+        # Actual test
+        pole_coordinates = ExtractResults(cad_model)
+        TestResults(pole_coordinates, str(test_number))
 
 # =======================================================================================================
-# Test 6: Rigid motions
+# Test: KL shell behavior
 # =======================================================================================================
-print("\n> Starting test 6...")
-with suppress_stdout():
+test_number = 4
+if test_number in specific_tests_to_run or len(specific_tests_to_run) == 0 :
+    print("\n> Starting test "+str(test_number)+"...")
 
-    with open("parameters.json",'r') as parameter_file:
-        parameters = KratosMultiphysics.Parameters(parameter_file.read())
+    with suppress_stdout():
+        with open("parameters.json",'r') as parameter_file:
+            parameters = KratosMultiphysics.Parameters(parameter_file.read())
 
-    parameters["conditions"]["faces"]["rigid"]["apply_rigid_conditions"].SetBool(True)
-    parameters["output"]["results_directory"].SetString("Results_Test_6")
+        parameters["conditions"]["faces"]["mechanical"]["apply_KL_shell"].SetBool(True)
+        parameters["output"]["results_directory"].SetString("Results_Test_"+str(test_number))
 
-    # Mapping
-    cad_model = an.Model()
-    PerformMapping(cad_model, fe_model, parameters)
+        # Mapping
+        cad_model = an.Model()
+        PerformMapping(cad_model, fe_model, parameters)
 
-    # Actual test
-    pole_coordinates = ExtractResults(cad_model)
-    TestResults(pole_coordinates, "06")
-
-# =======================================================================================================
-# Test 7: basic coupling conditions
-# =======================================================================================================
-print("\n> Starting test 7...")
-with suppress_stdout():
-
-    with open("parameters.json",'r') as parameter_file:
-        parameters = KratosMultiphysics.Parameters(parameter_file.read())
-
-    parameters["conditions"]["edges"]["coupling"]["apply_coupling_conditions"].SetBool(True)
-    parameters["output"]["results_directory"].SetString("Results_Test_7")
-
-    # Mapping
-    cad_model = an.Model()
-    PerformMapping(cad_model, fe_model, parameters)
-
-    # Actual test
-    pole_coordinates = ExtractResults(cad_model)
-    TestResults(pole_coordinates, "07")
+        # Actual test
+        pole_coordinates = ExtractResults(cad_model)
+        TestResults(pole_coordinates, str(test_number))
 
 # =======================================================================================================
-# Test 8: A prioi refinement
+# Test: Nonlinear solution iteration (using shell that shall enforce a quasi rigid hole)
 # =======================================================================================================
-print("\n> Starting test 8...")
-with suppress_stdout():
+test_number = 5
+if test_number in specific_tests_to_run or len(specific_tests_to_run) == 0 :
+    print("\n> Starting test "+str(test_number)+"...")
 
-    with open("parameters.json",'r') as parameter_file:
-        parameters = KratosMultiphysics.Parameters(parameter_file.read())
+    with suppress_stdout():
+            with open("parameters.json",'r') as parameter_file:
+                parameters = KratosMultiphysics.Parameters(parameter_file.read())
 
-    parameters["refinement"]["a_priori"]["apply_a_priori_refinement"].SetBool(True)
-    parameters["output"]["results_directory"].SetString("Results_Test_8")
+            parameters["solution"]["iterations"].SetInt(3)
+            parameters["conditions"]["faces"]["mechanical"]["apply_KL_shell"].SetBool(True)
+            parameters["conditions"]["faces"]["mechanical"]["exclusive_face_list"][0].SetString("Rhino<dba9a5ba-6b33-4a1b-b46e-3916bd68d053>.BrepFace<1>.Hole")
+            parameters["output"]["results_directory"].SetString("Results_Test_"+str(test_number))
 
-    print(parameters["refinement"]["a_priori"]["apply_a_priori_refinement"].GetBool())
+            # Mapping
+            cad_model = an.Model()
+            PerformMapping(cad_model, fe_model, parameters)
 
-    # Mapping
-    cad_model = an.Model()
-    PerformMapping(cad_model, fe_model, parameters)
-
-    # Actual test
-    pole_coordinates = ExtractResults(cad_model)
-    TestResults(pole_coordinates, "08")
-
-# =======================================================================================================
-# Test 9: A posteriori refinement
-# =======================================================================================================
-print("\n> Starting test 9...")
-with suppress_stdout():
-
-    with open("parameters.json",'r') as parameter_file:
-        parameters = KratosMultiphysics.Parameters(parameter_file.read())
-
-    parameters["refinement"]["a_posteriori"]["apply_a_posteriori_refinement"].SetBool(True)
-    parameters["output"]["results_directory"].SetString("Results_Test_9")
-
-    # Mapping
-    cad_model = an.Model()
-    PerformMapping(cad_model, fe_model, parameters)
-
-    # Actual test
-    pole_coordinates = ExtractResults(cad_model)
-    TestResults(pole_coordinates, "09")
+            # Actual test
+            pole_coordinates = ExtractResults(cad_model)
+            TestResults(pole_coordinates, str(test_number))
 
 # =======================================================================================================
-# Test time
+# Test: Rigid motions
 # =======================================================================================================
-print("\n> Starting time...")
-time_for_complete_test = time.time() - start_time
-relative_time_ratio = time_for_complete_test / time_for_first_test
+test_number = 6
+if test_number in specific_tests_to_run or len(specific_tests_to_run) == 0 :
+    print("\n> Starting test "+str(test_number)+"...")
 
-reference_ratio = 13.6
+    with suppress_stdout():
+        with open("parameters.json",'r') as parameter_file:
+            parameters = KratosMultiphysics.Parameters(parameter_file.read())
 
-if (relative_time_ratio - reference_ratio) / relative_time_ratio > 0.10:
-    raise RuntimeError("Test took unexpectedly long!")
-elif (relative_time_ratio - reference_ratio) / relative_time_ratio < -0.10:
-    raise RuntimeError("Test took unexpectedly short!")
+        parameters["conditions"]["faces"]["rigid"]["apply_rigid_conditions"].SetBool(True)
+        parameters["output"]["results_directory"].SetString("Results_Test_"+str(test_number))
+
+        # Mapping
+        cad_model = an.Model()
+        PerformMapping(cad_model, fe_model, parameters)
+
+        # Actual test
+        pole_coordinates = ExtractResults(cad_model)
+        TestResults(pole_coordinates, str(test_number))
+
+# =======================================================================================================
+# Test: basic coupling conditions
+# =======================================================================================================
+test_number = 7
+if test_number in specific_tests_to_run or len(specific_tests_to_run) == 0 :
+    print("\n> Starting test "+str(test_number)+"...")
+
+    with suppress_stdout():
+        with open("parameters.json",'r') as parameter_file:
+            parameters = KratosMultiphysics.Parameters(parameter_file.read())
+
+        parameters["conditions"]["edges"]["coupling"]["apply_coupling_conditions"].SetBool(True)
+        parameters["output"]["results_directory"].SetString("Results_Test_"+str(test_number))
+
+        # Mapping
+        cad_model = an.Model()
+        PerformMapping(cad_model, fe_model, parameters)
+
+        # Actual test
+        pole_coordinates = ExtractResults(cad_model)
+        TestResults(pole_coordinates, str(test_number))
+
+# =======================================================================================================
+# Test: A prioi refinement
+# =======================================================================================================
+test_number = 8
+if test_number in specific_tests_to_run or len(specific_tests_to_run) == 0 :
+    print("\n> Starting test "+str(test_number)+"...")
+
+    with suppress_stdout():
+        with open("parameters.json",'r') as parameter_file:
+            parameters = KratosMultiphysics.Parameters(parameter_file.read())
+
+        parameters["refinement"]["a_priori"]["apply_a_priori_refinement"].SetBool(True)
+        parameters["output"]["results_directory"].SetString("Results_Test_"+str(test_number))
+
+        # Mapping
+        cad_model = an.Model()
+        PerformMapping(cad_model, fe_model, parameters)
+
+        # Actual test
+        pole_coordinates = ExtractResults(cad_model)
+        TestResults(pole_coordinates, str(test_number))
+
+# =======================================================================================================
+# Test: A posteriori refinement
+# =======================================================================================================
+test_number = 9
+if test_number in specific_tests_to_run or len(specific_tests_to_run) == 0 :
+    print("\n> Starting test "+str(test_number)+"...")
+
+    with suppress_stdout():
+        with open("parameters.json",'r') as parameter_file:
+            parameters = KratosMultiphysics.Parameters(parameter_file.read())
+
+        parameters["refinement"]["a_posteriori"]["apply_a_posteriori_refinement"].SetBool(True)
+        parameters["output"]["results_directory"].SetString("Results_Test_"+str(test_number))
+
+        # Mapping
+        cad_model = an.Model()
+        PerformMapping(cad_model, fe_model, parameters)
+
+        # Actual test
+        pole_coordinates = ExtractResults(cad_model)
+        TestResults(pole_coordinates, str(test_number))
 
 # =======================================================================================================
 # Delete result files
 # =======================================================================================================
 os.remove("cad_reconstruction_process_test.post.lst")
 os.remove("plate.time")
-shutil.rmtree("Results_Test_1")
-shutil.rmtree("Results_Test_2")
-shutil.rmtree("Results_Test_3")
-shutil.rmtree("Results_Test_4")
-shutil.rmtree("Results_Test_5")
-shutil.rmtree("Results_Test_6")
-shutil.rmtree("Results_Test_7")
-shutil.rmtree("Results_Test_8")
-shutil.rmtree("Results_Test_9")
 
+if delete_result_folders:
+    for number in range(test_number+1):
+        if os.path.exists("Results_Test_"+str(number)):
+            shutil.rmtree("Results_Test_"+str(number))
+
+time_for_complete_test = time.time() - start_time
 print("\n> Test Successfully finished in " + str(round(time_for_complete_test,2)) + " s!\n")
 # =======================================================================================================

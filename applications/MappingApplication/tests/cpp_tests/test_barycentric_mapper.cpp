4d4c269fc5177ba9c2b48d5e86cf612bc01e2fb6
philbucher <philipp.bucher@tum.de>
2019-05-16 12:44:52 +0200

    adding empty c-test-file for barycentric

---
+++ applications/MappingApplication/tests/cpp_tests/test_barycentric_mapper.cpp
@@ -0,0 +1,28 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Philipp Bucher
+//
+
+// Project includes
+#include "geometries/line_2d_2.h"
+#include "geometries/triangle_3d_3.h"
+#include "geometries/tetrahedra_3d_4.h"
+#include "testing/testing.h"
+#include "custom_mappers/barycentric_mapper.h"
+#include "includes/stream_serializer.h"
+#include "custom_utilities/mapper_utilities.h"
+#include "mapping_application_variables.h"
+
+namespace Kratos {
+namespace Testing {
+
+
+}  // namespace Testing
+}  // namespace Kratos

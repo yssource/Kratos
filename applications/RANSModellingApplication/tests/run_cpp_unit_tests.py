8830380b402e5c3244b1001dbd109a3a598b0c7d
sunethwarna <suneth.warna@gmail.com>
2019-05-24 22:44:09 +0200

    removing rans_utility tests

--- applications/RANSModellingApplication/tests/run_cpp_unit_tests.py
+++ applications/RANSModellingApplication/tests/run_cpp_unit_tests.py
@@ -3,12 +3,10 @@ from KratosMultiphysics.RANSModellingApplication import *
 
 def run():
     Tester.SetVerbosity(Tester.Verbosity.FAILED_TESTS_OUTPUTS) # TESTS_OUTPUTS
-    # Tester.RunTestSuite("RansCalculationUtilities")
-    # Tester.RunTestSuite("RANSYPlusModels")
-    # Tester.RunTestSuite("RANSEvModelsKEpsilonNodalMatrices")
-    # Tester.RunTestSuite("RANSEvModelsKEpsilonGaussMatrices")
+    Tester.RunTestSuite("RANSYPlusModels")
+    Tester.RunTestSuite("RANSEvModelsKEpsilonNodalMatrices")
+    Tester.RunTestSuite("RANSEvModelsKEpsilonGaussMatrices")
     Tester.RunTestSuite("RANSEvModelsKEpsilonElementResidualMatrices")
-    Tester.RunTestSuite("RANSEvModelsKEpsilonElementResidualMatricesT1")
 
 if __name__ == '__main__':
     run()

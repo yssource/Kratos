0f51eeae2ed81c9f232f1c8c1f7a19635bf2bc85
sunethwarna <suneth.warna@gmail.com>
2019-05-24 09:59:10 +0200

    adding test_rans_utilities

--- applications/RANSModellingApplication/tests/run_cpp_unit_tests.py
+++ applications/RANSModellingApplication/tests/run_cpp_unit_tests.py
@@ -3,11 +3,11 @@ from KratosMultiphysics.RANSModellingApplication import *
 
 def run():
     Tester.SetVerbosity(Tester.Verbosity.FAILED_TESTS_OUTPUTS) # TESTS_OUTPUTS
+    Tester.RunTestSuite("RansCalculationUtilities")
     Tester.RunTestSuite("RANSYPlusModels")
     Tester.RunTestSuite("RANSEvModelsKEpsilonNodalMatrices")
     Tester.RunTestSuite("RANSEvModelsKEpsilonGaussMatrices")
     Tester.RunTestSuite("RANSEvModelsKEpsilonElementResidualMatrices")
-
 
 if __name__ == '__main__':
     run()

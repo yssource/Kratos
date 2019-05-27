fcf559b4d11cfc76da3e2a7fb8a7d06c168f7ca8
sunethwarna <suneth.warna@gmail.com>
2019-05-27 12:24:36 +0200

    relaxed_acceleration

--- applications/FluidDynamicsApplication/custom_strategies/strategies/residualbased_predictorcorrector_velocity_bossak_scheme_turbulent.h
+++ applications/FluidDynamicsApplication/custom_strategies/strategies/residualbased_predictorcorrector_velocity_bossak_scheme_turbulent.h
@@ -652,7 +652,10 @@ namespace Kratos {
                 const array_1d<double, 3 > & OldAcceleration = (itNode)->FastGetSolutionStepValue(ACCELERATION, 1);
                 const array_1d<double, 3> relaxed_acceleration = (1 - mAlphaBossak) * CurrentAcceleration
                                                                     + mAlphaBossak * OldAcceleration;
-                (itNode)->SetValue(RELAXED_ACCELERATION, relaxed_acceleration);
+                if (!itNode->SolutionStepsDataHas(RELAXED_ACCELERATION))
+                    (itNode)->SetValue(RELAXED_ACCELERATION, relaxed_acceleration);
+                else
+                    (itNode)->FastGetSolutionStepValue(RELAXED_ACCELERATION) = relaxed_acceleration;
             }
 
             //for (ModelPart::ElementsContainerType::ptr_iterator itElem = rModelPart.Elements().ptr_begin(); itElem != rModelPart.Elements().ptr_end(); ++itElem)

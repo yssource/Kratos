fafd61daa1c5b9b530b92faf2160499988cda9b1
Miguel Masó Sotomayor <mmaso@cimne.upc.edu>
2019-06-05 12:15:29 +0200

    python process

--- applications/ManufacturedFluidSolutionsApplication/python_scripts/manufactured_material_acceleration_process.py
+++ applications/ManufacturedFluidSolutionsApplication/python_scripts/manufactured_material_acceleration_process.py
@@ -1,6 +1,7 @@
 # Importing the Kratos Library
 import KratosMultiphysics as KM
 import KratosMultiphysics.ManufacturedFluidSolutionsApplication as MS
+import KratosMultiphysics.DEMApplication as DEM
 import KratosMultiphysics.SwimmingDEMApplication as SD
 
 from KratosMultiphysics.ManufacturedFluidSolutionsApplication.manufactured_solution_base_process import ManufacturedSolutionBaseProcess as ManufacturedBaseProcess
@@ -29,19 +30,19 @@ class ManufacturedMaterialAccelerationProcess(ManufacturedBaseProcess):
 
         self.framework = settings["framework"].GetString()
     
-        if self.framework = "eulerian":
+        if self.framework == "eulerian":
             recovery_parameters = KM.Parameters()
             sub_param = recovery_parameters.AddEmptyValue("variables_for_recovery")
             sub_param = sub_param.AddEmptyValue("material_derivative")
             sub_param = sub_param.AddEmptyValue("VELOCITY").SetString("MATERIAL_ACCELERATION")
             self.derivative_recoverer = SD.StandardRecoveryUtility(self.model_part, recovery_parameters)
-        if self.framework = "lagrangian":
+        if self.framework == "lagrangian":
             self.velocity_variable = KM.KratosGlobals.GetVariable("VELOCITY")
             self.acceleration_variable = KM.KratosGlobals.GetVariable("MATERIAL_ACCELERATION")
 
     def ExecuteFinalizeSolutionStep(self):
-        if self.framework = "eulerian":
-            self.derivative_recoverer.recover()
-        if self.framework = "lagrangian":
+        if self.framework == "eulerian":
+            self.derivative_recoverer.Recover()
+        if self.framework == "lagrangian":
             KM.VariableUtils().CopyVectorVar(self.velocity_variable, self.acceleration_variable, self.model_part.Nodes)
         self.manufactured_process.CopmuteMaterialAccelerationError

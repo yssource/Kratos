4e91fab7ed327a9f678b637af27bca6686db945b
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-06-27 12:44:36 +0200

    Optimization coulomb

--- applications/ContactStructuralMechanicsApplication/custom_frictional_laws/coulomb_frictional_law.cpp
+++ applications/ContactStructuralMechanicsApplication/custom_frictional_laws/coulomb_frictional_law.cpp
@@ -50,90 +50,104 @@ double CoulombFrictionalLaw<TDim,TNumNodes,TNormalVariation, TNumNodesMaster>::G
 /***********************************************************************************/
 
 template< std::size_t TDim, std::size_t TNumNodes, bool TNormalVariation, std::size_t TNumNodesMaster>
-double CoulombFrictionalLaw<TDim,TNumNodes,TNormalVariation, TNumNodesMaster>::GetDerivativeThresholdValue(
-    const NodeType& rNode,
+typename CoulombFrictionalLaw<TDim,TNumNodes,TNormalVariation, TNumNodesMaster>::DerivativesArray CoulombFrictionalLaw<TDim,TNumNodes,TNormalVariation, TNumNodesMaster>::GetDerivativesThresholdArray(
     const PairedCondition& rCondition,
     const ProcessInfo& rCurrentProcessInfo,
     const DerivativeDataType& rDerivativeData,
-    const MortarConditionMatrices& rMortarConditionMatrices,
-    const IndexType IndexDerivative,
-    const IndexType IndexNode
+    const MortarConditionMatrices& rMortarConditionMatrices
     )
 {
-    const double mu = this->GetFrictionCoefficient(rNode, rCondition, rCurrentProcessInfo);
+    // Auxiliar values
+    const double scale_factor = rCurrentProcessInfo[SCALE_FACTOR];
+    const double common_epsilon = rCurrentProcessInfo[INITIAL_PENALTY];
 
-    double derivative_threshold_value = 0.0;
+    // Mortar condition matrices - DOperator and MOperator
+    const BoundedMatrix<double, TNumNodes, TNumNodes>& DOperator = rMortarConditionMatrices.DOperator;
+    const BoundedMatrix<double, TNumNodes, TNumNodesMaster>& MOperator = rMortarConditionMatrices.MOperator;
 
-    const IndexType aux_index = IndexDerivative - TDim * (TNumNodes + TNumNodesMaster);
-    if (aux_index > 0) {
-        if (aux_index/TDim == IndexNode) {
-            const double scale_factor = rCurrentProcessInfo[SCALE_FACTOR];
-            const array_1d<double,3>& r_nodal_normal = rNode.FastGetSolutionStepValue(NORMAL);
-            derivative_threshold_value = - mu * (scale_factor * r_nodal_normal[aux_index%TDim]);
+    // Current coordinates
+    const BoundedMatrix<double, TNumNodes, TDim> x1 = rDerivativeData.u1 + rDerivativeData.X1;
+    const BoundedMatrix<double, TNumNodesMaster, TDim> x2 = rDerivativeData.u2 + rDerivativeData.X2;
+
+    // Contribution
+    const BoundedMatrix<double, TNumNodes, TDim> D_x1_M_x2 = prod(MOperator, x1) - prod(DOperator, x2);
+
+    // Auxiliar matrices
+    BoundedMatrix<double, TNumNodes, TDim> Deltax1;
+    BoundedMatrix<double, TNumNodesMaster, TDim> Deltax2;
+    BoundedMatrix<double, TNumNodes, TDim> D_Deltax1_M_Deltax2, DeltaD_x1_DeltaM_x2;
+
+    // The friction coefficient
+    DerivativesArray derivatives_threshold_values;
+    auto& r_geometry = rCondition.GetGeometry();
+
+    for (std::size_t i_node = 0; i_node < TNumNodes; ++i_node) {
+        const auto& r_node = r_geometry[i_node];
+
+        // Nodal values
+        const double epsilon = r_node.Has(INITIAL_PENALTY) ? r_node.GetValue(INITIAL_PENALTY) : common_epsilon;
+        const array_1d<double,3>& r_nodal_normal = r_node.FastGetSolutionStepValue(NORMAL);
+        const double mu = this->GetFrictionCoefficient(r_node, rCondition, rCurrentProcessInfo);
+
+        for (std::size_t i_der = 0; i_der < TDim * (2 * TNumNodes + TNumNodesMaster); ++i_der) {
+            const IndexType aux_index = i_der - TDim * (TNumNodes + TNumNodesMaster);
+            if (aux_index > 0) {
+                if (aux_index/TDim == i_node) {
+                    derivatives_threshold_values[i_der][i_node] = - mu * (scale_factor * r_nodal_normal[aux_index%TDim]);
+                }
+            } else {
+                double delta_weighted_gap = 0.0;
+
+                // Getting auxiliar indixes
+                const IndexType derivative_node_index = i_der/TDim;
+                const IndexType derivative_dimension_index = i_der%TDim;
+
+                // Mortar condition matrices - DOperator and MOperator
+                const BoundedMatrix<double, TNumNodes, TNumNodes>& DeltaMOperator = rMortarConditionMatrices.DeltaMOperator[i_der];
+                const BoundedMatrix<double, TNumNodes, TNumNodesMaster>& DeltaDOperator = rMortarConditionMatrices.DeltaDOperator[i_der];
+
+                // Some operations
+                array_1d<double, TDim> aux_normal, aux_array;
+
+                for (IndexType i_dim = 0; i_dim < TDim; ++i_dim) {
+                    aux_normal[i_dim] = r_nodal_normal[i_dim];
+                }
+
+                // Delta coordinates
+                noalias(Deltax1) = ZeroMatrix(TNumNodes, TDim);
+                noalias(Deltax2) = ZeroMatrix(TNumNodesMaster, TDim);
+
+                // Update matrix delta coordinates
+                if (derivative_node_index < TNumNodes) {
+                    Deltax1(derivative_node_index, derivative_dimension_index) = 1.0;
+                } else {
+                    Deltax2(derivative_node_index - TNumNodes, derivative_dimension_index) = 1.0;
+                }
+
+                // Adding delta contribution
+                noalias(D_Deltax1_M_Deltax2) = prod(MOperator, Deltax1) - prod(DOperator, Deltax2);
+                noalias(aux_array) = row(D_Deltax1_M_Deltax2, i_node);
+                delta_weighted_gap += inner_prod(aux_array, - aux_normal);
+
+                // Adding delta contribution
+                noalias(DeltaD_x1_DeltaM_x2) = prod(DeltaMOperator, x1) - prod(DeltaDOperator, x2);
+                noalias(aux_array) = row(DeltaD_x1_DeltaM_x2, i_node);
+                delta_weighted_gap += inner_prod(aux_array, - aux_normal);
+
+                // Delta normal contribution
+                if (TNormalVariation && (derivative_node_index < TNumNodes)) {
+                    const auto& DeltaNormalSlave = rDerivativeData.DeltaNormalSlave[i_der];
+                    noalias(aux_normal) = row(DeltaNormalSlave, i_node);
+                    noalias(aux_array) = row(D_x1_M_x2, i_node);
+                    delta_weighted_gap += inner_prod(aux_array, - aux_normal);
+                }
+
+                derivatives_threshold_values[i_der][i_node] = - mu * (epsilon * delta_weighted_gap);
+            }
         }
-    } else {
-        double delta_weighted_gap = 0.0;
-
-        // Getting auxiliar indixes
-        const IndexType derivative_node_index = IndexDerivative/TDim;
-        const IndexType derivative_dimension_index = IndexDerivative%TDim;
-
-        // Auxiliar values
-        const double common_epsilon = rCurrentProcessInfo[INITIAL_PENALTY];
-        const double epsilon = rNode.Has(INITIAL_PENALTY) ? rNode.GetValue(INITIAL_PENALTY) : common_epsilon;
-
-        // Mortar condition matrices - DOperator and MOperator
-        const BoundedMatrix<double, TNumNodes, TNumNodes>& DOperator = rMortarConditionMatrices.DOperator;
-        const BoundedMatrix<double, TNumNodes, TNumNodesMaster>& MOperator = rMortarConditionMatrices.MOperator;
-        const BoundedMatrix<double, TNumNodes, TNumNodes>& DeltaMOperator = rMortarConditionMatrices.DeltaMOperator[IndexDerivative];
-        const BoundedMatrix<double, TNumNodes, TNumNodesMaster>& DeltaDOperator = rMortarConditionMatrices.DeltaDOperator[IndexDerivative];
-
-        // Some operations
-        array_1d<double, TDim> aux_normal, aux_array;
-        const array_1d<double,3>& r_nodal_normal = rNode.FastGetSolutionStepValue(NORMAL);
-
-        for (IndexType i_dim = 0; i_dim < TDim; ++i_dim) {
-            aux_normal[i_dim] = r_nodal_normal[i_dim];
-        }
-
-        // Delta coordinates
-        BoundedMatrix<double, TNumNodes, TDim> Deltax1 = ZeroMatrix(TNumNodes, TDim);
-        BoundedMatrix<double, TNumNodesMaster, TDim> Deltax2 = ZeroMatrix(TNumNodesMaster, TDim);
-
-        // Update matrix delta coordinates
-        if (derivative_node_index < TNumNodes) {
-            Deltax1(derivative_node_index, derivative_dimension_index) = 1.0;
-        } else {
-            Deltax2(derivative_node_index - TNumNodes, derivative_dimension_index) = 1.0;
-        }
-
-        // Adding delta contribution
-        const BoundedMatrix<double, TNumNodes, TDim> D_Deltax1_M_Deltax2 = prod(MOperator, Deltax1) - prod(DOperator, Deltax2);
-        noalias(aux_array) = row(D_Deltax1_M_Deltax2, IndexNode);
-        delta_weighted_gap += inner_prod(aux_array, - aux_normal);
-
-        // Current coordinates
-        const BoundedMatrix<double, TNumNodes, TDim> x1 = rDerivativeData.u1 + rDerivativeData.X1;
-        const BoundedMatrix<double, TNumNodesMaster, TDim> x2 = rDerivativeData.u2 + rDerivativeData.X2;
-
-        // Adding delta contribution
-        const BoundedMatrix<double, TNumNodes, TDim> DeltaD_x1_DeltaM_x2 = prod(DeltaMOperator, x1) - prod(DeltaDOperator, x2);
-        noalias(aux_array) = row(DeltaD_x1_DeltaM_x2, IndexNode);
-        delta_weighted_gap += inner_prod(aux_array, - aux_normal);
-
-        // Delta normal contribution
-        if (TNormalVariation && (derivative_node_index < TNumNodes)) {
-            const auto& DeltaNormalSlave = rDerivativeData.DeltaNormalSlave[IndexDerivative];
-            noalias(aux_normal) = row(DeltaNormalSlave, IndexNode);
-            const BoundedMatrix<double, TNumNodes, TDim> D_x1_M_x2 = prod(MOperator, x1) - prod(DOperator, x2);
-            noalias(aux_array) = row(D_x1_M_x2, IndexNode);
-            delta_weighted_gap += inner_prod(aux_array, - aux_normal);
-        }
-
-        derivative_threshold_value = - mu * (epsilon * delta_weighted_gap);
     }
 
-    return derivative_threshold_value;
+    return derivatives_threshold_values;
 }
 
 /***********************************************************************************/

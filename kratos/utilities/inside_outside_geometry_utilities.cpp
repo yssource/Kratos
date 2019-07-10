9a86f43308fb8838734e8d81cb0f30d003086dae
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2019-07-10 15:52:06 +0200

    i was repeated index

--- kratos/utilities/inside_outside_geometry_utilities.cpp
+++ kratos/utilities/inside_outside_geometry_utilities.cpp
@@ -114,16 +114,16 @@ bool IsInside2D(
         delaunator::Delaunator delaunator(rInnerLoops[i]);
         const auto& r_triangles = delaunator.triangles;
 
-        for (std::size_t i = 0; i < r_triangles.size(); i += 3) {
+        for (std::size_t j = 0; j < r_triangles.size(); j += 3) {
             const bool check_is_inside = IsInsideTriangle2D(
                 LocationX,
                 LocationY,
-                rInnerLoops[i][2 * r_triangles[i]],
-                rInnerLoops[i][2 * r_triangles[i] + 1],
-                rInnerLoops[i][2 * r_triangles[i + 1]],
-                rInnerLoops[i][2 * r_triangles[i + 1] + 1],
-                rInnerLoops[i][2 * r_triangles[i + 2]],
-                rInnerLoops[i][2 * r_triangles[i + 2] + 1]
+                rInnerLoops[i][2 * r_triangles[j]],
+                rInnerLoops[i][2 * r_triangles[j] + 1],
+                rInnerLoops[i][2 * r_triangles[j + 1]],
+                rInnerLoops[i][2 * r_triangles[j + 1] + 1],
+                rInnerLoops[i][2 * r_triangles[j + 2]],
+                rInnerLoops[i][2 * r_triangles[j + 2] + 1]
             );
             if (check_is_inside)
                 return false;
@@ -133,16 +133,16 @@ bool IsInside2D(
         delaunator::Delaunator delaunator(rOuterLoops[i]);
         const auto& r_triangles = delaunator.triangles;
 
-        for (std::size_t i = 0; i < r_triangles.size(); i += 3) {
+        for (std::size_t j = 0; j < r_triangles.size(); j += 3) {
             const bool check_is_inside = IsInsideTriangle2D(
                 LocationX,
                 LocationY,
-                rOuterLoops[i][2 * r_triangles[i]],
-                rOuterLoops[i][2 * r_triangles[i] + 1],
-                rOuterLoops[i][2 * r_triangles[i + 1]],
-                rOuterLoops[i][2 * r_triangles[i + 1] + 1],
-                rOuterLoops[i][2 * r_triangles[i + 2]],
-                rOuterLoops[i][2 * r_triangles[i + 2] + 1]
+                rOuterLoops[i][2 * r_triangles[j]],
+                rOuterLoops[i][2 * r_triangles[j] + 1],
+                rOuterLoops[i][2 * r_triangles[j + 1]],
+                rOuterLoops[i][2 * r_triangles[j + 1] + 1],
+                rOuterLoops[i][2 * r_triangles[j + 2]],
+                rOuterLoops[i][2 * r_triangles[j + 2] + 1]
             );
             if (check_is_inside)
                 return true;

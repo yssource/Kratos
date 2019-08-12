corrected scaling to norm inf
before it was max norm (but wrongly implemented)

applications/ShapeOptimizationApplication/python_scripts/cad_reconstruction_condition_factory.py
@@ -252,7 +252,7 @@ class ConditionFactory:
             list_of_points, list_of_parameters, list_of_integration_weights = self.__CreateIntegrationPointsForFace(face_i, drawing_tolerance)
 
             # Create conditions
-            for (x,y,z), (u,v), weight in zip(list_of_points, list_of_parameters, list_of_integration_weights):
+            for (x,y,z), (u,v), integration_weight in zip(list_of_points, list_of_parameters, list_of_integration_weights):
 
                 nonzero_indices, shape_functions = surface_geometry_data.ShapeFunctionsAt(u, v, order=2)
                 nonzero_pole_nodes = [pole_nodes[i] for i in nonzero_indices]
@@ -261,8 +261,7 @@ class ConditionFactory:
                 # point_ptr.Attributes().SetLayer('IntegrationPoints_'+str(face_i.Key()))
 
                 if apply_kl_shell:
-                    weight = shell_penalty_fac * weight
-                    new_condition = clib.KLShellConditionWithAD(nonzero_pole_nodes, shape_functions, weight)
+                    new_condition = clib.KLShellConditionWithAD(nonzero_pole_nodes, shape_functions, shell_penalty_fac, integration_weight)
                     conditions.append(new_condition)
 
     # --------------------------------------------------------------------------
@@ -408,9 +407,8 @@ class ConditionFactory:
                     # Positions enforcement
                     if penalty_factor_position_enforcement > 0:
                         target_position = integration_point + np.array(target_displacement)
-                        weight = penalty_factor_position_enforcement * integration_weight
 
-                        new_condition = clib.PositionEnforcementCondition(target_position, nonzero_pole_nodes, shape_functions, weight)
+                        new_condition = clib.PositionEnforcementCondition(target_position, nonzero_pole_nodes, shape_functions, penalty_factor_position_enforcement, integration_weight)
                         conditions.append(new_condition)
 
             elif num_adjacent_faces == 2:
@@ -481,23 +479,21 @@ class ConditionFactory:
                     if penalty_factor_position_enforcement > 0:
                         target_displacement = fe_node.GetSolutionStepValue(KratosShape.SHAPE_CHANGE)
                         target_position = np.array([fe_node.X+target_displacement[0], fe_node.Y+target_displacement[1], fe_node.Z+target_displacement[2]])
-                        weight = penalty_factor_position_enforcement * integration_weight
 
-                        new_condition_a = clib.PositionEnforcementCondition(target_position, nonzero_pole_nodes_a, shape_functions_a, weight)
+                        new_condition_a = clib.PositionEnforcementCondition(target_position, nonzero_pole_nodes_a, shape_functions_a, penalty_factor_position_enforcement, integration_weight)
                         position_conditions.append(new_condition_a)
 
-                        new_condition_b = clib.PositionEnforcementCondition(target_position, nonzero_pole_nodes_b, shape_functions_b, weight)
+                        new_condition_b = clib.PositionEnforcementCondition(target_position, nonzero_pole_nodes_b, shape_functions_b, penalty_factor_position_enforcement, integration_weight)
                         position_conditions.append(new_condition_b)
 
                     # Tangents enforcement
                     if penalty_factor_tangent_enforcement > 0:
                         target_normal = fe_node.GetSolutionStepValue(KratosShape.NORMALIZED_SURFACE_NORMAL)
-                        weight = penalty_factor_tangent_enforcement * integration_weight
 
-                        new_condition_a = clib.TangentEnforcementConditionWithAD(target_normal, nonzero_pole_nodes_a, shape_functions_a, weight)
+                        new_condition_a = clib.TangentEnforcementConditionWithAD(target_normal, nonzero_pole_nodes_a, shape_functions_a, penalty_factor_tangent_enforcement, integration_weight)
                         tangent_conditions.append(new_condition_a)
 
-                        new_condition_b = clib.TangentEnforcementConditionWithAD(target_normal, nonzero_pole_nodes_b, shape_functions_b, weight)
+                        new_condition_b = clib.TangentEnforcementConditionWithAD(target_normal, nonzero_pole_nodes_b, shape_functions_b, penalty_factor_tangent_enforcement, integration_weight)
                         tangent_conditions.append(new_condition_b)
             else:
                 raise RuntimeError("Max number of adjacent has to be 2!!")
@@ -608,17 +604,15 @@ class ConditionFactory:
 
             # Tangents enforcement
             target_normal = fe_node.GetSolutionStepValue(KratosShape.NORMALIZED_SURFACE_NORMAL)
-            weight = penalty_factor * integration_weight
 
-            new_condition = clib.TangentEnforcementConditionWithAD(target_normal, nonzero_pole_nodes, shape_functions, weight)
+            new_condition = clib.TangentEnforcementConditionWithAD(target_normal, nonzero_pole_nodes, shape_functions, penalty_factor, integration_weight)
             tangent_conditions.append(new_condition)
 
             # Positions enforcement
             target_displacement = fe_node.GetSolutionStepValue(KratosShape.SHAPE_CHANGE)
             target_position = np.array([fe_node.X+target_displacement[0], fe_node.Y+target_displacement[1], fe_node.Z+target_displacement[2]])
-            weight = penalty_factor * integration_weight
 
-            new_condition = clib.PositionEnforcementCondition(target_position, nonzero_pole_nodes, shape_functions, weight)
+            new_condition = clib.PositionEnforcementCondition(target_position, nonzero_pole_nodes, shape_functions, penalty_factor, integration_weight)
             position_conditions.append(new_condition)
 
     # --------------------------------------------------------------------------
@@ -670,8 +664,6 @@ class ConditionFactory:
 
                     # displacement coupling condition
                     if penalty_factor_displacement > 0:
-                        weight = penalty_factor_displacement * integration_weight
-
                         new_condition = clib.DisplacementCouplingCondition( nonzero_pole_nodes_a,
                                                                             nonzero_pole_nodes_b,
                                                                             shape_functions_a,
@@ -680,15 +672,14 @@ class ConditionFactory:
                                                                             surface_geometry_b,
                                                                             (u_a, v_a),
                                                                             (u_b, v_b),
-                                                                            weight )
+                                                                            penalty_factor_displacement,
+                                                                            integration_weight )
                         displacement_conditions.append(new_condition)
 
                     # rotation coupling condition
                     if penalty_factor_rotation > 0:
                         _, T2_edge = edge_curve_a.DerivativesAt(t_a, order=1)
                         T2_edge /=   np.linalg.norm(T2_edge)
-
-                        weight = penalty_factor_rotation * integration_weight
 
                         new_condition = clib.RotationCouplingConditionWithAD( nonzero_pole_nodes_a,
                                                                               nonzero_pole_nodes_b,
@@ -699,7 +690,8 @@ class ConditionFactory:
                                                                               surface_geometry_b,
                                                                               (u_a, v_a),
                                                                               (u_b, v_b),
-                                                                              weight )
+                                                                              penalty_factor_rotation,
+                                                                              integration_weight )
                         rotation_conditions.append(new_condition)
             else:
                 raise RuntimeError("Max number of adjacent has to be 2!!")

applications/ShapeOptimizationApplication/python_scripts/cad_reconstruction_conditions.py
@@ -29,8 +29,11 @@ class ReconstructionCondition(eq.Element):
         return dof_list
 
     # --------------------------------------------------------------------------
-    def GetWeight(self):
-        return self.weight
+    def GetPenaltyFactor(self):
+        if hasattr(self, 'penalty_factor'):
+            return self.penalty_factor
+        else:
+            raise RuntimeError("Trying to get a penalty factor for a condition that does not have any!!")
 
     # --------------------------------------------------------------------------
     @staticmethod
@@ -182,21 +185,22 @@ class DistanceMinimizationConditionWithAD(ReconstructionConditionWithAD):
 # ==============================================================================
 class PositionEnforcementCondition(ReconstructionCondition):
     # --------------------------------------------------------------------------
-    def __init__(self, target_position, pole_nodes, shape_functions, weight):
+    def __init__(self, target_position, pole_nodes, shape_functions, penalty_factor, weight):
         super().__init__()
 
         self.target_position = target_position
         self.pole_nodes = pole_nodes
         self.shape_function_values = np.asarray(shape_functions[0], float)
+        self.penalty_factor = penalty_factor
         self.weight = weight
 
     # --------------------------------------------------------------------------
     def CalculateRHS(self, rhs):
         cad_location = super().ComputeActual(self.pole_nodes, self.shape_function_values)
         distance_vec =  cad_location - self.target_position
-        value = 0.5 * self.weight * distance_vec.dot(distance_vec)
+        value = 0.5 * self.penalty_factor * self.weight * distance_vec.dot(distance_vec)
 
-        local_rhs = - self.weight * np.outer(self.shape_function_values, distance_vec)
+        local_rhs = - self.penalty_factor * self.weight * np.outer(self.shape_function_values, distance_vec)
 
         rhs[0::3] = local_rhs[:,0]
         rhs[1::3] = local_rhs[:,1]
@@ -209,7 +213,7 @@ class PositionEnforcementCondition(ReconstructionCondition):
         num_dofs = len(self.shape_function_values)*3
 
         # LHS
-        local_lhs_block = self.weight * np.outer(self.shape_function_values, self.shape_function_values)
+        local_lhs_block = self.penalty_factor * self.weight * np.outer(self.shape_function_values, self.shape_function_values)
 
         lhs[:] = np.zeros([num_dofs,num_dofs])
         lhs[0::3,0::3] = local_lhs_block
@@ -230,13 +234,14 @@ class PositionEnforcementCondition(ReconstructionCondition):
 # ==============================================================================
 class TangentEnforcementConditionWithAD( ReconstructionConditionWithAD ):
     # --------------------------------------------------------------------------
-    def __init__(self, target_normal, pole_nodes, shape_functions, weight):
+    def __init__(self, target_normal, pole_nodes, shape_functions, penalty_factor, weight):
         super().__init__()
 
         self.target_normal = target_normal
         self.pole_nodes = pole_nodes
         self.shape_function_derivatives_u = np.asarray(shape_functions[1], float)
         self.shape_function_derivatives_v = np.asarray(shape_functions[2], float)
+        self.penalty_factor = penalty_factor
         self.weight = weight
 
     # --------------------------------------------------------------------------
@@ -244,8 +249,8 @@ class TangentEnforcementConditionWithAD( ReconstructionConditionWithAD ):
         a1 = super().ComputeActualJet(self.pole_nodes, self.shape_function_derivatives_u)
         a2 = super().ComputeActualJet(self.pole_nodes, self.shape_function_derivatives_v)
 
-        f_1 = self.weight * np.dot(a1,self.target_normal)**2 * 0.5
-        f_2 = self.weight * np.dot(a2,self.target_normal)**2 * 0.5
+        f_1 = self.penalty_factor * self.weight * np.dot(a1,self.target_normal)**2 * 0.5
+        f_2 = self.penalty_factor * self.weight * np.dot(a2,self.target_normal)**2 * 0.5
         value = f_1.f + f_2.f
 
         rhs[:]  = - f_1.g
@@ -258,8 +263,8 @@ class TangentEnforcementConditionWithAD( ReconstructionConditionWithAD ):
         a1 = super().ComputeActualHyperJet(self.pole_nodes, self.shape_function_derivatives_u)
         a2 = super().ComputeActualHyperJet(self.pole_nodes, self.shape_function_derivatives_v)
 
-        f_1 = self.weight * np.dot(a1,self.target_normal)**2 * 0.5
-        f_2 = self.weight * np.dot(a2,self.target_normal)**2 * 0.5
+        f_1 = self.penalty_factor * self.weight * np.dot(a1,self.target_normal)**2 * 0.5
+        f_2 = self.penalty_factor * self.weight * np.dot(a2,self.target_normal)**2 * 0.5
         value = f_1.f + f_2.f
 
         # LHS
@@ -281,7 +286,7 @@ class TangentEnforcementConditionWithAD( ReconstructionConditionWithAD ):
 # ==============================================================================
 class DisplacementCouplingCondition(ReconstructionCondition):
     # --------------------------------------------------------------------------
-    def __init__(self, pole_nodes_a, pole_nodes_b, shape_functions_a, shape_functions_b, geometry_a, geometry_b, parameters_a, parameters_b,  weight):
+    def __init__(self, pole_nodes_a, pole_nodes_b, shape_functions_a, shape_functions_b, geometry_a, geometry_b, parameters_a, parameters_b, penalty_factor, weight):
         super().__init__()
 
         self.pole_nodes_a = pole_nodes_a
@@ -292,6 +297,7 @@ class DisplacementCouplingCondition(ReconstructionCondition):
         self.geometry_b = geometry_b # only strored for aposteriori refinement
         self.parameters_a = parameters_a # only strored for aposteriori refinement
         self.parameters_b = parameters_b # only strored for aposteriori refinement
+        self.penalty_factor = penalty_factor
         self.weight = weight
 
         self.pos_0_a = super().ComputeActual(pole_nodes_a, self.shape_function_values_a)
@@ -332,7 +338,7 @@ class DisplacementCouplingCondition(ReconstructionCondition):
         disp_b = super().ComputeActual(self.pole_nodes_b, self.shape_function_values_b) - self.pos_0_b
         delta = disp_a - disp_b
 
-        value = 0.5  * self.weight * delta.dot(delta)
+        value = 0.5  * self.penalty_factor * self.weight * delta.dot(delta)
 
         # a
         delta_ab = np.zeros(num_dofs)
@@ -345,7 +351,7 @@ class DisplacementCouplingCondition(ReconstructionCondition):
         delta_ab[3*num_pole_nodes_a+1::3] = delta[1]
         delta_ab[3*num_pole_nodes_a+2::3] = delta[2]
 
-        rhs[:] = - self.weight * np.multiply(N_ab, delta_ab)
+        rhs[:] = - self.penalty_factor * self.weight * np.multiply(N_ab, delta_ab)
 
         return value
 
@@ -358,25 +364,25 @@ class DisplacementCouplingCondition(ReconstructionCondition):
         lhs[:] = np.zeros([num_dofs,num_dofs])
 
         # aa
-        local_lhs_aa = self.weight * np.outer(self.shape_function_values_a, self.shape_function_values_a)
+        local_lhs_aa = self.penalty_factor * self.weight * np.outer(self.shape_function_values_a, self.shape_function_values_a)
         lhs[0:num_pole_nodes_a*3:3 , 0:num_pole_nodes_a*3:3] = local_lhs_aa
         lhs[1:num_pole_nodes_a*3:3 , 1:num_pole_nodes_a*3:3] = local_lhs_aa
         lhs[2:num_pole_nodes_a*3:3 , 2:num_pole_nodes_a*3:3] = local_lhs_aa
 
         # ab
-        local_lhs_ab = - self.weight * np.outer(self.shape_function_values_a, self.shape_function_values_b)
+        local_lhs_ab = - self.penalty_factor * self.weight * np.outer(self.shape_function_values_a, self.shape_function_values_b)
         lhs[0:num_pole_nodes_a*3:3 , 3*num_pole_nodes_a+0::3] = local_lhs_ab
         lhs[1:num_pole_nodes_a*3:3 , 3*num_pole_nodes_a+1::3] = local_lhs_ab
         lhs[2:num_pole_nodes_a*3:3 , 3*num_pole_nodes_a+2::3] = local_lhs_ab
 
         # ba
-        local_lsh_ba = - self.weight * np.outer(self.shape_function_values_b, self.shape_function_values_a)
+        local_lsh_ba = - self.penalty_factor * self.weight * np.outer(self.shape_function_values_b, self.shape_function_values_a)
         lhs[3*num_pole_nodes_a+0::3 , 0:num_pole_nodes_a*3:3] = local_lsh_ba
         lhs[3*num_pole_nodes_a+1::3 , 1:num_pole_nodes_a*3:3] = local_lsh_ba
         lhs[3*num_pole_nodes_a+2::3 , 2:num_pole_nodes_a*3:3] = local_lsh_ba
 
         # bb
-        local_lhs_bb = self.weight * np.outer(self.shape_function_values_b, self.shape_function_values_b)
+        local_lhs_bb = self.penalty_factor * self.weight * np.outer(self.shape_function_values_b, self.shape_function_values_b)
         lhs[3*num_pole_nodes_a+0::3 , 3*num_pole_nodes_a+0::3] = local_lhs_bb
         lhs[3*num_pole_nodes_a+1::3 , 3*num_pole_nodes_a+1::3] = local_lhs_bb
         lhs[3*num_pole_nodes_a+2::3 , 3*num_pole_nodes_a+2::3] = local_lhs_bb
@@ -395,7 +401,7 @@ class DisplacementCouplingCondition(ReconstructionCondition):
 # ==============================================================================
 class DisplacementCouplingConditionWithAD(ReconstructionConditionWithAD):
     # --------------------------------------------------------------------------
-    def __init__(self, pole_nodes_a, pole_nodes_b, shape_functions_a, shape_functions_b, geometry_a, geometry_b, parameters_a, parameters_b,  weight):
+    def __init__(self, pole_nodes_a, pole_nodes_b, shape_functions_a, shape_functions_b, geometry_a, geometry_b, parameters_a, parameters_b, penalty_factor, weight):
         super().__init__()
 
         self.pole_nodes_a = pole_nodes_a
@@ -406,6 +412,7 @@ class DisplacementCouplingConditionWithAD(ReconstructionConditionWithAD):
         self.geometry_b = geometry_b # only strored for aposteriori refinement
         self.parameters_a = parameters_a # only strored for aposteriori refinement
         self.parameters_b = parameters_b # only strored for aposteriori refinement
+        self.penalty_factor = penalty_factor
         self.weight = weight
 
         self.pos_0_a = super().ComputeActual(pole_nodes_a, self.shape_function_values_a)
@@ -439,7 +446,7 @@ class DisplacementCouplingConditionWithAD(ReconstructionConditionWithAD):
 
         position_difference = pos_a - pos_b
 
-        f = 0.5  * self.weight * position_difference.dot(position_difference)
+        f = 0.5  * self.penalty_factor * self.weight * position_difference.dot(position_difference)
 
         rhs[:] = -f.g
         return f.f
@@ -457,7 +464,7 @@ class DisplacementCouplingConditionWithAD(ReconstructionConditionWithAD):
 
         position_difference = pos_a - pos_b
 
-        f = 0.5 * self.weight * position_difference.dot(position_difference)
+        f = 0.5 * self.penalty_factor * self.weight * position_difference.dot(position_difference)
 
         rhs[:] = -f.g
         lhs[:] = f.h
@@ -474,7 +481,7 @@ class RotationCouplingConditionWithAD(ReconstructionConditionWithAD):
     # Note that this condition considers the rotation around a common tangent vector from one of the two coupled faces (T2_edge)
     # This is different to the classical IGA coupling where the rotation is calculated on the individual tangents (which requires a direction/sign check)
     # --------------------------------------------------------------------------
-    def __init__(self, pole_nodes_a, pole_nodes_b, T2_edge, shape_functions_a, shape_functions_b, geometry_a, geometry_b, parameters_a, parameters_b, weight):
+    def __init__(self, pole_nodes_a, pole_nodes_b, T2_edge, shape_functions_a, shape_functions_b, geometry_a, geometry_b, parameters_a, parameters_b, penalty_factor, weight):
         super().__init__()
 
         self.pole_nodes_a = pole_nodes_a
@@ -488,6 +495,7 @@ class RotationCouplingConditionWithAD(ReconstructionConditionWithAD):
         self.geometry_b = geometry_b # only strored for aposteriori refinement
         self.parameters_a = parameters_a # only strored for aposteriori refinement
         self.parameters_b = parameters_b # only strored for aposteriori refinement
+        self.penalty_factor = penalty_factor
         self.weight = weight
 
         A1_a = super().ComputeActual(self.pole_nodes_a, self.shape_function_derivatives_u_a)
@@ -561,7 +569,7 @@ class RotationCouplingConditionWithAD(ReconstructionConditionWithAD):
 
         angular_difference = angle_a - angle_b
 
-        f = 0.5 * self.weight * angular_difference**2
+        f = 0.5 * self.penalty_factor * self.weight * angular_difference**2
 
         rhs[:] = -f.g
         return f.f
@@ -595,7 +603,7 @@ class RotationCouplingConditionWithAD(ReconstructionConditionWithAD):
 
         angular_difference = angle_a - angle_b
 
-        f = 0.5 * self.weight * angular_difference**2
+        f = 0.5 * self.penalty_factor * self.weight * angular_difference**2
 
         rhs[:] = -f.g
         lhs[:] = f.h
@@ -610,7 +618,7 @@ class RotationCouplingConditionWithAD(ReconstructionConditionWithAD):
 # ==============================================================================
 class KLShellConditionWithAD( ReconstructionConditionWithAD ):
     # --------------------------------------------------------------------------
-    def __init__(self, pole_nodes, shape_function_values_and_dervatives, weight):
+    def __init__(self, pole_nodes, shape_function_values_and_dervatives, penalty_factor, weight):
         super().__init__()
 
         self.pole_nodes = pole_nodes
@@ -619,6 +627,7 @@ class KLShellConditionWithAD( ReconstructionConditionWithAD ):
         self.shape_function_derivatives_uu = np.asarray(shape_function_values_and_dervatives[3], float)
         self.shape_function_derivatives_uv = np.asarray(shape_function_values_and_dervatives[4], float)
         self.shape_function_derivatives_vv = np.asarray(shape_function_values_and_dervatives[5], float)
+        self.penalty_factor = penalty_factor
         self.weight = weight
 
         # Reference configuration
@@ -701,7 +710,7 @@ class KLShellConditionWithAD( ReconstructionConditionWithAD ):
         n = np.dot(eps, self.Dm)
         m = np.dot(kap, self.Db)
 
-        f = 0.5 * self.weight * (np.dot(eps, n) + np.dot(kap, m))
+        f = self.penalty_factor * self.weight * (np.dot(eps, n) + np.dot(kap, m))
 
         rhs[:] = -f.g
         return f.f
@@ -729,7 +738,7 @@ class KLShellConditionWithAD( ReconstructionConditionWithAD ):
         n = np.dot(eps, self.Dm)
         m = np.dot(kap, self.Db)
 
-        f = 0.5 * self.weight * (np.dot(eps, n) + np.dot(kap, m))
+        f = self.penalty_factor * self.weight * (np.dot(eps, n) + np.dot(kap, m))
 
         rhs[:] = -f.g
         lhs[:] = f.h
@@ -752,7 +761,7 @@ class AlphaRegularizationCondition(ReconstructionCondition):
         self.geometry_data = surface_geometry.Data()
         self.pole_nodes = pole_nodes
         self.shape_function_values = np.asarray(shape_functions[0], float)
-        self.weight = alpha
+        self.penalty_factor = alpha
 
         r = target_pole_rs[0]
         s = target_pole_rs[1]
@@ -771,12 +780,12 @@ class AlphaRegularizationCondition(ReconstructionCondition):
         distance_vec = pole_coords-greville_point_coords
         distance_vec = np.array(distance_vec)
 
-        value = 0.5 * self.weight * distance_vec.dot(distance_vec)
+        value = 0.5 * self.penalty_factor * distance_vec.dot(distance_vec)
 
         # RHS
-        rhs[0::3] = -self.weight * self.IminN * distance_vec[0]
-        rhs[1::3] = -self.weight * self.IminN * distance_vec[1]
-        rhs[2::3] = -self.weight * self.IminN * distance_vec[2]
+        rhs[0::3] = -self.penalty_factor * self.IminN * distance_vec[0]
+        rhs[1::3] = -self.penalty_factor * self.IminN * distance_vec[1]
+        rhs[2::3] = -self.penalty_factor * self.IminN * distance_vec[2]
 
         return value
 
@@ -785,7 +794,7 @@ class AlphaRegularizationCondition(ReconstructionCondition):
         num_dofs = len(self.shape_function_values)*3
 
         # LHS
-        local_lhs_block = self.weight * np.outer(self.IminN, self.IminN)
+        local_lhs_block = self.penalty_factor * np.outer(self.IminN, self.IminN)
 
         lhs[:] = np.zeros([num_dofs,num_dofs])
         lhs[0::3,0::3] = local_lhs_block

applications/ShapeOptimizationApplication/python_scripts/cad_reconstruction_mapper.py
@@ -24,6 +24,7 @@ from cad_reconstruction_condition_factory import ConditionFactory
 import ANurbs as an
 import numpy as np
 import numpy.linalg as la
+import scipy.sparse.linalg as sla
 import time, os, shutil
 import EQlib as eq
 
@@ -581,15 +582,20 @@ class CADMapper:
         self.conditions_scaling_factors.append(1)
 
         if is_initial_assembly:
-            system_max = abs(self.system.working_h).max()
-            print("> Max absolute system LHS = " + str(system_max))
+            system_norm_inf = sla.norm(self.system.working_h, np.inf)
+
+            if self.parameters["output"]["echo_level"].GetInt() > 1:
+                print("> Max absolute value system LHS = " + str(abs(self.system.working_h).max()))
+                print("> Norm infinity of system LHS = " + str(system_norm_inf))
+                print("> Max value system LHS = " + str((self.system.working_h).max()))
+                print("> Min value system LHS = " + str((self.system.working_h).min()))
 
         # Beta regularization
         beta = self.parameters["regularization"]["beta"].GetDouble()
 
         if is_initial_assembly:
-            max_beta = 1
-            self.beta_scaling_factor = system_max/max_beta
+            beta_norm_inf = 1
+            self.beta_scaling_factor = system_norm_inf/beta_norm_inf
             print("> beta_scaling_factor = "+str(self.beta_scaling_factor)+"\n")
 
         self.system.add_diagonal(self.beta_scaling_factor*beta)
@@ -600,14 +606,21 @@ class CADMapper:
             self.system.compute(index=itr, parallel=self.parameters["solution"]["parallel_assembly"].GetBool())
 
             if is_initial_assembly:
-                weight_of_element_type = self.conditions[itr][0].GetWeight()
-                constraint_max = abs(self.system.working_h).max() / weight_of_element_type
-                scaling_factor = system_max/constraint_max
+                penalty_fac_of_element_type = self.conditions[itr][0].GetPenaltyFactor()
 
+                constraint_norm_inf = sla.norm(self.system.working_h, np.inf) / penalty_fac_of_element_type
+
+                scaling_factor = system_norm_inf/constraint_norm_inf
                 self.conditions_scaling_factors.append(scaling_factor)
 
-                print("> scaling_factor_"+str(itr)+" = "+str(scaling_factor))
-                print("> Max absolute value constraint LHS_"+str(itr)+"= " + str(constraint_max)+"\n")
+                if self.parameters["output"]["echo_level"].GetInt() > 1:
+                    print("> Infinity norm constraint LHS_"+str(itr)+" before scaling without penalty factor = " + str(constraint_norm_inf))
+                    print("> Infinity norm constraint LHS_"+str(itr)+" after scaling with penalty factor= " + str(penalty_fac_of_element_type*scaling_factor*constraint_norm_inf))
+                    constraint_max = abs(self.system.working_h).max() / penalty_fac_of_element_type
+                    print("> Max absolute value constraint LHS_"+str(itr)+" before scaling without penalty factor = " + str(constraint_max))
+                    print("> Max absolute value constraint LHS_"+str(itr)+" after scaling with penalty factor = " + str(penalty_fac_of_element_type*scaling_factor*constraint_max))
+                    print("> penalty_factor_"+str(itr)+" = "+str(penalty_fac_of_element_type))
+                    print("> scaling_factor_"+str(itr)+" = "+str(scaling_factor)+"\n")
 
             self.system.add_working_system(factor=self.conditions_scaling_factors[itr])
 
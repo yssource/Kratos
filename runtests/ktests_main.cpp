d915efa64d400d02c6c03c170118746d5d404bb9
Carlos Roig <roigcarlo@gmail.com>
2019-08-13 10:54:00 +0200

    Initializing kernel

--- runtests/ktests_main.cpp
+++ runtests/ktests_main.cpp
@@ -1,7 +1,11 @@
+#include "includes/kernel.h"
 #include "testing/tester.h"
 
 int main(int argc, char *argv[])
 {
+    Kratos::Kernel mKernel;
+    mKernel.Initialize();
+
     int error_code = Kratos::Testing::Tester::RunAllTestCases();
 
     if(error_code != 0)

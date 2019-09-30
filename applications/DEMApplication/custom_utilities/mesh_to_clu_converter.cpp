cf7f43565840341ba6c249ae66bdd79d0371bcdc
Joaquín <jirazabal@cimne.upc.edu>
2019-09-30 18:23:02 +0200

    Fix file

--- applications/DEMApplication/custom_utilities/mesh_to_clu_converter.cpp
+++ applications/DEMApplication/custom_utilities/mesh_to_clu_converter.cpp
@@ -1,4 +1,4 @@
-// Translated to C++ from the original work in FORTRAN by Alberto F�rriz, 2016
+// Translated to C++ from the original work in FORTRAN by Alberto Ferriz, 2016
 
 #include <iostream>
 #include <fstream>
@@ -10,20 +10,20 @@
 void Diagonalize(const double (&A)[3][3], double (&Q)[3][3], double (&D)[3][3]);
 
 int main() {
-    std::ifstream infile("ballast6cluster3Dred.msh");
-    std::ifstream infilesph("ballast6cluster3Dred.sph");
+    std::ifstream infile("M6x25.msh");
+    std::ifstream infilesph("M6x25_120spheres.sph");
     std::string line, linesph;
     infile.ignore(80,'\n'); infile.ignore(80,'\n');
 
     // Hacer una primera pasada para saber NUM_OF_NODES y NUM_OF_ELEMENTS
     // Pasar el nombre del caso como argumento de un script de python y crear el fichero clu
     // Poner esto el PreUtilities?
-    // Salta un segmentation fault al reservar la memoria de los arrays para grandes tama�os
-    // Deber�a poderse optimizar el uso de la memoria para evitar eso
-    // Primera aproximaci�n de Size: Longitud de la diagonal m�xima del prisma contenedor
-    // Mover el centroide del objeto al origen si no lo est� ya
+    // Salta un segmentation fault al reservar la memoria de los arrays para grandes tamanos
+    // Deberia poderse optimizar el uso de la memoria para evitar eso
+    // Primera aproximacion de Size: Longitud de la diagonal maxima del prisma contenedor
+    // Mover el centroide del objeto al origen si no lo esta ya
     // Para el clu, se necesita la inercia por unidad de masa
-    // Entonces, mover el objeto al origen y girarlo tal que est� en sus ejes principales
+    // Entonces, mover el objeto al origen y girarlo tal que esta en sus ejes principales
 
     int node_counter = 0;
     while (std::getline(infile, line)) {
@@ -34,7 +34,7 @@ int main() {
             node_counter++;
         } else break;
     }
-    KRATOS_INFO("DEM") << "\nNumber of nodes: " << node_counter << '\n';
+    std::cout << "\nNumber of nodes: " << node_counter << '\n';
 
     infile.ignore(80,'\n'); infile.ignore(80,'\n');
 
@@ -47,7 +47,7 @@ int main() {
             element_counter++;
         } else break;
     }
-    KRATOS_INFO("DEM") << "Number of elements: " << element_counter << '\n';
+    std::cout << "Number of elements: " << element_counter << '\n';
 
     int spheres_counter = 0;
     while (std::getline(infilesph, linesph)) {
@@ -57,7 +57,7 @@ int main() {
             spheres_counter++;
         } else break;
     }
-    KRATOS_INFO("DEM") << "Number of spheres: " << spheres_counter << '\n';
+    std::cout << "Number of spheres: " << spheres_counter << '\n';
 
     infile.seekg(0, std::ios::beg);
     infile.ignore(80,'\n'); infile.ignore(80,'\n');
@@ -76,8 +76,8 @@ int main() {
     double I[3][3];
     double Q[3][3];
     double D[3][3];
-
     node_counter = 0;
+
     while (std::getline(infile, line)) {
         std::istringstream iss(line);
         int a;
@@ -150,22 +150,22 @@ int main() {
         total_volume += Volum[element_counter];
         Vmass[element_counter] = Volum[element_counter]*density;
 
-        //C�lculo del baricentro de cada tetraedro
+        //Calculo del baricentro de cada tetraedro
         BARIC[0][element_counter]= (tcoord[0][NodeA-1]+tcoord[0][NodeB-1]+tcoord[0][NodeC-1]+tcoord[0][NodeD-1])/4;
         BARIC[1][element_counter]= (tcoord[1][NodeA-1]+tcoord[1][NodeB-1]+tcoord[1][NodeC-1]+tcoord[1][NodeD-1])/4;
         BARIC[2][element_counter]= (tcoord[2][NodeA-1]+tcoord[2][NodeB-1]+tcoord[2][NodeC-1]+tcoord[2][NodeD-1])/4;
     }
 
-    KRATOS_INFO("DEM") << "\nTotal volume: " << total_volume << '\n';
+    std::cout << "\nTotal volume: " << total_volume << '\n';
 
-    //C�lculo de la masa total del cluster o piedra
+    //Calculo de la masa total del cluster o piedra
     double Vmaspiedra=0.0;
     for (int element_counter = 0; element_counter < NUM_OF_ELEMENTS; element_counter++) {
         Vmaspiedra+=Vmass[element_counter];
     }
-    //KRATOS_INFO("DEM") << "Total mass: " << Vmaspiedra << '\n';
+    std::cout << "Total mass: " << Vmaspiedra << '\n';
 
-    //C�lculo del centro de gravedad del cluster o piedra
+    //Calculo del centro de gravedad del cluster o piedra
     double Valor1=0.0;
     double Valor2=0.0;
     double Valor3=0.0;
@@ -180,7 +180,7 @@ int main() {
     double Xcdgrav=Valor1/Vmaspiedra;
     double Ycdgrav=Valor2/Vmaspiedra;
     double Zcdgrav=Valor3/Vmaspiedra;
-    KRATOS_INFO("DEM") << "Centroid: " << Xcdgrav << " " << Ycdgrav << " " << Zcdgrav << "\n\n";
+    std::cout << "Centroid: " << Xcdgrav << " " << Ycdgrav << " " << Zcdgrav << "\n\n";
 
     // Movemos el objecto y lo dejamos colocado tal que su centroide coincida con el origen:
     for (int node_counter = 0; node_counter < NUM_OF_NODES; node_counter++) {
@@ -196,7 +196,7 @@ int main() {
         sphcoord[2][spheres_counter] -= Zcdgrav;
     }
 
-    // C�lculo del tensor de inercias de cada elemento con respecto al CDG de cada piedra o cluster
+    // Calculo del tensor de inercias de cada elemento con respecto al CDG de cada piedra o cluster
     for (int element_counter = 0; element_counter < NUM_OF_ELEMENTS; element_counter++) {
 
         Local[0][element_counter]= BARIC[0][element_counter]-Xcdgrav;
@@ -229,10 +229,10 @@ int main() {
         VNERT[8]+=Vnerc[8][element_counter];
     }
 
-    // Queremos las inercias por unidad de masa, as� que dividimos las inercias por la masa total=total_volume*density (density = 1, no la ponemos en la f�rmula)
-    KRATOS_INFO("DEM") << "Inertias: " << VNERT[0]/total_volume << " " << VNERT[1]/total_volume << " " << VNERT[2]/total_volume << '\n';
-    KRATOS_INFO("DEM") << "Inertias: " << VNERT[3]/total_volume << " " << VNERT[4]/total_volume << " " << VNERT[5]/total_volume << '\n';
-    KRATOS_INFO("DEM") << "Inertias: " << VNERT[6]/total_volume << " " << VNERT[7]/total_volume << " " << VNERT[8]/total_volume << "\n\n";
+    // Queremos las inercias por unidad de masa, así que dividimos las inercias por la masa total=total_volume*density (density = 1, no la ponemos en la fórmula)
+    std::cout << "Inertias: " << VNERT[0]/total_volume << " " << VNERT[1]/total_volume << " " << VNERT[2]/total_volume << '\n';
+    std::cout << "Inertias: " << VNERT[3]/total_volume << " " << VNERT[4]/total_volume << " " << VNERT[5]/total_volume << '\n';
+    std::cout << "Inertias: " << VNERT[6]/total_volume << " " << VNERT[7]/total_volume << " " << VNERT[8]/total_volume << "\n\n";
 
     I[0][0] = VNERT[0]/total_volume;
     I[0][1] = VNERT[1]/total_volume;
@@ -246,13 +246,13 @@ int main() {
 
     Diagonalize(I, Q, D);
 
-    KRATOS_INFO("DEM") << "Eigenvectors: " << Q[0][0] << " " << Q[0][1] << " " << Q[0][2] << '\n';
-    KRATOS_INFO("DEM") << "Eigenvectors: " << Q[1][0] << " " << Q[1][1] << " " << Q[1][2] << '\n';
-    KRATOS_INFO("DEM") << "Eigenvectors: " << Q[2][0] << " " << Q[2][1] << " " << Q[2][2] << "\n\n";
+    std::cout << "Eigenvectors: " << Q[0][0] << " " << Q[0][1] << " " << Q[0][2] << '\n';
+    std::cout << "Eigenvectors: " << Q[1][0] << " " << Q[1][1] << " " << Q[1][2] << '\n';
+    std::cout << "Eigenvectors: " << Q[2][0] << " " << Q[2][1] << " " << Q[2][2] << "\n\n";
 
-    KRATOS_INFO("DEM") << "Eigenvalues: " << D[0][0] << " " << D[0][1] << " " << D[0][2] << '\n';
-    KRATOS_INFO("DEM") << "Eigenvalues: " << D[1][0] << " " << D[1][1] << " " << D[1][2] << '\n';
-    KRATOS_INFO("DEM") << "Eigenvalues: " << D[2][0] << " " << D[2][1] << " " << D[2][2] << "\n\n";
+    std::cout << "Eigenvalues: " << D[0][0] << " " << D[0][1] << " " << D[0][2] << '\n';
+    std::cout << "Eigenvalues: " << D[1][0] << " " << D[1][1] << " " << D[1][2] << '\n';
+    std::cout << "Eigenvalues: " << D[2][0] << " " << D[2][1] << " " << D[2][2] << "\n\n";
 
     // Rotamos el objeto y lo colocamos paralelo a sus ejes principales de inercia
 
@@ -281,48 +281,86 @@ int main() {
     }
 
     double Distance, CenterX, CenterY, CenterZ, Radius = 0.0;
-    int extreme_node_1, extreme_node_2;
-    std::vector<int> extreme_node;
-    for (int node_counter_1 = 0; node_counter_1 < NUM_OF_NODES; node_counter_1++) {
-        for (int node_counter_2 = 0; node_counter_2 < NUM_OF_NODES; node_counter_2++) {
-            if (node_counter_2 < node_counter_1) {
-                Distance  = sqrt((tcoord[0][node_counter_2] - tcoord[0][node_counter_1]) * (tcoord[0][node_counter_2] - tcoord[0][node_counter_1]) + (tcoord[1][node_counter_2] - tcoord[1][node_counter_1]) * (tcoord[1][node_counter_2] - tcoord[1][node_counter_1]) + (tcoord[2][node_counter_2] - tcoord[2][node_counter_1]) * (tcoord[2][node_counter_2] - tcoord[2][node_counter_1]));
+    int extreme_sphere_1, extreme_sphere_2;
+    std::vector<int> extreme_sphere;
+    for (int spheres_counter_1 = 0; spheres_counter_1 < NUM_OF_SPHERES; spheres_counter_1++) {
+        for (int spheres_counter_2 = 0; spheres_counter_2 < NUM_OF_SPHERES; spheres_counter_2++) {
+            if (spheres_counter_2 < spheres_counter_1) {
+                Distance  = sqrt((sphcoord[0][spheres_counter_2] - sphcoord[0][spheres_counter_1]) * (sphcoord[0][spheres_counter_2] - sphcoord[0][spheres_counter_1]) + (sphcoord[1][spheres_counter_2] - sphcoord[1][spheres_counter_1]) * (sphcoord[1][spheres_counter_2] - sphcoord[1][spheres_counter_1]) + (sphcoord[2][spheres_counter_2] - sphcoord[2][spheres_counter_1]) * (sphcoord[2][spheres_counter_2] - sphcoord[2][spheres_counter_1])) + sphrad[spheres_counter_2] + sphrad[spheres_counter_1];
                 if (Distance > 2 * Radius) {
                     Radius = 0.5 * Distance;
-                    extreme_node_1 = node_counter_1;
-                    extreme_node_2 = node_counter_2;
+                    extreme_sphere_1 = spheres_counter_1;
+                    extreme_sphere_2 = spheres_counter_2;
                 }
             }
         }
     }
 
-    extreme_node.push_back(extreme_node_1);
-    extreme_node.push_back(extreme_node_2);
+    extreme_sphere.push_back(extreme_sphere_1);
+    extreme_sphere.push_back(extreme_sphere_2);
 
-    CenterX = 0.5 * (tcoord[0][extreme_node[0]] + tcoord[0][extreme_node[1]]);
-    CenterY = 0.5 * (tcoord[1][extreme_node[0]] + tcoord[1][extreme_node[1]]);
-    CenterZ = 0.5 * (tcoord[2][extreme_node[0]] + tcoord[2][extreme_node[1]]);
+    double SphDistance[3];
+    SphDistance[0] = sphcoord[0][extreme_sphere[1]] - sphcoord[0][extreme_sphere[0]];
+    SphDistance[1] = sphcoord[1][extreme_sphere[1]] - sphcoord[1][extreme_sphere[0]];
+    SphDistance[2] = sphcoord[2][extreme_sphere[1]] - sphcoord[2][extreme_sphere[0]];
+
+    double SphDistanceNorm = sqrt(SphDistance[0] * SphDistance[0] + SphDistance[1] * SphDistance[1] + SphDistance[2] * SphDistance[2]);
+
+    double SphDistanceUnitVect[3];
+    SphDistanceUnitVect[0] = SphDistance[0] / SphDistanceNorm;
+    SphDistanceUnitVect[1] = SphDistance[1] / SphDistanceNorm;
+    SphDistanceUnitVect[2] = SphDistance[2] / SphDistanceNorm;
+
+    CenterX = 0.5 * (sphcoord[0][extreme_sphere[0]] - sphrad[extreme_sphere[0]] * SphDistanceUnitVect[0] + sphcoord[0][extreme_sphere[1]] + sphrad[extreme_sphere[1]] * SphDistanceUnitVect[0]);
+    CenterY = 0.5 * (sphcoord[1][extreme_sphere[0]] - sphrad[extreme_sphere[0]] * SphDistanceUnitVect[1] + sphcoord[1][extreme_sphere[1]] + sphrad[extreme_sphere[1]] * SphDistanceUnitVect[1]);
+    CenterZ = 0.5 * (sphcoord[2][extreme_sphere[0]] - sphrad[extreme_sphere[0]] * SphDistanceUnitVect[2] + sphcoord[2][extreme_sphere[1]] + sphrad[extreme_sphere[1]] * SphDistanceUnitVect[2]);
 
     double CheckX, CheckY, CheckZ, TempRad;
     std::vector<double> extreme_radius;
-    int node_counter_check = 0;
+    int sphere_counter_check = 0;
+    double CheckDistance[3];
+    double CheckDistanceNorm;
+    double CheckDistanceUnitVect[3];
+    double CheckRadius[3];
+    double CheckRadiusNorm;
+    double CheckRadiusUnitVect[3];
 
-    while (node_counter_check < (NUM_OF_NODES)) {  //CHECK that all nodes are inside the big sphere
-        CheckX = tcoord[0][node_counter_check] - CenterX;
-        CheckY = tcoord[1][node_counter_check] - CenterY;
-        CheckZ = tcoord[2][node_counter_check] - CenterZ;
+    while (sphere_counter_check < (NUM_OF_SPHERES)) {  //CHECK that all nodes are inside the big sphere
+        CheckDistance[0] = sphcoord[0][sphere_counter_check] - CenterX;
+        CheckDistance[1] = sphcoord[1][sphere_counter_check] - CenterY;
+        CheckDistance[2] = sphcoord[2][sphere_counter_check] - CenterZ;
+
+        CheckDistanceNorm = sqrt(CheckDistance[0] * CheckDistance[0] + CheckDistance[1] * CheckDistance[1] + CheckDistance[2] * CheckDistance[2]);
+
+        CheckDistanceUnitVect[0] = CheckDistance[0] / CheckDistanceNorm;
+        CheckDistanceUnitVect[1] = CheckDistance[1] / CheckDistanceNorm;
+        CheckDistanceUnitVect[2] = CheckDistance[2] / CheckDistanceNorm;
+
+        CheckX = sphcoord[0][sphere_counter_check] + sphrad[sphere_counter_check] * CheckDistanceUnitVect[0] - CenterX;
+        CheckY = sphcoord[1][sphere_counter_check] + sphrad[sphere_counter_check] * CheckDistanceUnitVect[1] - CenterY;
+        CheckZ = sphcoord[2][sphere_counter_check] + sphrad[sphere_counter_check] * CheckDistanceUnitVect[2] - CenterZ;
         TempRad  = sqrt(CheckX * CheckX + CheckY * CheckY + CheckZ * CheckZ);
 
-        if (TempRad > Radius) {
-            extreme_node.push_back(node_counter_check);
+        if (TempRad - Radius > 1.0e-15) {
+            extreme_sphere.push_back(sphere_counter_check);
             CenterX = CenterX + CheckX - (Radius * (CheckX/TempRad));
             CenterY = CenterY + CheckY - (Radius * (CheckY/TempRad));
             CenterZ = CenterZ + CheckZ - (Radius * (CheckZ/TempRad));
 
             extreme_radius.clear();
 
-            for(int i = 0; i < extreme_node.size(); i++) {
-                extreme_radius.push_back(sqrt((tcoord[0][extreme_node[i]] - CenterX) * (tcoord[0][extreme_node[i]] - CenterX) + (tcoord[1][extreme_node[i]] - CenterY) * (tcoord[1][extreme_node[i]] - CenterY) + (tcoord[2][extreme_node[i]] - CenterZ) * (tcoord[2][extreme_node[i]] - CenterZ)));
+            for(int i = 0; i < extreme_sphere.size(); i++) {
+                CheckRadius[0] = sphcoord[0][sphere_counter_check] - CenterX;
+                CheckRadius[1] = sphcoord[1][sphere_counter_check] - CenterY;
+                CheckRadius[2] = sphcoord[2][sphere_counter_check] - CenterZ;
+
+                CheckRadiusNorm = sqrt(CheckRadius[0] * CheckRadius[0] + CheckRadius[1] * CheckRadius[1] + CheckRadius[2] * CheckRadius[2]);
+
+                CheckRadiusUnitVect[0] = CheckRadius[0] / CheckRadiusNorm;
+                CheckRadiusUnitVect[1] = CheckRadius[1] / CheckRadiusNorm;
+                CheckRadiusUnitVect[2] = CheckRadius[2] / CheckRadiusNorm;
+
+                extreme_radius.push_back(sqrt((sphcoord[0][extreme_sphere[i]] + sphrad[extreme_sphere[i]] * CheckRadiusUnitVect[0] - CenterX) * (sphcoord[0][extreme_sphere[i]] + sphrad[extreme_sphere[i]] * CheckRadiusUnitVect[0] - CenterX) + (sphcoord[1][extreme_sphere[i]] + sphrad[extreme_sphere[i]] * CheckRadiusUnitVect[1] - CenterX) * (sphcoord[1][extreme_sphere[i]] + sphrad[extreme_sphere[i]] * CheckRadiusUnitVect[1] - CenterX) + (sphcoord[2][extreme_sphere[i]] + sphrad[extreme_sphere[i]] * CheckRadiusUnitVect[2] - CenterX) * (sphcoord[2][extreme_sphere[i]] + sphrad[extreme_sphere[i]] * CheckRadiusUnitVect[2] - CenterX)));
             }
 
             for(int i = 0; i < extreme_radius.size(); i++) {
@@ -330,33 +368,36 @@ int main() {
                     Radius = extreme_radius[i];
                 }
             }
-            node_counter_check = 0;
+            sphere_counter_check = 0;
         }
         else {
-            node_counter_check++;
+            sphere_counter_check++;
         }
     }
 
     double diameter = 2 * Radius;
 
-    double characteristic_size = 2 * std::pow(3 * total_volume / (4 * Globals::Pi), 0.33333333333333333);
+    double characteristic_size = 2 * std::pow(3 * total_volume / (4 * M_PI), 1/3.);
 
-    KRATOS_INFO("DEM") << "\nThe diameter is: " << diameter << "\n\n";
+    std::cout << "\nThe diameter is: " << diameter << "\n\n";
 
-    KRATOS_INFO("DEM") << "\nThe characteristic size is: " << characteristic_size << "\n\n";
+    std::cout << "\nThe characteristic size is: " << characteristic_size << "\n\n";
 
-    std::ofstream outputfile("ballast6cluster3Dred.clu", std::ios_base::out);
-    outputfile << "//\n//   Cluster Name: \"ballast6cluster3Dred\"\n";
+    std::cout << "\nThe size ratio is: " << diameter/characteristic_size << "\n\n";
+
+    std::ofstream outputfile("M6x25_120spheres.clu", std::ios_base::out);
+    outputfile << "//\n//   Cluster Name: \"M6x25_120spheres\"\n";
     outputfile << "//   Author: Joaquin Irazabal\n";
-    outputfile << "//   Date:   2016-10-26\n//\n\n";
+    outputfile << "//   Date:   2019-09-30\n//\n\n";
 
-    outputfile << "Name\nballast6cluster3Dred\n\nBegin centers_and_radii\n";
+    outputfile << "Name\nM6x25_120spheres\n\nBegin centers_and_radii\n";
     for (int spheres_counter = 0; spheres_counter < NUM_OF_SPHERES; spheres_counter++) {
         outputfile << sphcoord[0][spheres_counter] << " " << sphcoord[1][spheres_counter] << " " << sphcoord[2][spheres_counter] << " " << sphrad[spheres_counter] << '\n';
     }
     outputfile << "End centers_and_radii\n\n";
     outputfile << "Particle_center_and_diameter\n" << CenterX << " " << CenterY << " " << CenterZ << " " << diameter << "\n\n";
-    outputfile << "Size\n" << characteristic_size << "\n\n" << "Volume\n" << total_volume << "\n\n";
+//     outputfile << "Size\n" << characteristic_size << "\n\n" << "Volume\n" << total_volume << "\n\n";
+    outputfile << "Size\n" << diameter << "\n\n" << "Volume\n" << total_volume << "\n\n";
     outputfile << "Inertia per unit mass\n" << D[0][0] << '\n' << D[1][1] << '\n' << D[2][2] << '\n';
 
     outputfile.close();
@@ -445,7 +486,7 @@ void Diagonalize(const double (&A)[3][3], double (&Q)[3][3], double (&D)[3][3]) 
         if (c == 1.0) break;  // No room for improvement, reached machine precision.
 
         jr[0 ]  = jr[1] = jr[2] = jr[3] = 0.0;
-        jr[k0]  = sgn * sqrt((1.0-c)/2.0);  // using 1/2 angle identity std::sin(a/2) = sqrt((1-cos(a))/2)
+        jr[k0]  = sgn * sqrt((1.0-c)/2.0);  // using 1/2 angle identity sin(a/2) = sqrt((1-cos(a))/2)
         jr[k0] *= -1.0; // since our quat-to-matrix convention was for v*M instead of M*v
         jr[3 ]  = sqrt(1.0f - jr[k0] * jr[k0]);
 

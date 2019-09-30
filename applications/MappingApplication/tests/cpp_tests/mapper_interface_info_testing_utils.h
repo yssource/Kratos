e754af73164f0e4e1200038ec5b8e35bfffa56e5
Philipp <philipp.bucher@tum.de>
2019-09-30 08:58:16 +0200

    adding files for testing utils

---
+++ applications/MappingApplication/tests/cpp_tests/mapper_interface_info_testing_utils.h
@@ -0,0 +1,26 @@
+
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Philipp Bucher
+//
+
+// Project includes
+#include "testing/define.h"
+#include "testing/testing.h"
+
+namespace Kratos {
+namespace Testing {
+namespace MapperInterfaceInfoTeestingUtils {
+
+
+
+}  // namespace MapperInterfaceInfoTeestingUtils
+}  // namespace Testing
+}  // namespace Kratos
---
+++ applications/MappingApplication/tests/cpp_tests/mapper_local_system_testing_utils.cpp
@@ -0,0 +1,25 @@
+
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:		 BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Philipp Bucher
+//
+
+// Project includes
+#include "testing/mapper_local_system_testing_utils.h"
+
+namespace Kratos {
+namespace Testing {
+namespace MapperLocalSystemTestingUtils {
+
+
+
+}  // namespace MapperLocalSystemTestingUtils
+}  // namespace Testing
+}  // namespace Kratos

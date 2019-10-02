55b32cdca4b871cdaa3ae2ecb5725eebb0f9a144
riccardotosi <rtosi@cimne.upd.edu>
2019-10-02 12:30:36 +0200

    Update import mechanism

--- applications/ExaquteSandboxApplication/ExaquteSandboxApplication.py
+++ applications/ExaquteSandboxApplication/ExaquteSandboxApplication.py
@@ -1,11 +1,11 @@
 from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
-from KratosExaquteSandboxApplication import *
-application = KratosExaquteSandboxApplication()
+
+# Application dependent names and paths
+import KratosMultiphysics
+import KratosExaquteSandboxApplication
+application = KratosExaquteSandboxApplication.KratosExaquteSandboxApplication()
 application_name = "KratosExaquteSandboxApplication"
 application_folder = "ExaquteSandboxApplication"
 
 # The following lines are common for all applications
-from .. import application_importer
-import inspect
-caller = inspect.stack()[1]  # Information about the file that imported this, to check for unexpected imports
-application_importer.ImportApplication(application, application_name, application_folder, caller, __path__)
+KratosMultiphysics._ImportApplicationAsModule(application, application_name, application_folder, __path__)

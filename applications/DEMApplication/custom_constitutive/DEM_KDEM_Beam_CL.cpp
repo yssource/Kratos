c353f38807009a68a7fcd3ac30b88ff8e519e4d3
Joaquín <jirazabal@cimne.upc.edu>
2019-10-25 11:59:32 +0200

    More improvements

--- applications/DEMApplication/custom_constitutive/DEM_KDEM_Beam_CL.cpp
+++ applications/DEMApplication/custom_constitutive/DEM_KDEM_Beam_CL.cpp
@@ -46,12 +46,12 @@ namespace Kratos {
         const double Inertia_Ix = 0.5 * (element1->GetProperties()[BEAM_PLANAR_MOMENT_OF_INERTIA_X] + element2->GetProperties()[BEAM_PLANAR_MOMENT_OF_INERTIA_X]);
         const double Inertia_Iy = 0.5 * (element1->GetProperties()[BEAM_PLANAR_MOMENT_OF_INERTIA_Y] + element2->GetProperties()[BEAM_PLANAR_MOMENT_OF_INERTIA_Y]);
 
-        kt_el_0 = 3.0 * equiv_young * Inertia_Iy / (calculation_area * initial_dist);
-        kt_el_1 = 3.0 * equiv_young * Inertia_Ix / (calculation_area * initial_dist);
+        // kt_el_0 = 3.0 * equiv_young * Inertia_Iy / (calculation_area * initial_dist);
+        // kt_el_1 = 3.0 * equiv_young * Inertia_Ix / (calculation_area * initial_dist);
 
         // 2013 Bourrier. Discrete modeling of granular soils reinforcement by plant roots FOR CYLINDERS!!! CHECK!!!
-        // kt_el_0 = 12.0 * equiv_young * Inertia_Iy / (initial_dist * initial_dist * initial_dist);
-        // kt_el_1 = 12.0 * equiv_young * Inertia_Ix / (initial_dist * initial_dist * initial_dist);
+        kt_el_0 = 12.0 * equiv_young * Inertia_Iy / (initial_dist * initial_dist * initial_dist);
+        kt_el_1 = 12.0 * equiv_young * Inertia_Ix / (initial_dist * initial_dist * initial_dist);
     }
 
     void DEM_KDEM_Beam::CalculateViscoDampingCoeff(double& equiv_visco_damp_coeff_normal,
@@ -64,7 +64,7 @@ namespace Kratos {
                                                    const double kt_el_1) {
 
         const double equiv_mass  = std::max(element1->GetMass(), element2->GetMass());
-        const double aux_mass    = 0.5 *(element1->GetProperties()[BEAM_MASS] / equiv_mass);
+        const double aux_mass    = 0.5 * (element1->GetProperties()[BEAM_MASS] / equiv_mass);
         const double equiv_gamma = 0.5 * (element1->GetProperties()[DAMPING_GAMMA] + element2->GetProperties()[DAMPING_GAMMA]);
 
         equiv_visco_damp_coeff_normal       = 2.0 * equiv_gamma * aux_mass * sqrt(equiv_mass * kn_el  );
@@ -190,13 +190,8 @@ namespace Kratos {
                                                   DenseVector<int>& search_control_vector,
                                                   const ProcessInfo& r_process_info) {
 
-        array_1d<double, 3> other_to_me_vect;
-        noalias(other_to_me_vect) = element1->GetGeometry()[0].Coordinates() - element2->GetGeometry()[0].Coordinates();
-        const double distance = DEM_MODULUS_3(other_to_me_vect);
-        const double norm_distance = (element1->GetRadius() + element2->GetRadius()) / distance; // If spheres are not tangent the Damping coefficient has to be normalized
-
-        LocalElasticContactForce[0] = OldLocalElasticContactForce[0] - kt_el_0 * LocalDeltDisp[0] * norm_distance * norm_distance; // 0: first tangential
-        LocalElasticContactForce[1] = OldLocalElasticContactForce[1] - kt_el_1 * LocalDeltDisp[1] * norm_distance * norm_distance; // 1: second tangential
+        LocalElasticContactForce[0] = OldLocalElasticContactForce[0] - kt_el_0 * LocalDeltDisp[0]; // 0: first tangential
+        LocalElasticContactForce[1] = OldLocalElasticContactForce[1] - kt_el_1 * LocalDeltDisp[1]; // 1: second tangential
     }
 
     void DEM_KDEM_Beam::CalculateViscoDamping(double LocalRelVel[3],
@@ -242,9 +237,11 @@ namespace Kratos {
 
         // const double element_mass = element->GetDensity() * element->GetProperties()[BEAM_DISTANCE] * element->GetProperties()[BEAM_CROSS_SECTION];
 
-        const double auxX = 0.5 * element->GetProperties()[FULL_BEAM_PRINCIPAL_MOMENTS_OF_INERTIA_Y] * element->GetProperties()[BEAM_MASS] / MomentOfInertiaX;
-        const double auxY = 0.5 * element->GetProperties()[FULL_BEAM_PRINCIPAL_MOMENTS_OF_INERTIA_Z] * element->GetProperties()[BEAM_MASS] / MomentOfInertiaY;
-        const double auxZ = 0.5 * element->GetProperties()[FULL_BEAM_PRINCIPAL_MOMENTS_OF_INERTIA_X] * element->GetProperties()[BEAM_MASS] / MomentOfInertiaZ;
+        double norm_distance = (element->GetRadius() + neighbor->GetRadius()) / distance; // If spheres are not tangent the Damping coefficient, DeltaRotatedAngle and DeltaAngularVelocity have to be normalized
+
+        const double auxX = 0.5 * element->GetProperties()[BEAM_PRINCIPAL_MOMENTS_OF_INERTIA_Y] * element->GetProperties()[BEAM_MASS] / MomentOfInertiaX;
+        const double auxY = 0.5 * element->GetProperties()[BEAM_PRINCIPAL_MOMENTS_OF_INERTIA_Z] * element->GetProperties()[BEAM_MASS] / MomentOfInertiaY;
+        const double auxZ = 0.5 * element->GetProperties()[BEAM_PRINCIPAL_MOMENTS_OF_INERTIA_X] * element->GetProperties()[BEAM_MASS] / MomentOfInertiaZ;
 
         const double equiv_shear   = equiv_young / (2.0 * (1 + equiv_poisson));
 
@@ -256,19 +253,18 @@ namespace Kratos {
         const double other_gamma = neighbor->GetProperties()[DAMPING_GAMMA];
         const double equiv_gamma = 0.5 * (my_gamma + other_gamma);
 
-        double norm_distance = (element->GetRadius() + neighbor->GetRadius()) / distance; // If spheres are not tangent the Damping coefficient, DeltaRotatedAngle and DeltaAngularVelocity have to be normalized
         // double norm_distance_torsion = 2.0 / (element->GetRadius() + neighbor->GetRadius());
 
-        const double k_rot_x = equiv_young * Inertia_Ix * norm_distance / distance;
-        const double k_rot_y = equiv_young * Inertia_Iy * norm_distance / distance;
-        const double k_tor   = equiv_shear * Inertia_J / distance;
+        const double k_rot_x = equiv_young * Inertia_Ix / distance;
+        const double k_rot_y = equiv_young * Inertia_Iy / distance;
+        const double k_tor   = equiv_shear * Inertia_J  / distance;
 
-        const double visc_param_rot_x = 2.0 * equiv_gamma * auxX * sqrt(MomentOfInertiaX * k_rot_x) * norm_distance;
-        const double visc_param_rot_y = 2.0 * equiv_gamma * auxY * sqrt(MomentOfInertiaY * k_rot_y) * norm_distance;
+        const double visc_param_rot_x = 2.0 * equiv_gamma * auxX * sqrt(MomentOfInertiaX * k_rot_x);// * norm_distance;
+        const double visc_param_rot_y = 2.0 * equiv_gamma * auxY * sqrt(MomentOfInertiaY * k_rot_y);// * norm_distance;
         const double visc_param_tor   = 2.0 * equiv_gamma * auxZ * sqrt(MomentOfInertiaZ * k_tor  );// * norm_distance_torsion;
 
-        ElasticLocalRotationalMoment[0] = -k_rot_x * LocalDeltaRotatedAngle[0];
-        ElasticLocalRotationalMoment[1] = -k_rot_y * LocalDeltaRotatedAngle[1];
+        ElasticLocalRotationalMoment[0] = -k_rot_x * LocalDeltaRotatedAngle[0] * norm_distance;
+        ElasticLocalRotationalMoment[1] = -k_rot_y * LocalDeltaRotatedAngle[1] * norm_distance;
         ElasticLocalRotationalMoment[2] = -k_tor   * LocalDeltaRotatedAngle[2];
 
         ViscoLocalRotationalMoment[0] = -visc_param_rot_x * LocalDeltaAngularVelocity[0];
